59b4c440907ac4cb70b76f95068f068819c40b8a
Robert Nelson <robertcnelson@gmail.com>
2014-09-08 13:29:26 -0500

    fixed
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/generate_dsc.sh
+++ tools/generate_dsc.sh
@@ -5,7 +5,7 @@
 if [ -f ${package}_${version}.orig.tar.xz ] ; then
 	tar xf ${package}_${version}.orig.tar.xz
 	cd ./${package}_${version}
-	cp -rv ../debian/jessie/ ./
+	cp -rv ../debian/jessie/* ./
 	debuild -us -uc -S
 fi
 
--- tools/generate_wheezy_dsc.sh
+++ tools/generate_wheezy_dsc.sh
@@ -5,7 +5,7 @@
 if [ -f ${package}_${version}.orig.tar.xz ] ; then
 	tar xf ${package}_${version}.orig.tar.xz
 	cd ./${package}_${version}
-	cp -rv ../debian/wheezy/ ./
+	cp -rv ../debian/wheezy/* ./
 	debuild -us -uc -S
 fi
 
