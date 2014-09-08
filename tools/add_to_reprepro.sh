4f5ff1a1faef1ecefc14a52c5f708e51eb1ad9a9
Robert Nelson <robertcnelson@gmail.com>
2014-09-08 15:06:49 -0500

    and backports
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/add_to_reprepro.sh
+++ tools/add_to_reprepro.sh
@@ -6,9 +6,17 @@ repo="/var/www/html/repos.rcn-ee.net/debian/"
 outgoing="/var/www/html/farm/outgoing"
 
 add_to_reprepro () {
-	if [ -f ${outgoing}/${dist}/${package}_${debian_version}_armhf.changes ] ; then
-		reprepro -b ${repo} -C main include ${dist} ${outgoing}/${dist}/${package}_${debian_version}_armhf.changes
+	if [ ! -f ${repo}pool/main/${reprepro_dir}/${package}_${debian_version}${bpo}_armhf.deb ] ; then
+		if [ -f ${outgoing}/${dist}/${package}_${debian_version}${bpo}_armhf.changes ] ; then
+			reprepro -b ${repo} -C main include ${dist} ${outgoing}/${dist}/${package}_${debian_version}${bpo}_armhf.changes
+		fi
 	fi
 }
 
+dist="wheezy"
+bpo="~bpo70+1"
+add_to_reprepro
+
 dist="jessie"
+bpo=""
+add_to_reprepro
