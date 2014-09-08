18a540c157ca8df7e5d8ee74e4507ee8355b2520
Robert Nelson <robertcnelson@gmail.com>
2014-09-08 11:32:40 -0500

    upload files
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/build_deb.sh
+++ tools/build_deb.sh
@@ -3,10 +3,32 @@
 . version.sh
 
 build_package () {
+	out_dir="/mnt/farm/outgoing/${dist}/${package}_${debian_version}"
 	if [ -f /var/lib/sbuild/${dist}-armhf.tar.gz ] ; then
 		mkdir ./${dist}
 		cd ./${dist}
 		sbuild -d ${dist} http://httphost/farm/incoming/${dist}/${package}_${debian_version}.dsc
+
+		if [ -f ${package}_${debian_version}_armhf.changes ] ; then
+			mkdir -p ${out_dir}/
+			cp -v ${package}_${debian_version}_armhf.changes ${out_dir}/
+
+			if [ -f ${package}_${version}.orig.tar.xz ] ; then
+				cp -v ${package}_${version}.orig.tar.xz ${out_dir}/
+			fi
+
+			if [ -f ${package}_${debian_version}.debian.tar.gz ] ; then
+				cp -v ${package}_${debian_version}.debian.tar.gz ${out_dir}/
+			fi
+
+			if [ -f ${package}_${debian_version}.debian.tar.xz ] ; then
+				cp -v ${package}_${debian_version}.debian.tar.xz ${out_dir}/
+			fi
+
+			if [ -f ${package}_${debian_version}_armhf.deb ] ; then
+				cp -v ${package}_${debian_version}_armhf.deb ${out_dir}/
+			fi
+		fi
 		cd ../
 	fi
 }
