082f680fab01dd27bebe6afb2eeebbbc7699c1b1
Robert Nelson <robertcnelson@gmail.com>
2014-09-08 13:23:08 -0500

    needs to backport to wheezy
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/generate_wheezy_dsc.sh
+++ tools/generate_wheezy_dsc.sh
@@ -5,7 +5,7 @@
 if [ -f ${package}_${version}.orig.tar.xz ] ; then
 	tar xf ${package}_${version}.orig.tar.xz
 	cd ./${package}_${version}
-	cp -rv ../jessie/debian/ ./
+	cp -rv ../wheezy/debian/ ./
 	debuild -us -uc -S
 fi
 
