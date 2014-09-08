4042545da93c0ec6f051f9ee8870eef395b37dca
Robert Nelson <robertcnelson@gmail.com>
2014-09-08 11:08:28 -0500

    add tool to setup sbuilds
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ tools/setup_sbuild.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+proxy="apt-proxy:3142/"
+debian_server="ftp.us.debian.org/debian"
+ubuntu_server="ports.ubuntu.com/ubuntu"
+
+setup_update_sbuild () {
+	if [ ! -f /var/lib/sbuild/${dist}-armhf.tar.gz ] ; then
+		sudo sbuild-createchroot --make-sbuild-tarball=/var/lib/sbuild/${dist}-armhf.tar.gz ${dist} `mktemp -d` ${mirror}
+	else
+		sudo sbuild-update -udcar ${dist}-armhf-sbuild
+	fi
+}
+
+mirror="http://${proxy}/${debian_server}"
+dist="wheezy"
+setup_update_sbuild
+
+dist="jessie"
+setup_update_sbuild
+
+dist="sid"
+setup_update_sbuild
+
+mirror="http://${proxy}/${ubuntu_server}"
+dist="trusty"
+setup_update_sbuild
+
+dist="utopic"
+setup_update_sbuild
+
