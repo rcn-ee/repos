d6539a855f628f1c2ee804125319cac3aa36b1f4
Robert Nelson <robertcnelson@gmail.com>
2014-09-08 13:25:54 -0500

    do the correct order
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/generate_wheezy_dsc.sh
+++ tools/generate_wheezy_dsc.sh
@@ -5,7 +5,7 @@
 if [ -f ${package}_${version}.orig.tar.xz ] ; then
 	tar xf ${package}_${version}.orig.tar.xz
 	cd ./${package}_${version}
-	cp -rv ../wheezy/debian/ ./
+	cp -rv ../debian/wheezy/ ./
 	debuild -us -uc -S
 fi
 
