b0f5fe754709dbab5ea721d389837662b757d015
Robert Nelson <robertcnelson@gmail.com>
2014-09-08 10:49:49 -0500

    push out
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ tools/push_builder.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+. version.sh
+
+pushout () {
+	if [ ! -d /mnt/farm/incoming/${dist}/ ] ; then
+		mkdir -p /mnt/farm/incoming/${dist}/
+	fi
+
+	if [ -f ${package}_${version}.orig.tar.xz ] ; then
+		cp -v ${package}_${version}.orig.tar.xz /mnt/farm/incoming/${dist}/
+	else
+		../tools/generate_source.sh
+		if [ -f ${package}_${version}.orig.tar.xz ] ; then
+			cp -v ${package}_${version}.orig.tar.xz /mnt/farm/incoming/${dist}/
+		else
+			exit 1
+		fi
+	fi
+
+	if [ -f ${package}_${version}.dsc ] ; then
+		cp -v ${package}_${version}.debian.tar.xz /mnt/farm/incoming/${dist}/
+		cp -v ${package}_${version}.dsc /mnt/farm/incoming/${dist}/
+	else
+		../tools/generate_dsc.sh
+		if [ -f ${package}_${version}.dsc ] ; then
+			cp -v ${package}_${version}.debian.tar.xz /mnt/farm/incoming/${dist}/
+			cp -v ${package}_${version}.dsc /mnt/farm/incoming/${dist}/
+		else
+			exit 1
+		fi
+	fi
+}
+
+dist="wheezy"
+pushout
+
+dist="jessie"
+pushout
+
+dist="sid"
+pushout
+
+dist="trusty"
+pushout
+
+dist="utopic"
+pushout
