d8255912a6abba2b4383b69fffcc1869699286aa
Robert Nelson <robertcnelson@gmail.com>
2014-09-09 11:05:40 -0500

    build backport
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ tools/bpo_build_deb.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+. version.sh
+
+build_package () {
+	out_dir="/mnt/farm/outgoing/${dist}/${package}_${debian_version}"
+	if [ -f /var/lib/sbuild/${dist}-armhf.tar.gz ] ; then
+		if [ -d ./${dist} ] ; then
+			rm -rf ./${dist}/
+		fi
+
+		mkdir ./${dist}
+		cd ./${dist}
+		sbuild -d ${dist} http://httphost/farm/incoming/${dist}/${package}_${debian_version}${bpo}.dsc
+
+		if [ -f ${package}_${debian_version}${bpo}_armhf.changes ] ; then
+			mkdir -p ${out_dir}/
+			cp -v ${package}_${debian_version}${bpo}_armhf.changes ${out_dir}/
+
+			if [ -f ${package}_${version}.orig.tar.xz ] ; then
+				cp -v ${package}_${version}.orig.tar.xz ${out_dir}/
+			fi
+
+			if [ -f ${package}_${debian_version}${bpo}.dsc ] ; then
+				cp -v ${package}_${debian_version}${bpo}.dsc ${out_dir}/
+			fi
+
+			if [ -f ${package}_${debian_version}${bpo}.debian.tar.gz ] ; then
+				cp -v ${package}_${debian_version}${bpo}.debian.tar.gz ${out_dir}/
+			fi
+
+			if [ -f ${package}_${debian_version}${bpo}.debian.tar.xz ] ; then
+				cp -v ${package}_${debian_version}${bpo}.debian.tar.xz ${out_dir}/
+			fi
+
+			if [ -f ${package}_${debian_version}${bpo}_armhf.deb ] ; then
+				cp -v ${package}_${debian_version}${bpo}_armhf.deb ${out_dir}/
+			fi
+		fi
+		cd ../
+	fi
+}
+
+dist="wheezy"
+bpo="~bpo70+1"
+build_package
+
