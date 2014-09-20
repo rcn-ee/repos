78175713d7add6da21a8ca1b7adc75111397e10d
Robert Nelson <robertcnelson@gmail.com>
2014-09-19 19:57:44 -0500

    dart: try again
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- dart/version.sh
+++ dart/version.sh
@@ -2,7 +2,7 @@
 
 package="dart"
 archive="dart"
-version="1.7.0-edge.40405"
-debian_version="1.7.0-edge.40405-1"
+version="1.7.0-edge.40531"
+debian_version="1.7.0-edge.40531-1"
 reprepro_dir="d/${package}"
 wheezy_backport="~bpo70+1"
