5cba4b8ad451b8733e85c40767cf462e016dfb74
Robert Nelson <robertcnelson@gmail.com>
2014-09-24 22:00:51 -0500

    fix: xf86-video-modesetting
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- xf86-video-modesetting/version.sh
+++ xf86-video-modesetting/version.sh
@@ -3,19 +3,19 @@
 mirror="http://ftp.de.debian.org/debian"
 
 package_name="xf86-video-modesetting"
-debian_pkg_name="${package_name}"
+debian_pkg_name="xserver-xorg-video-modesetting"
 package_version="0.9.0"
-package_source="xserver-xorg-video-modesetting_${package_version}.orig.tar.gz"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
 git_repo=""
 git_sha=""
-reprepro_dir="x/xserver-xorg-video-modesetting"
+reprepro_dir="x/${debian_pkg_name}"
 dl_path="${mirror}/pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1"
 debian_untar=""
-debian_patch="xserver-xorg-video-modesetting_${debian_version}.diff.gz"
+debian_patch="${debian_pkg_name}_${debian_version}.diff.gz"
 
 wheezy_version="~bpo70+20140924+1"
 jessie_version="~20140924+1"
