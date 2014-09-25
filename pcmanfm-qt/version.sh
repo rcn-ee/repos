85874a30eebbf98ed4a703d33fd4a371382f71fb
Robert Nelson <robertcnelson@gmail.com>
2014-09-25 15:48:40 -0500

    pkg: pcmanfm-qt
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ pcmanfm-qt/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#http://packages.siduction.org/lxqt/pool/main/p/pcmanfm-qt/
+
+mirror="http://packages.siduction.org"
+
+package_name="pcmanfm-qt"
+debian_pkg_name="${package_name}"
+package_version="0.8.0"
+package_source="${package_name}_${package_version}.orig.tar.bz2"
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="p/${package_name}"
+dl_path="${mirror}/lxqt/pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-2"
+debian_untar=""
+debian_patch="${package_name}_${debian_version}.debian.tar.xz"
+
+wheezy_version="~bpo70+20140924+1"
+jessie_version="~20140925+1"
