5b726999d5844ee593d8d4e7bd51785bd4e0865e
Robert Nelson <robertcnelson@gmail.com>
2014-09-24 19:32:46 -0500

    reprepro: do this here
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/add_to_reprepro.sh
+++ tools/add_to_reprepro.sh
@@ -6,6 +6,12 @@ repo="/var/www/html/repos.rcn-ee.net/debian/"
 outgoing="/var/www/html/farm/outgoing"
 
 add_to_reprepro () {
+
+	if [ -f ${repo}pool/main/${reprepro_dir}/${deb_source} ] ; then
+		mkdir -p ${repo}pool/main/${reprepro_dir}/
+		cp -v ${outgoing}/${dist}/${archive}_${debian_version}/${deb_source} ${repo}pool/main/${reprepro_dir}/
+	fi
+
 	wfile="${outgoing}/${dist}/${archive}_${debian_version}/${archive}_${debian_version}${bpo}_armhf.changes"
 	if [ ! -f ${repo}pool/main/${reprepro_dir}/${archive}_${debian_version}${bpo}_armhf.deb ] ; then
 		if [ -f ${wfile} ] ; then
