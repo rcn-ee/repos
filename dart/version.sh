5fdee17fa3e74659380edbf322559818268069cf
Robert Nelson <robertcnelson@gmail.com>
2014-09-29 18:02:52 -0500

    dart: 1.7.0-edge.40791
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- dart/version.sh
+++ dart/version.sh
@@ -2,7 +2,7 @@
 
 package_name="dart"
 debian_pkg_name="${package_name}"
-package_version="1.7.0-edge.40785"
+package_version="1.7.0-edge.40791"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -15,7 +15,7 @@ debian_version="${package_version}-1"
 debian_patch=""
 debian_diff=""
 
-
 archive="enable"
+
 wheezy_version="~bpo70+20140929+1"
 jessie_version="~20140929+1"
