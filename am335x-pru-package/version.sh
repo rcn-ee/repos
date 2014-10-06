44c8baa1c519b5cb839dc74152ec8f1c71890664
Robert Nelson <robertcnelson@gmail.com>
2014-10-06 10:50:36 -0500

    pkg: am335x-pru-package
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ am335x-pru-package/version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+package_name="am335x-pru-package"
+debian_pkg_name="${package_name}"
+package_version="0.20140817"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="https://github.com/beagleboard/am335x_pru_package"
+git_sha="506e074859891a2b350eb4f5fcb451c4961410ea"
+reprepro_dir="a/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-1"
+debian_patch=""
+debian_diff=""
+
+wheezy_version="~bpo70+20140924+1"
+jessie_version="~20141006+1"
