b6ffddf9b20d8ab98671f2dd4c7fd3e84502fffb
Robert Nelson <robertcnelson@gmail.com>
2014-10-07 11:02:45 -0500

    pkg: add cmst
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ cmst/version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+package_name="cmst"
+debian_pkg_name="${package_name}"
+package_version="2014.10.05"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="https://github.com/andrew-bibb/cmst.git"
+git_sha="3fcaea373dfb5b5ad0e4a48d1c196fd920c02b71"
+reprepro_dir="c/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-2"
+debian_patch=""
+debian_diff=""
+
+wheezy_version="~bpo70+20141006+1"
+jessie_version="~20141007+1"
