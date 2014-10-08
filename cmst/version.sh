fcad1e82afda5e9ffd54b12dcb444e3c74f7a52f
Robert Nelson <robertcnelson@gmail.com>
2014-10-08 13:17:47 -0500

    cmst: should have had git tag from the start
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cmst/version.sh
+++ cmst/version.sh
@@ -2,7 +2,7 @@
 
 package_name="cmst"
 debian_pkg_name="${package_name}"
-package_version="2014.10.05-3"
+package_version="2014.10.05-git20141007"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
@@ -11,7 +11,7 @@ git_sha="44ed6cf6f9ae74b802e11b2cb4300c5b912c1657"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
-debian_version="${package_version}"
+debian_version="${package_version}-1"
 debian_patch=""
 debian_diff=""
 
