7268d5c56065a579d0081f7cd07af86f279176ca
Robert Nelson <robertcnelson@gmail.com>
2014-10-08 12:20:02 -0500

    chromium-browser: 37.0.2062.120
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ chromium-browser/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#https://omahaproxy.appspot.com/
+
+package_name="chromium"
+debian_pkg_name="chromium-browser"
+package_version="37.0.2062.120"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path="http://gsdview.appspot.com/chromium-browser-official"
+
+debian_version="${package_version}-4"
+debian_patch=""
+debian_diff=""
+
+archive="enable"
+
+wheezy_version="~bpo70+20141006+1"
+jessie_version="~20141008+1"
