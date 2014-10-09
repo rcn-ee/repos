c9a5ea1e540697711fd6aa16f34e80ee10eb388d
Robert Nelson <robertcnelson@gmail.com>
2014-10-09 08:40:37 -0500

    cmst: bump to ed6d04fd65672c594fa867fb2a7dfe46240e118c
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cmst/version.sh
+++ cmst/version.sh
@@ -2,12 +2,12 @@
 
 package_name="cmst"
 debian_pkg_name="${package_name}"
-package_version="2014.10.05-git20141007"
+package_version="2014.10.05-git20141008"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/andrew-bibb/cmst.git"
-git_sha="44ed6cf6f9ae74b802e11b2cb4300c5b912c1657"
+git_sha="ed6d04fd65672c594fa867fb2a7dfe46240e118c"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
@@ -16,4 +16,4 @@ debian_patch=""
 debian_diff=""
 
 wheezy_version="~bpo70+20141006+1"
-jessie_version="~20141008+1"
+jessie_version="~20141009+1"
