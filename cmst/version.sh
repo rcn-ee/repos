3f9d79375e0fa1701bd34f6ddf754479b20beae4
Robert Nelson <robertcnelson@gmail.com>
2014-10-10 07:21:32 -0500

    cmst/chrome bump
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cmst/version.sh
+++ cmst/version.sh
@@ -2,12 +2,12 @@
 
 package_name="cmst"
 debian_pkg_name="${package_name}"
-package_version="2014.10.05-git20141008"
+package_version="2014.10.05-git20141009"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/andrew-bibb/cmst.git"
-git_sha="ed6d04fd65672c594fa867fb2a7dfe46240e118c"
+git_sha="68cdaa073e45df9902da9f06dcdf615199a0eb11"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
@@ -16,4 +16,4 @@ debian_patch=""
 debian_diff=""
 
 wheezy_version="~bpo70+20141006+1"
-jessie_version="~20141009+1"
+jessie_version="~20141010+1"
