3f48b196de7acaa1cbe836968eab282b4fe97ffe
Robert Nelson <robertcnelson@gmail.com>
2014-10-13 08:36:17 -0500

    chromium-browser: 38.0.2125.101
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- chromium-browser/version.sh
+++ chromium-browser/version.sh
@@ -4,7 +4,7 @@
 
 package_name="chromium"
 debian_pkg_name="chromium-browser"
-package_version="37.0.2062.120"
+package_version="38.0.2125.101"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,11 +13,11 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="http://gsdview.appspot.com/chromium-browser-official"
 
-debian_version="${package_version}-4"
+debian_version="${package_version}-1"
 debian_patch=""
 debian_diff=""
 
 archive="enable"
 
 wheezy_version="~bpo70+20141006+1"
-jessie_version="~20141010+2"
+jessie_version="~20141013+1"
