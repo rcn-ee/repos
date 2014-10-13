d31f3c8755a0f9b5c9973fbc913cd9c63f9acb27
Robert Nelson <robertcnelson@gmail.com>
2014-10-13 11:30:44 -0500

    chromium-browser: back to gcc-4.9
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- chromium-browser/version.sh
+++ chromium-browser/version.sh
@@ -13,11 +13,11 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="http://gsdview.appspot.com/chromium-browser-official"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_patch=""
 debian_diff=""
 
 archive="enable"
 
 wheezy_version="~bpo70+20141006+1"
-jessie_version="~20141013+1"
+jessie_version="~20141013+3"
