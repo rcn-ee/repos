a4486c013649647f6fdc3cf1fc1810c0c721354f
Robert Nelson <robertcnelson@gmail.com>
2014-10-15 14:01:21 -0500

    cmst (2014.10.05-git20141014-1~20141015+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cmst/version.sh
+++ cmst/version.sh
@@ -2,12 +2,12 @@
 
 package_name="cmst"
 debian_pkg_name="${package_name}"
-package_version="2014.10.05-git20141009"
+package_version="2014.10.05-git20141014"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/andrew-bibb/cmst.git"
-git_sha="68cdaa073e45df9902da9f06dcdf615199a0eb11"
+git_sha="ce66bf892bfd0aca3ec3dc791998945284b2e982"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
@@ -16,4 +16,4 @@ debian_patch=""
 debian_diff=""
 
 wheezy_version="~bpo70+20141006+1"
-jessie_version="~20141010+1"
+jessie_version="~20141015+1"
