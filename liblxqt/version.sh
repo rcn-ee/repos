fef387926ec2fd70488bd114417898fea0d345ca
Robert Nelson <robertcnelson@gmail.com>
2014-10-15 14:34:30 -0500

    liblxqt (0.8.0-1~20141015+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- liblxqt/version.sh
+++ liblxqt/version.sh
@@ -6,8 +6,8 @@ mirror="http://packages.siduction.org"
 
 package_name="liblxqt"
 debian_pkg_name="${package_name}"
-package_version="0.7.96"
-package_source="${package_name}_${package_version}.orig.tar.bz2"
+package_version="0.8.0"
+package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir=""
 
 git_repo=""
@@ -15,9 +15,9 @@ git_sha=""
 reprepro_dir="libl/${package_name}"
 dl_path="${mirror}/lxqt/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
 wheezy_version="~bpo70+20140924+1"
-jessie_version="~20141001+1"
+jessie_version="~20141015+1"
