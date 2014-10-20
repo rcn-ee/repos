32dc68b7048e347c14b6766f81ba8b33ec78e472
Robert Nelson <robertcnelson@gmail.com>
2014-10-20 11:23:28 -0500

    lxqt-session (0.8.0-1~20141020+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- lxqt-session/version.sh
+++ lxqt-session/version.sh
@@ -6,9 +6,9 @@ mirror="http://packages.siduction.org"
 
 package_name="lxqt-session"
 debian_pkg_name="${package_name}"
-package_version="0.7.96"
-package_source="${package_name}_${package_version}.orig.tar.bz2"
-src_dir=""
+package_version="0.8.0"
+package_source="${package_name}_${package_version}.orig.tar.gz"
+src_dir="${package_name}-${package_version}"
 
 git_repo=""
 git_sha=""
@@ -20,4 +20,4 @@ debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
 wheezy_version="~bpo70+20140924+1"
-jessie_version="~20140925+1"
+jessie_version="~20141020+1"
