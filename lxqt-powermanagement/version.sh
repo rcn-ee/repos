3d10c94bb212bbb93a2d17d07649cc590d442cf1
Robert Nelson <robertcnelson@gmail.com>
2014-10-20 12:43:56 -0500

    lxqt-powermanagement (0.8.0-1~20141020+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- lxqt-powermanagement/version.sh
+++ lxqt-powermanagement/version.sh
@@ -6,18 +6,18 @@ mirror="http://packages.siduction.org"
 
 package_name="lxqt-powermanagement"
 debian_pkg_name="${package_name}"
-package_version="0.7.96"
-package_source="${package_name}_${package_version}.orig.tar.bz2"
-src_dir="tmp4FywJ8"
+package_version="0.8.0"
+package_source="${package_name}_${package_version}.orig.tar.gz"
+src_dir="${package_name}-${package_version}"
 
 git_repo=""
 git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="${mirror}/lxqt/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
 wheezy_version="~bpo70+20140924+1"
-jessie_version="~20140925+1"
+jessie_version="~20141020+1"
