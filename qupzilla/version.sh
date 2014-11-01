de46f393a63e7671c525e215f2cae730c7306ca5
Robert Nelson <robertcnelson@gmail.com>
2014-11-01 12:31:26 -0500

    qupzilla (1.8.3-git20141031-1~20141101+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- qupzilla/version.sh
+++ qupzilla/version.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 
 #https://launchpad.net/~nowrep/+archive/ubuntu/qupzilla/+packages
+#https://github.com/QupZilla/qupzilla/commits/v1.8
 
 package_name="qupzilla"
 debian_pkg_name="${package_name}"
 #package_version="1.8.3"
-package_version="1.8.3-git20141028"
+package_version="1.8.3-git20141031"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/QupZilla/qupzilla"
-git_sha="44b5146e8c32813bf690b5d98c827856f5e65368"
+git_sha="8888e9d29ab58d0098858ac3f096fdebdfa9584d"
 reprepro_dir="q/${package_name}"
 dl_path=""
 
@@ -19,4 +20,4 @@ debian_patch=""
 debian_diff=""
 
 wheezy_version="~bpo70+20141006+1"
-jessie_version="~20141029+1"
+jessie_version="~20141101+1"
