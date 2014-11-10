c4d72988b9d78dcb300b76a90ed1249058c78560
Robert Nelson <robertcnelson@gmail.com>
2014-11-10 15:33:03 -0600

    cmst: test build
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cmst/version.sh
+++ cmst/version.sh
@@ -5,11 +5,13 @@
 package_name="cmst"
 debian_pkg_name="${package_name}"
 package_version="2014.10.15-git20141015"
+#package_version="2014.11.09-git20141109"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/andrew-bibb/cmst.git"
 git_sha="da5f19592b0ccadc670964569552d532a549cb0e"
+#git_sha="abe867b0ea597e44238e93856c3e84bd94a1314d"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
