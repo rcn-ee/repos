091c9af8b5acc75a013f6b2910e4e95d13286888
Robert Nelson <robertcnelson@gmail.com>
2014-11-12 10:11:37 -0600

    xf86-video-omap (0.4.3-git20130328-1~20141112+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- xf86-video-omap/version.sh
+++ xf86-video-omap/version.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
-mirror="http://ftp.de.debian.org/debian"
+#http://cgit.freedesktop.org/xorg/driver/xf86-video-omap/
 
 package_name="xf86-video-omap"
 debian_pkg_name="${package_name}"
-package_version="0.4.3"
-package_source="${package_name}_${package_version}.orig.tar.gz"
-src_dir=""
+package_version="0.4.3-git20130328"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
 
-git_repo=""
-git_sha=""
+git_repo="git://anongit.freedesktop.org/xorg/driver/xf86-video-omap"
+git_sha="d48d09cf60437f04af46758567be6449ab3bfbe5"
 reprepro_dir="x/${package_name}"
-dl_path="${mirror}/pool/main/${reprepro_dir}/"
+dl_path=""
 
 debian_version="${package_version}-1"
-debian_untar="${package_name}_${debian_version}.debian.tar.gz"
 debian_patch=""
+debian_diff=""
 
-wheezy_version="~bpo70+20140924+1"
-jessie_version="~20140924+1"
+wheezy_version="~bpo70+20141112+1"
+jessie_version="~20141112+1"
