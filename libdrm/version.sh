d5c07cd6c4013c7977a5fc6c67f17644c1e0debf
Robert Nelson <robertcnelson@gmail.com>
2014-11-12 08:41:50 -0600

    libdrm (2.4.58-2~bpo70+20141112+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- libdrm/version.sh
+++ libdrm/version.sh
@@ -4,7 +4,7 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="libdrm"
 debian_pkg_name="${package_name}"
-package_version="2.4.56"
+package_version="2.4.58"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,9 +13,9 @@ git_sha=""
 reprepro_dir="libd/${package_name}"
 dl_path="${mirror}/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-2"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
 
-wheezy_version="~bpo70+20140924+1"
 jessie_version="~20140924+1"
+wheezy_version="~bpo70+20141112+1"
