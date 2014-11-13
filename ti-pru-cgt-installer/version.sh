29dec34b7246ab187cdde0a8e69e713d412f7aec
Robert Nelson <robertcnelson@gmail.com>
2014-11-13 10:21:25 -0600

    initial: ti-pru-cgt-installer
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-pru-cgt-installer/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+#http://software-dl.ti.com/codegen/non-esd/downloads/download.htm#PRU
+
+package_name="ti-pru-cgt-installer"
+debian_pkg_name="${package_name}"
+package_version="2.1.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="${mirror}/pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar=""
+debian_patch=""
+
+wheezy_version="~bpo70+20141113+1"
+jessie_version="~20141113+1"
