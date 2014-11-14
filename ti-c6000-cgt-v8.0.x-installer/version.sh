cf35ba82663ef70bb82c48e9aec90247a4b594d2
Robert Nelson <robertcnelson@gmail.com>
2014-11-13 19:51:35 -0600

    add: ti-c6000-cgt-v8.0.x-installer
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-c6000-cgt-v8.0.x-installer/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+#http://software-dl.ti.com/codegen/non-esd/downloads/download.htm#C6000
+
+package_name="ti-c6000-cgt-v8.0.x-installer"
+debian_pkg_name="${package_name}"
+package_version="8.0.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="${mirror}/pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar=""
+debian_patch=""
+
+wheezy_version="~bpo70+20141113+1"
+jessie_version="~20141113+1"
