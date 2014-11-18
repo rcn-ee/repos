fe6632f7cde9f3aa35c8a22cccc4ac8b113bdf05
Robert Nelson <robertcnelson@gmail.com>
2014-11-18 09:08:27 -0600

    Revert "qupzilla (1.8.4-git20141118-1~20141118+1)"
    This reverts commit 6f905c622b3cd6244f4b2ddd0da0d24c308b9e4f.

--- qupzilla/version.sh
+++ qupzilla/version.sh
@@ -6,12 +6,12 @@
 package_name="qupzilla"
 debian_pkg_name="${package_name}"
 #package_version="1.8.4"
-package_version="1.8.4-git20141118"
+package_version="1.8.4-git20141116"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/QupZilla/qupzilla"
-git_sha="b1c12998c7db6664193d4e55d0e76d4b5d562fa1"
+git_sha="b2446bb83d5507b8f0bbf84d1ed2517f0d771c17"
 reprepro_dir="q/${package_name}"
 dl_path=""
 
@@ -20,4 +20,4 @@ debian_patch=""
 debian_diff=""
 
 wheezy_version="~bpo70+20141006+1"
-jessie_version="~20141118+1"
+jessie_version="~20141116+1"
