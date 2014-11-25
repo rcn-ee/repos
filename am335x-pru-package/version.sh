f7eec856ff7a16879e568bceac4472366f79480d
Robert Nelson <robertcnelson@gmail.com>
2014-11-25 08:37:19 -0600

    am335x-pru-package (0.20141125-1~20141125+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- am335x-pru-package/version.sh
+++ am335x-pru-package/version.sh
@@ -2,12 +2,12 @@
 
 package_name="am335x-pru-package"
 debian_pkg_name="${package_name}"
-package_version="0.20140817"
+package_version="0.20141125"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/beagleboard/am335x_pru_package"
-git_sha="506e074859891a2b350eb4f5fcb451c4961410ea"
+git_sha="2c5b1849df51c3f0e0d79eb7bf16fa371bdc899c"
 reprepro_dir="a/${package_name}"
 dl_path=""
 
@@ -15,5 +15,5 @@ debian_version="${package_version}-1"
 debian_patch=""
 debian_diff=""
 
-wheezy_version="~bpo70+20141006+1"
-jessie_version="~20141006+1"
+wheezy_version="~bpo70+20141125+1"
+jessie_version="~20141125+1"
