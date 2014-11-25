ef1867d8ace09f0510b7f80cb0e67fe6d703bbaf
Robert Nelson <robertcnelson@gmail.com>
2014-11-25 10:15:58 -0600

    libv8-3.14 (3.14.5.8-8~bpo70+20141125+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ libv8-3.14/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+mirror="http://ftp.de.debian.org/debian"
+
+package_name="libv8-3.14"
+debian_pkg_name="${package_name}"
+package_version="3.14.5.8"
+package_source="${package_name}_${package_version}.orig.tar.gz"
+src_dir="v8-6b10fef46edfb4dc2a7aed389d75574c40a14243"
+
+git_repo=""
+git_sha=""
+reprepro_dir="libv/${package_name}"
+dl_path="${mirror}/pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-8"
+debian_untar="libv8-3.14_3.14.5.8-8~bpo70+1.debian.tar.gz"
+debian_patch=""
+
+jessie_version="~20141124+1"
+wheezy_version="~bpo70+20141125+1"
