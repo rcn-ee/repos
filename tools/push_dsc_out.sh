2cbfab52585d993d3d704e31df1611345ae2dce0
Robert Nelson <robertcnelson@gmail.com>
2014-12-01 10:38:49 -0600

    suite/dist
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/push_dsc_out.sh
+++ tools/push_dsc_out.sh
@@ -3,46 +3,55 @@
 . version.sh
 
 run () {
-	if [ ! -d /mnt/farm/incoming/${dist}/ ] ; then
-		mkdir -p /mnt/farm/incoming/${dist}/
+	if [ ! -d /mnt/farm/incoming/${suite}/ ] ; then
+		mkdir -p /mnt/farm/incoming/${suite}/
 	fi
 
-	if [ -f ${dist}/${package_source} ] ; then
-		cp -v ${dist}/${package_source} /mnt/farm/incoming/${dist}/
+	if [ -f ${suite}/${package_source} ] ; then
+		cp -v ${suite}/${package_source} /mnt/farm/incoming/${suite}/
 	fi
 
-	if [ -f ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.dsc ] ; then
-		cp -v ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.dsc /mnt/farm/incoming/${dist}/
-	elif [ -f ${dist}/${package_name}_${package_version}${rcn_ee_version}.dsc ] ; then
-		cp -v ${dist}/${package_name}_${package_version}${rcn_ee_version}.dsc /mnt/farm/incoming/${dist}/
+	if [ -f ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.dsc ] ; then
+		cp -v ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.dsc /mnt/farm/incoming/${suite}/
+	elif [ -f ${suite}/${package_name}_${package_version}${rcn_ee_version}.dsc ] ; then
+		cp -v ${suite}/${package_name}_${package_version}${rcn_ee_version}.dsc /mnt/farm/incoming/${suite}/
 	fi
 
-	if [ -f ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.debian.tar.xz ] ; then
-		cp -v ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.debian.tar.xz /mnt/farm/incoming/${dist}/
-	elif [ -f ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.debian.tar.gz ] ; then
-		cp -v ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.debian.tar.gz /mnt/farm/incoming/${dist}/
-	elif [ -f ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.tar.gz ] ; then
-		cp -v ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.tar.gz /mnt/farm/incoming/${dist}/
-	elif [ -f ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.diff.gz ] ; then
-		cp -v ${dist}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.diff.gz /mnt/farm/incoming/${dist}/
+	if [ -f ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.debian.tar.xz ] ; then
+		cp -v ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.debian.tar.xz /mnt/farm/incoming/${suite}/
+	elif [ -f ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.debian.tar.gz ] ; then
+		cp -v ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.debian.tar.gz /mnt/farm/incoming/${suite}/
+	elif [ -f ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.tar.gz ] ; then
+		cp -v ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.tar.gz /mnt/farm/incoming/${suite}/
+	elif [ -f ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.diff.gz ] ; then
+		cp -v ${suite}/${debian_pkg_name}_${debian_version}${rcn_ee_version}.diff.gz /mnt/farm/incoming/${suite}/
 	fi
 
-	if [ -f ${dist}/${package_name}_${package_version}${rcn_ee_version}.tar.xz ] ; then
-		cp -v ${dist}/${package_name}_${package_version}${rcn_ee_version}.tar.xz /mnt/farm/incoming/${dist}/
-	elif [ -f ${dist}/${package_name}_${package_version}${rcn_ee_version}.tar.bz2 ] ; then
-		cp -v ${dist}/${package_name}_${package_version}${rcn_ee_version}.tar.bz2 /mnt/farm/incoming/${dist}/
+	if [ -f ${suite}/${package_name}_${package_version}${rcn_ee_version}.tar.xz ] ; then
+		cp -v ${suite}/${package_name}_${package_version}${rcn_ee_version}.tar.xz /mnt/farm/incoming/${suite}/
+	elif [ -f ${suite}/${package_name}_${package_version}${rcn_ee_version}.tar.bz2 ] ; then
+		cp -v ${suite}/${package_name}_${package_version}${rcn_ee_version}.tar.bz2 /mnt/farm/incoming/${suite}/
 	fi
 }
 
-dist="wheezy"
-if [ -d debian/${dist}/ ] ; then
+dist="debian"
+suite="wheezy"
+if [ -d ${dist}/${suite}/ ] ; then
 	rcn_ee_version="${wheezy_version}"
 	run
 fi
 
-dist="jessie"
-if [ -d debian/${dist}/ ] ; then
+dist="debian"
+suite="jessie"
+if [ -d ${dist}/${suite}/ ] ; then
 	rcn_ee_version="${jessie_version}"
+	run
+fi
+
+dist="ubuntu"
+suite="trust"
+if [ -d ${dist}/${suite}/ ] ; then
+	rcn_ee_version="${trusty_version}"
 	run
 fi
 
