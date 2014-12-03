dcfd838cd2474a1c5b9b24f470c3c569d875fbbe
Robert Nelson <robertcnelson@gmail.com>
2014-12-03 12:36:46 -0600

    xinput-calibrator (0.7.5+git20140213-1~20141203+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ xinput-calibrator/version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+package_name="xinput-calibrator"
+debian_pkg_name="${package_name}"
+package_version="0.7.5+git20140213"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="https://github.com/tias/xinput_calibrator"
+git_sha="03dadf55109bd43d3380f040debe9f82f66f2f35"
+reprepro_dir="x/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-1"
+debian_patch=""
+debian_diff=""
+
+wheezy_version="~bpo70+20141203+1"
+jessie_version="~20141203+1"
