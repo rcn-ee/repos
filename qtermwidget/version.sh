47d21ebdad1c75c1b7c61b28136edc0ff9b304e8
Robert Nelson <robertcnelson@gmail.com>
2014-12-03 10:35:51 -0600

    qtermwidget (0.6.0-git20141202-1~20141203+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- qtermwidget/version.sh
+++ qtermwidget/version.sh
@@ -2,12 +2,12 @@
 
 package_name="qtermwidget"
 debian_pkg_name="${package_name}"
-package_version="0.6.0"
+package_version="0.6.0-git20141202"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/qterminal/qtermwidget"
-git_sha="a8aad8edf6cded627258b0deac81c28074ad444f"
+git_sha="3ce99c7450fa85294a29322267b18a31d21f9886"
 reprepro_dir="libq/${package_name}"
 dl_path=""
 
@@ -16,4 +16,4 @@ debian_patch=""
 debian_diff=""
 
 wheezy_version="~bpo70+20140924+1"
-jessie_version="~20141027+1"
+jessie_version="~20141203+1"
