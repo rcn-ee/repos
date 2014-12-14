882a494235af25261dd19e51fa651266b65d554b
Robert Nelson <robertcnelson@gmail.com>
2014-12-14 15:32:26 -0600

    python-pyftpdlib fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- python-pyftpdlib/version.sh
+++ python-pyftpdlib/version.sh
@@ -6,7 +6,7 @@ package_name="python-pyftpdlib"
 debian_pkg_name="${package_name}"
 package_version="1.2.0"
 package_source="${package_name}_${package_version}.orig.tar.gz"
-src_dir="${package_name}-${package_version}"
+src_dir="pyftpdlib-${package_version}"
 
 git_repo=""
 git_sha=""
