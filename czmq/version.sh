e851d59482b40f67076bcc8193fa6f256db47f06
Robert Nelson <robertcnelson@gmail.com>
2014-12-22 09:47:39 -0600

    czmq (3.0.0~rc1+git20141217-1~20141222+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ czmq/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+mirror="http://ftp.de.debian.org/debian"
+
+package_name="czmq"
+debian_pkg_name="${package_name}"
+package_version="3.0.0~rc1+git20141217"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="https://github.com/zeromq/czmq"
+git_sha="869c3145b2ae60802f6239b9615b2bcd8b715d96"
+reprepro_dir="c/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-1"
+debian_untar=""
+debian_patch=""
+
+trusty_version="~20141222+1"
+wheezy_version="~bpo70+20141222+1"
+jessie_version="~20141222+1"
