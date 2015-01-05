8e1ea67f8a0a935317d9f463a63a7044e82d5137
Robert Nelson <robertcnelson@gmail.com>
2015-01-05 09:50:28 -0600

    cloud9-installer (3.0.645-1~20150105+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cloud9-installer/version.sh
+++ cloud9-installer/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-#https://rcn-ee.net/pkgs/c9v3/c9v3_3.0.1-git20140211-build.tar.xz
+#https://rcn-ee.net/pkgs/c9v3/
 
 package_name="cloud9-installer"
 debian_pkg_name="${package_name}"
-package_version="3.0.642"
+package_version="3.0.645"
 package_source=""
 src_dir=""
 
@@ -17,6 +17,6 @@ debian_version="${package_version}-1"
 debian_untar=""
 debian_patch=""
 
-wheezy_version="~bpo70+20141229+1"
-jessie_version="~20141229+1"
-trusty_version="~20141229+1"
+wheezy_version="~bpo70+20150105+1"
+jessie_version="~20150105+1"
+trusty_version="~20150105+1"
