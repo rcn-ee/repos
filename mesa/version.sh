bbbbfa691125b376245ef53e862875b40659db05
Robert Nelson <robertcnelson@gmail.com>
2015-01-13 14:18:11 -0600

    mesa (10.4.2-1~bpo80+20150113+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mesa/version.sh
+++ mesa/version.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
-#mirror="http://ftp.de.debian.org/debian"
+mirror="http://ftp.de.debian.org/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="10.3.3-git20141119"
-package_source="${package_name}_${package_version}.orig.tar.xz"
-src_dir="${package_name}_${package_version}"
+package_version="10.4.2"
+package_source="${package_name}_${package_version}.orig.tar.gz"
+src_dir="Mesa-${package_version}"
 
-git_repo="git://anongit.freedesktop.org/mesa/mesa"
-git_sha="b43792f67947055fae3131959cbb04ad83ec5df3"
+git_repo=""
+git_sha=""
 reprepro_dir="m/${package_name}"
-dl_path=""
+dl_path="${mirror}/pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1"
-debian_patch=""
-debian_diff=""
+debian_untar=""
+debian_patch="${package_name}_${debian_version}.diff.gz"
 
 wheezy_version="~bpo70+20141112+1"
-jessie_version="~20141119+1"
+jessie_version="~bpo80+20150113+1"
