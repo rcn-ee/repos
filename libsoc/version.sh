2b2780a4e22d70d6e96c469f86e5e12c30e73618
Robert Nelson <robertcnelson@gmail.com>
2015-01-19 12:28:51 -0600

    libsoc (0.6.3-git20141011-1~20150119+1) + pkgconfig
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- libsoc/version.sh
+++ libsoc/version.sh
@@ -2,12 +2,12 @@
 
 package_name="libsoc"
 debian_pkg_name="${package_name}"
-package_version="0.6.3"
+package_version="0.6.3-git20141011"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
-git_repo="https://github.com/jackmitch/libsoc/"
-git_sha="df9b256337790a5cefe0aa4d7bc11225612098dd"
+git_repo="https://github.com/jackmitch/libsoc.git"
+git_sha="872e6c4b4c44ab5574a440f421a32cc75c46e85f"
 reprepro_dir="libs/${package_name}"
 dl_path=""
 
@@ -15,5 +15,6 @@ debian_version="${package_version}-1"
 debian_patch=""
 debian_diff=""
 
-wheezy_version="~bpo70+20141006+1"
-jessie_version="~20141006+1"
+wheezy_version="~bpo70+20150119+1"
+jessie_version="~20150119+1"
+trusty_version="~20150119+1"
