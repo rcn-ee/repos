40fcf51eaeb67e8fb0929f0fae5a767cbcafebb8
Robert Nelson <robertcnelson@gmail.com>
2015-01-20 20:32:31 -0600

    dart-edge (1.9.0-edge.43033-1~20150120+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- dart-edge/version.sh
+++ dart-edge/version.sh
@@ -6,7 +6,7 @@ package_name="dart-edge"
 debian_pkg_name="${package_name}"
 #repo_branch="1.7"
 repo_branch="bleeding_edge"
-package_version="1.9.0-edge.42796"
+package_version="1.9.0-edge.43033"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="dart-${package_version}"
 
@@ -21,6 +21,6 @@ debian_diff=""
 
 archive="enable"
 
-wheezy_version="~bpo70+20150112+1"
-jessie_version="~20150112+1"
-trusty_version="~20150112+1"
+wheezy_version="~bpo70+20150120+1"
+jessie_version="~20150120+1"
+trusty_version="~20150120+1"
