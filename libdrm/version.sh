c7da7660c1fe9fb5410de60b9bd4850469b6c5b3
Robert Nelson <robertcnelson@gmail.com>
2015-01-23 12:35:37 -0600

    revert this change libdrm
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- libdrm/version.sh
+++ libdrm/version.sh
@@ -4,7 +4,7 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="libdrm"
 debian_pkg_name="${package_name}"
-package_version="2.4.59"
+package_version="2.4.58"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,7 +13,7 @@ git_sha=""
 reprepro_dir="libd/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-2etnaviv"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
 
