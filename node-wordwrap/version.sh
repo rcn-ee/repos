74bf761ad34abee1c230cef9a248f9ce526c91b7
Robert Nelson <robertcnelson@gmail.com>
2015-01-23 13:39:42 -0600

    node-wordwrap, fix dir
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- node-wordwrap/version.sh
+++ node-wordwrap/version.sh
@@ -6,7 +6,7 @@ package_name="node-wordwrap"
 debian_pkg_name="${package_name}"
 package_version="0.0.2"
 package_source="${package_name}_${package_version}.orig.tar.gz"
-src_dir="${package_name}-${package_version}"
+src_dir="package"
 
 git_repo=""
 git_sha=""
