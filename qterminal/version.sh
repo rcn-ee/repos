e52f57f02641c8434163c9873d5712dfaefd425a
Robert Nelson <robertcnelson@gmail.com>
2015-01-30 16:53:57 -0600

    qterminal: fix version
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- qterminal/version.sh
+++ qterminal/version.sh
@@ -16,7 +16,7 @@ git_sha=""
 reprepro_dir="q/${package_name}"
 dl_path="lxqt/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-4"
+debian_version="${package_version}-6"
 debian_patch=""
 
 jessie_version="~20150130+1"
