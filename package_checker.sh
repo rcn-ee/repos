c20d9814a99bbd6246231a073d0214c62c3e7595
Robert Nelson <robertcnelson@gmail.com>
2015-02-11 12:08:58 -0600

    qtermwidget (0.6.0-5~20150211+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -40,7 +40,7 @@ package_name="connman" ; package_version="connman_1.27~20150123g04cbd7e-2" ; che
 package_name="cmst" ; package_version="cmst_2015.02.01-1" ; check_http
 
 site="http://packages.siduction.org/lxqt/pool/main/q"
-package_name="qtermwidget" ; package_version="qtermwidget_0.6.0-4" ; check_http
+package_name="qtermwidget" ; package_version="qtermwidget_0.6.0-5" ; check_http
 package_name="qterminal" ; package_version="qterminal_0.6.0-6" ; check_http
 
 #really slow...
--- qtermwidget/version.sh
+++ qtermwidget/version.sh
@@ -7,7 +7,7 @@ mirror="http://packages.siduction.org"
 package_name="qtermwidget"
 debian_pkg_name="${package_name}"
 package_version="0.6.0"
-dl_package_source="${package_name}_${package_version}-4.tar.gz"
+dl_package_source="${package_name}_${package_version}-5.tar.gz"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="checkout"
 
@@ -16,7 +16,7 @@ git_sha=""
 reprepro_dir="q/${package_name}"
 dl_path="lxqt/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-4.2"
+debian_version="${package_version}-5"
 debian_patch=""
 
-jessie_version="~20150130+1"
+jessie_version="~20150211+1"
