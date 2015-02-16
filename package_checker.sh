50b9dbc5a7fe4729b6c27fb0cd6fcdeb89e2bf29
Robert Nelson <robertcnelson@gmail.com>
2015-02-16 10:23:21 -0600

    lxqt-session (0.9.0-1~20150216+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -70,6 +70,9 @@ package_name="lxqt-panel" ; package_version="${package_name}_0.9.0" ; check_http
 site="http://packages.siduction.org/lxqt/pool/main/p"
 package_name="pcmanfm-qt" ; package_version="${package_name}_0.9.0-1" ; check_http
 
+site="http://packages.siduction.org/lxqt/pool/main/l"
+package_name="lxqt-session" ; package_version="${package_name}_0.9.0-1" ; check_http
+
 #really slow...
 #exit
 
