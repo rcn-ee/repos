cfe812056a3496c8960c91d7c84d907132023d40
Robert Nelson <robertcnelson@gmail.com>
2015-02-16 12:24:45 -0600

    fix; dir
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- polkit-qt-1/version.sh
+++ polkit-qt-1/version.sh
@@ -9,7 +9,7 @@ debian_pkg_name="${package_name}"
 package_version="0.112.0"
 dl_package_source="${package_name}_${package_version}-2.tar.gz"
 package_source="${package_name}_${package_version}.orig.tar.gz"
-src_dir="checkout"
+src_dir="polkit-qt-1"
 
 git_repo=""
 git_sha=""
