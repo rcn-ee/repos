73c8959d6d6ef31b27b41969e5cbc6ab881226ce
Robert Nelson <robertcnelson@gmail.com>
2015-02-26 16:31:45 -0600

    and tar files
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/build_deb.sh
+++ tools/build_deb.sh
@@ -28,6 +28,7 @@ run () {
 			mkdir -p ${out_dir}/
 			cp -v *orig* ${out_dir}/ || true
 			cp -v *debian* ${out_dir}/ || true
+			cp -v *tar* ${out_dir}/ || true
 			cp -v *.changes ${out_dir}/ || true
 			cp -v *.deb ${out_dir}/ || true
 			cp -v *.dsc ${out_dir}/ || true
