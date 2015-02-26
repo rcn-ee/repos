ca696c5e50435d3a15b9fd624cc782fe4b9c1aa1
Robert Nelson <robertcnelson@gmail.com>
2015-02-26 16:07:38 -0600

    libdrm: fixup
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- libdrm/version.sh
+++ libdrm/version.sh
@@ -6,7 +6,7 @@ package_name="libdrm"
 debian_pkg_name="${package_name}"
 package_version="2.4.59"
 package_source="${package_name}_${package_version}.orig.tar.xz"
-src_dir="${package_name}-${package_version}"
+src_dir="${package_name}_${package_version}"
 
 git_repo="git://anongit.freedesktop.org/mesa/drm"
 git_sha="d2e0f552467ca11162fe04dfed55de4590280f23"
