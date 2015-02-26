bef4f5339009841cf7f07f9eabe73f4f9411b495
Robert Nelson <robertcnelson@gmail.com>
2015-02-26 16:00:03 -0600

    libdrm (2.4.59-0etnaviv~20150226+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- libdrm/version.sh
+++ libdrm/version.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 
-mirror="http://ftp.de.debian.org/debian"
+#mirror="http://ftp.de.debian.org/debian"
 
 package_name="libdrm"
 debian_pkg_name="${package_name}"
-package_version="2.4.58"
-package_source="${package_name}_${package_version}.orig.tar.gz"
+package_version="2.4.59"
+package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
-git_repo=""
-git_sha=""
+git_repo="git://anongit.freedesktop.org/mesa/drm"
+git_sha="d2e0f552467ca11162fe04dfed55de4590280f23"
 reprepro_dir="libd/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2etnaviv"
+debian_version="${package_version}-0etnaviv"
 debian_untar=""
-debian_patch="${package_name}_${debian_version}.diff.gz"
+#debian_patch="${package_name}_${debian_version}.diff.gz"
 
-wheezy_version="~bpo70+20150106+1"
-jessie_version="~20150106+1"
-trusty_version="~20150106+1"
+wheezy_version="~bpo70+20150226+1"
+jessie_version="~20150226+1"
+trusty_version="~20150226+1"
