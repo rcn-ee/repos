b2ca45529a5998dc479ed5e16fad34902917c847
Robert Nelson <robertcnelson@gmail.com>
2015-02-27 10:22:48 -0600

    chrome update
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -105,7 +105,7 @@ package_name="extra-cmake-modules" ; package_version="1.6.1-1" ; check
 package_name="policykit-1" ; package_version="0.112-4" ; check
 
 suite="sid"
-package_name="chromium" ; package_version="39.0.2171.71-2" ; check
+package_name="chromium" ; package_version="40.0.2214.111-1" ; check
 package_name="libdrm"; package_version="2.4.58-2"; check
 package_name="llvm-toolchain-3.6"; package_version="1:3.6~+rc4-1"; check
 package_name="mesa"; package_version="10.4.2-2"; check
