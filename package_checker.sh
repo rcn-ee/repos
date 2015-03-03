fb8605d4986f2210b88b4342b45e1af17c42a209
Robert Nelson <robertcnelson@gmail.com>
2015-03-03 10:36:58 -0600

    lxqt-notificationd (0.9.0-2~20150303+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -66,7 +66,7 @@ package_name="liblxqt-mount" ; package_version="liblxqt-mount_0.9.0-1" ; check_h
 
 site="http://packages.siduction.org/lxqt/pool/main/l"
 package_name="lxqt-globalkeys" ; package_version="${package_name}_0.9.0-2" ; check_http
-package_name="lxqt-notificationd" ; package_version="${package_name}_0.9.0-1" ; check_http
+package_name="lxqt-notificationd" ; package_version="${package_name}_0.9.0-2" ; check_http
 
 site="http://packages.siduction.org/lxqt/pool/main/libs"
 package_name="libsysstat" ; package_version="${package_name}_0.3.0-1" ; check_http
