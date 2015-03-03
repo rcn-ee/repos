ebebaeb0d85130285d0dd61401a385de0b0c8c9f
Robert Nelson <robertcnelson@gmail.com>
2015-03-03 09:38:07 -0600

    libqtxdg: this is the same
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- libqtxdg/version.sh
+++ libqtxdg/version.sh
@@ -8,7 +8,7 @@ package_name="libqtxdg"
 debian_pkg_name="${package_name}"
 package_version="1.1.0"
 dl_package_source="${package_name}_${package_version}-4.tar.gz"
-package_source="${package_name}_${package_version}.orig.tar.gz"
+package_source="${package_name}_${package_version}-4.tar.gz"
 src_dir="checkout"
 
 git_repo=""
