c941d40878c17e47a2c83c2cfdf86f5c467e2f1f
Robert Nelson <robertcnelson@gmail.com>
2015-03-05 08:58:42 -0600

    libqtxdg (1.1.0-6~20150305+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -53,7 +53,7 @@ site="http://packages.siduction.org/lxqt/pool/main/p"
 package_name="polkit-qt-1" ; package_version="${package_name}_0.112.0-2" ; check_http
 
 site="http://packages.siduction.org/lxqt/pool/main/libq"
-package_name="libqtxdg" ; package_version="${package_name}_1.1.0-4" ; check_http
+package_name="libqtxdg" ; package_version="${package_name}_1.1.0-6" ; check_http
 
 site="http://packages.siduction.org/lxqt/pool/main/k"
 package_name="kwindowsystem" ; package_version="${package_name}_5.7.0-1" ; check_http
