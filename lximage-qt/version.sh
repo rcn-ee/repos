e45f1264e21aaa9cea891057ef5b7325d2cabc03
Robert Nelson <robertcnelson@gmail.com>
2015-03-07 12:55:26 -0600

    fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- lximage-qt/version.sh
+++ lximage-qt/version.sh
@@ -8,7 +8,7 @@ package_name="lximage-qt"
 debian_pkg_name="${package_name}"
 package_version="0.4.0"
 dl_package_source="${package_name}_${package_version}-1.tar.gz"
-package_source="${package_name}_${package_version}.orig.tar.gz"
+package_source="${dl_package_source}"
 src_dir="checkout"
 
 git_repo=""
