8bfe29da7e9b8e574c81926f19f246f9e2c114fb
Robert Nelson <robertcnelson@gmail.com>
2015-03-20 09:56:09 -0500

    tools: push to seperate dir
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/push_dsc_out.sh
+++ tools/push_dsc_out.sh
@@ -3,12 +3,12 @@
 . version.sh
 
 run () {
-	if [ ! -d /mnt/farm/incoming/${suite}/ ] ; then
-		mkdir -p /mnt/farm/incoming/${suite}/
+	if [ ! -d /mnt/farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/ ] ; then
+		mkdir -p /mnt/farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/
 	fi
 
-	cp -v ${suite}/*.dsc /mnt/farm/incoming/${suite}/
-	cp -v ${suite}/*.gz /mnt/farm/incoming/${suite}/
+	cp -v ${suite}/*.dsc /mnt/farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/
+	cp -v ${suite}/*.gz /mnt/farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/
 }
 
 dist="debian"
