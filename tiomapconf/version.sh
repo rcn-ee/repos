97cf3ec1a5a2a15956530e561be156534dd0432b
Robert Nelson <robertcnelson@gmail.com>
2015-03-26 19:44:54 -0500

    tiomapconf (1.71.0-git20150321-1~bpo80+20150326+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tiomapconf/version.sh
+++ tiomapconf/version.sh
@@ -4,12 +4,12 @@
 
 package_name="tiomapconf"
 debian_pkg_name="${package_name}"
-package_version="1.71.0-git20150203"
+package_version="1.71.0-git20150321"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/omapconf/omapconf"
-git_sha="81117e1d5230ac0d7d882ece6394517944a26e1a"
+git_sha="3bf78ee44d811e9266825ac7c3ace25cbcd13a38"
 reprepro_dir="o/${package_name}"
 dl_path=""
 
@@ -17,6 +17,8 @@ debian_version="${package_version}-1"
 debian_patch=""
 debian_diff=""
 
-wheezy_version="~bpo70+20150211+1"
-jessie_version="~20150211+1"
-trusty_version="~20150211+1"
+wheezy_version="~bpo70+20150326+1"
+jessie_version="~bpo80+20150326+1"
+trusty_version="~bpo1404+20150326+1"
+utopic_version="~bpo1410+20150326+1"
+vivid_version="~bpo1504+20150326+1"
