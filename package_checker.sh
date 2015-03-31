be926a89667d8357878ca781a08aa68788027559
Robert Nelson <robertcnelson@gmail.com>
2015-03-31 16:29:46 -0500

    lxqt-powermanagement (0.9.0-5~bpo80+20150331+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -151,7 +151,7 @@ qt5_lxqt () {
 	package_name="lxqt-policykit" ; package_version="${package_name}_0.9.0-3" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/l"
-	package_name="lxqt-powermanagement" ; package_version="${package_name}_0.9.0-3" ; check_http
+	package_name="lxqt-powermanagement" ; package_version="${package_name}_0.9.0-5" ; check_http
 	package_name="lximage-qt" ; package_version="${package_name}_0.4.0-1" ; check_http
 	package_name="lxqt-config" ; package_version="${package_name}_0.9.0-2" ; check_http
 
