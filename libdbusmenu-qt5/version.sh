70c9af62cb765ad100a5ec467d8142c8c3593ce9
Robert Nelson <robertcnelson@gmail.com>
2015-03-31 14:42:38 -0500

    libdbusmenu-qt5 (0.9.3-1siduction3~bpo80+20150331+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- libdbusmenu-qt5/version.sh
+++ libdbusmenu-qt5/version.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -e
 
-#http://packages.siduction.org/extra/pool/main/libd/libdbusmenu-qt5/libdbusmenu-qt5_0.9.3-1siduction.dsc
+#http://packages.siduction.org/extra/pool/main/libd/libdbusmenu-qt5/
 
 mirror="http://packages.siduction.org"
 
 package_name="libdbusmenu-qt5"
 debian_pkg_name="${package_name}"
 package_version="0.9.3"
-dl_package_source="${package_name}_${package_version}-1siduction.tar.gz"
-package_source="${package_name}_${package_version}.orig.tar.gz"
+package_source="${package_name}_${package_version}-1siduction3.tar.gz"
 src_dir="libdbusmenu-qt"
 
 git_repo=""
@@ -16,8 +15,7 @@ git_sha=""
 reprepro_dir="libd/${package_name}"
 dl_path="extra/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-1siduction3"
 debian_patch=""
 
-wheezy_version="~bpo70+20140924+1"
-jessie_version="~20150216+1"
+jessie_version="~bpo80+20150331+1"
