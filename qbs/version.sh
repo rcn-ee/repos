fb06ae402ff8cc476630f164546199f6edfd0279
Robert Nelson <robertcnelson@gmail.com>
2015-04-06 08:55:36 -0500

    qbs (1.3.4+dfsg-1~bpo80+20150406+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- qbs/version.sh
+++ qbs/version.sh
@@ -4,17 +4,17 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="qbs"
 debian_pkg_name="${package_name}"
-package_version="1.4~7fd456"
-package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
-src_dir=""
+package_version="1.3.4+dfsg"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
+src_dir="qbs-1.3.4"
 
 git_repo=""
 git_sha=""
 reprepro_dir="q/${debian_pkg_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2.2"
-debian_untar="${debian_pkg_name}_1.4~7fd456-1.debian.tar.xz"
+debian_version="${package_version}-1"
+debian_untar="${debian_pkg_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
-jessie_version="~bpo80+20150401+1"
+jessie_version="~bpo80+20150406+1"
