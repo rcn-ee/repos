a0e8513e5b75c484b818151d4b3e7c872065dff2
Robert Nelson <robertcnelson@gmail.com>
2015-04-07 17:02:09 -0500

    compton-conf (0.1.2-3~bpo80+20150407+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- compton-conf/version.sh
+++ compton-conf/version.sh
@@ -7,7 +7,7 @@ mirror="http://packages.siduction.org"
 package_name="compton-conf"
 debian_pkg_name="${package_name}"
 package_version="0.1.2"
-package_source="${package_name}_${package_version}-4+exp.tar.gz"
+package_source="${package_name}_${package_version}-3.tar.gz"
 src_dir="checkout"
 
 git_repo=""
@@ -15,7 +15,7 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="lxqt/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-4+exp"
+debian_version="${package_version}-3"
 debian_patch=""
 
-jessie_version="~bpo80+20150331+1"
+jessie_version="~bpo80+20150407+1"
