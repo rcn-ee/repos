eee858ed838a850e19aa2d0f8bd01fe2bcf80f39
Robert Nelson <robertcnelson@gmail.com>
2015-04-22 15:25:32 -0500

    bb-customizations (1.20150422-1~bpo80+20150422+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20150417"
+package_version="1.20150422"
 package_source=""
 src_dir=""
 
@@ -11,9 +11,10 @@ git_sha=""
 reprepro_dir="t/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1.2"
+debian_version="${package_version}-1"
 debian_untar=""
 debian_patch=""
 
 
-jessie_version="~bpo80+20150417+1"
+wheezy_version="~bpo70+20150422+1"
+jessie_version="~bpo80+20150422+1"
