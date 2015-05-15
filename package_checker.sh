16c8b6521f90d0ed58e66a7b685553d06447cc76
Robert Nelson <robertcnelson@gmail.com>
2015-05-15 10:11:49 -0500

    pcmanfm-qt (0.9.0-8+exp2~bpo80+20150515+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -145,7 +145,7 @@ qt5_lxqt () {
 	package_name="lxqt-panel" ; package_version="${package_name}_0.9.0-9a" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/p"
-	package_name="pcmanfm-qt" ; package_version="${package_name}_0.9.0-8%2bexp1" ; check_http
+	package_name="pcmanfm-qt" ; package_version="${package_name}_0.9.0-8%2bexp2" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/l"
 	package_name="lxqt-session" ; package_version="${package_name}_0.9.0-8%2bexp1" ; check_http
--- pcmanfm-qt/version.sh
+++ pcmanfm-qt/version.sh
@@ -7,7 +7,7 @@ mirror="http://packages.siduction.org"
 package_name="pcmanfm-qt"
 debian_pkg_name="${package_name}"
 package_version="0.9.0"
-package_source="${package_name}_${package_version}-8+exp1.tar.gz"
+package_source="${package_name}_${package_version}-8+exp2.tar.gz"
 src_dir="checkout"
 
 git_repo=""
@@ -15,7 +15,7 @@ git_sha=""
 reprepro_dir="p/${package_name}"
 dl_path="lxqt/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-8+exp1"
+debian_version="${package_version}-8+exp2"
 debian_patch=""
 
-jessie_version="~bpo80+20150504+1"
+jessie_version="~bpo80+20150515+1"
