14d45e2eeadd916eb6eaaa9ebfad58ff9469d90a
Robert Nelson <robertcnelson@gmail.com>
2015-05-15 10:18:57 -0500

    lxqt-session (0.9.0-9+exp1~bpo80+20150515+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- lxqt-session/version.sh
+++ lxqt-session/version.sh
@@ -7,7 +7,7 @@ mirror="http://packages.siduction.org"
 package_name="lxqt-session"
 debian_pkg_name="${package_name}"
 package_version="0.9.0"
-package_source="${package_name}_${package_version}-8+exp1.tar.gz"
+package_source="${package_name}_${package_version}-9+exp1.tar.gz"
 src_dir="checkout"
 
 git_repo=""
@@ -15,7 +15,7 @@ git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="lxqt/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-8+exp1"
+debian_version="${package_version}-9+exp1"
 debian_patch=""
 
-jessie_version="~bpo80+20150504+1"
+jessie_version="~bpo80+20150515+1"
