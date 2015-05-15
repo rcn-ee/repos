086c3d02a04984281a60c51534e2cbd17aaf56b2
Robert Nelson <robertcnelson@gmail.com>
2015-05-15 10:59:24 -0500

    lxqt-common
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -165,7 +165,7 @@ qt5_lxqt () {
 	site="http://packages.siduction.org/lxqt/pool/main/l"
 	package_name="lxqt-about" ; package_version="${package_name}_0.9.0-2" ; check_http
 	package_name="lxqt-admin" ; package_version="${package_name}_0.9.0-4" ; check_http
-	package_name="lxqt-common" ; package_version="${package_name}_0.9.1-7%2bexp1" ; check_http
+	package_name="lxqt-common" ; package_version="${package_name}_0.9.1-8%2bexp1" ; check_http
 }
 
 mesa
