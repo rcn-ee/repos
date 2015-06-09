782d0de1ae63c9e3a78a02c3e3fa2a7f740a9942
Robert Nelson <robertcnelson@gmail.com>
2015-06-09 09:55:48 -0500

    lxqt-admin (0.9.0-5~bpo80+20150609+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -189,7 +189,7 @@ qt5_lxqt () {
 
 	site="http://packages.siduction.org/lxqt/pool/main/l"
 	package_name="lxqt-about" ; package_version="${package_name}_0.9.0-4" ; check_http
-	package_name="lxqt-admin" ; package_version="${package_name}_0.9.0-4" ; check_http
+	package_name="lxqt-admin" ; package_version="${package_name}_0.9.0-5" ; check_http
 	package_name="lxqt-common" ; package_version="${package_name}_0.9.1-9%2bexp1" ; check_http
 }
 
