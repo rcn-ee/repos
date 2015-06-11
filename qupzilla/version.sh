dff3055d3cd43cace7c582c0bf87e12bc03c6f80
Robert Nelson <robertcnelson@gmail.com>
2015-06-11 11:42:37 -0500

    qupzilla (1.8.6-git20150528-1~bpo80+20150611+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- qupzilla/version.sh
+++ qupzilla/version.sh
@@ -5,12 +5,12 @@
 package_name="qupzilla"
 debian_pkg_name="${package_name}"
 #package_version="1.8.4"
-package_version="1.8.6-git20150126"
+package_version="1.8.6-git20150528"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/QupZilla/qupzilla"
-git_sha="a0de5dc24a15d4662d39014276fd2357223f5e87"
+git_sha="61ef93396bc434a0159193bf31e039d1817716f8"
 reprepro_dir="q/${package_name}"
 dl_path=""
 
@@ -18,5 +18,4 @@ debian_version="${package_version}-1"
 debian_patch=""
 debian_diff=""
 
-wheezy_version="~bpo70+20141006+1"
-jessie_version="~20150126+1"
+jessie_version="~bpo80+20150611+1"
