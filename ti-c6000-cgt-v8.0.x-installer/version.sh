5ad1ab5ae9e01a436892c8caaab77f7d12d73a8d
Robert Nelson <robertcnelson@gmail.com>
2015-06-11 10:22:42 -0500

    ti-c6000-cgt-v8.0.x-installer (8.0.3-1~bpo80+20150611+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-c6000-cgt-v8.0.x-installer/version.sh
+++ ti-c6000-cgt-v8.0.x-installer/version.sh
@@ -4,7 +4,7 @@
 
 package_name="ti-c6000-cgt-v8.0.x-installer"
 debian_pkg_name="${package_name}"
-package_version="8.0.1"
+package_version="8.0.3"
 package_source=""
 src_dir=""
 
@@ -17,6 +17,10 @@ debian_version="${package_version}-1"
 debian_untar=""
 debian_patch=""
 
-wheezy_version="~bpo70+20150128+1"
-jessie_version="~20150128+1"
-trusty_version="~20150128+1"
+wheezy_version="~bpo70+20150611+1"
+jessie_version="~bpo80+20150611+1"
+stretch_version="~bpo90+20150611+1"
+trusty_version="~bpo1404+20150611+1"
+utopic_version="~bpo1410+20150611+1"
+vivid_version="~bpo1504+20150611+1"
+wily_version="~bpo1510+20150611+1"
