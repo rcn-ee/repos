8ec7ef09bc31dbe352974037f2681c930fc7104a
Robert Nelson <robertcnelson@gmail.com>
2015-06-15 11:23:26 -0500

    lxqt-policykit (0.9.0-5~bpo80+20150615+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- lxqt-policykit/version.sh
+++ lxqt-policykit/version.sh
@@ -7,7 +7,7 @@ mirror="http://packages.siduction.org"
 package_name="lxqt-policykit"
 debian_pkg_name="${package_name}"
 package_version="0.9.0"
-package_source="${package_name}_${package_version}-4.tar.gz"
+package_source="${package_name}_${package_version}-5.tar.gz"
 src_dir="checkout"
 
 git_repo=""
@@ -15,7 +15,7 @@ git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="lxqt/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-4"
+debian_version="${package_version}-5"
 debian_patch=""
 
-jessie_version="~bpo80+20150608+1"
+jessie_version="~bpo80+20150615+1"
