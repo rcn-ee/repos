b99b908e972389c5cef206bb44cf70fad35eb951
Robert Nelson <robertcnelson@gmail.com>
2015-06-15 10:20:07 -0500

    lxqt-globalkeys (0.9.0-7~bpo80+20150615+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -163,7 +163,7 @@ qt5_lxqt () {
 	package_name="liblxqt" ; package_version="${package_name}_0.9.0-14" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/l"
-	package_name="lxqt-globalkeys" ; package_version="${package_name}_0.9.0-6%2bexp1" ; check_http
+	package_name="lxqt-globalkeys" ; package_version="${package_name}_0.9.0-7" ; check_http
 	package_name="lxqt-notificationd" ; package_version="${package_name}_0.9.0-8%2bexp1" ; check_http
 	package_name="lxqt-sudo" ; package_version="${package_name}_0.1.0-3" ; check_http
 
