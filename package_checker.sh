a2a5698d7796b2cccdbbf45c6988284dd767e30d
Robert Nelson <robertcnelson@gmail.com>
2015-06-15 11:49:28 -0500

    lxqt-about (0.9.0-5~bpo80+20150615+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -192,7 +192,7 @@ qt5_lxqt () {
 	package_name="compton-conf" ; package_version="${package_name}_0.1.2-6%2bexp1" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/l"
-	package_name="lxqt-about" ; package_version="${package_name}_0.9.0-4" ; check_http
+	package_name="lxqt-about" ; package_version="${package_name}_0.9.0-5" ; check_http
 	package_name="lxqt-admin" ; package_version="${package_name}_0.9.0-5" ; check_http
 	package_name="lxqt-common" ; package_version="${package_name}_0.9.1-9%2bexp1" ; check_http
 }
