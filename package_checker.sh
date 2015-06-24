15b38af5bec48988556b5e2ae98e4bf9262127c2
Robert Nelson <robertcnelson@gmail.com>
2015-06-24 14:55:14 -0500

    lxqt-panel (0.9.0-22~bpo80+20150624+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -170,7 +170,7 @@ qt5_lxqt () {
 	package_name="libsysstat" ; package_version="${package_name}_0.3.0-2" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/l"
-	package_name="lxqt-panel" ; package_version="${package_name}_0.9.0-20" ; check_http
+	package_name="lxqt-panel" ; package_version="${package_name}_0.9.0-22" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/p"
 	package_name="pcmanfm-qt" ; package_version="${package_name}_0.9.0-12" ; check_http
