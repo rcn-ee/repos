877c66e1b82729e04201730832a3476ee860e7de
Robert Nelson <robertcnelson@gmail.com>
2015-06-24 15:12:55 -0500

    lxqt-common (0.9.1-13~bpo80+20150624+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -193,7 +193,7 @@ qt5_lxqt () {
 	site="http://packages.siduction.org/lxqt/pool/main/l"
 	package_name="lxqt-about" ; package_version="${package_name}_0.9.0-6" ; check_http
 	package_name="lxqt-admin" ; package_version="${package_name}_0.9.0-5" ; check_http
-	package_name="lxqt-common" ; package_version="${package_name}_0.9.1-10" ; check_http
+	package_name="lxqt-common" ; package_version="${package_name}_0.9.1-13" ; check_http
 }
 
 machinekit () {
