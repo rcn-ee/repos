8f3073e7360e7f43269c57460ad05fd33bc9b8ec
Robert Nelson <robertcnelson@gmail.com>
2015-06-24 10:19:39 -0500

    fix libdbusmenu-qt
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- libdbusmenu-qt/version.sh
+++ libdbusmenu-qt/version.sh
@@ -6,7 +6,7 @@ package_name="libdbusmenu-qt"
 debian_pkg_name="${package_name}"
 package_version="0.9.3+15.10.20150604"
 package_source="${package_name}_${package_version}.orig.tar.gz"
-src_dir="${package_name}_${package_version}"
+src_dir="${package_name}-${package_version}"
 
 git_repo=""
 git_sha=""
