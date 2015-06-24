5c5afe074e6c0f134c04a0587ac676c587927728
Robert Nelson <robertcnelson@gmail.com>
2015-06-24 12:26:48 -0500

    lxqt-session (0.9.0-12~bpo80+20150624+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -179,7 +179,7 @@ qt5_lxqt () {
 	package_name="pcmanfm-qt" ; package_version="${package_name}_0.9.0-12" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/l"
-	package_name="lxqt-session" ; package_version="${package_name}_0.9.0-10" ; check_http
+	package_name="lxqt-session" ; package_version="${package_name}_0.9.0-12" ; check_http
 	package_name="lxqt-runner" ; package_version="${package_name}_0.9.0-5" ; check_http
 	package_name="lxqt-policykit" ; package_version="${package_name}_0.9.0-5" ; check_http
 	package_name="lxqt-powermanagement" ; package_version="${package_name}_0.9.0-9%2bexp1" ; check_http
