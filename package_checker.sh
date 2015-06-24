d880197f9d9a03776f5b7da003cd2ca43926f07f
Robert Nelson <robertcnelson@gmail.com>
2015-06-24 12:09:26 -0500

    lxqt-sudo (0.1.0-6~bpo80+20150624+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -167,7 +167,7 @@ qt5_lxqt () {
 	site="http://packages.siduction.org/lxqt/pool/main/l"
 	package_name="lxqt-globalkeys" ; package_version="${package_name}_0.9.0-8" ; check_http
 	package_name="lxqt-notificationd" ; package_version="${package_name}_0.9.0-9" ; check_http
-	package_name="lxqt-sudo" ; package_version="${package_name}_0.1.0-4" ; check_http
+	package_name="lxqt-sudo" ; package_version="${package_name}_0.1.0-6" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/libs"
 	package_name="libsysstat" ; package_version="${package_name}_0.3.0-2" ; check_http
