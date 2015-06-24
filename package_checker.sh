a89de3dba224c2301fb282a77d52844b309d4ff9
Robert Nelson <robertcnelson@gmail.com>
2015-06-24 12:59:29 -0500

    lxqt-qtplugin (0.9.0-5~bpo80+20150624+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -183,7 +183,7 @@ qt5_lxqt () {
 	package_name="lxqt-runner" ; package_version="${package_name}_0.9.0-5" ; check_http
 	package_name="lxqt-policykit" ; package_version="${package_name}_0.9.0-8" ; check_http
 	package_name="lxqt-powermanagement" ; package_version="${package_name}_0.9.0-11" ; check_http
-	package_name="lxqt-qtplugin" ; package_version="${package_name}_0.9.0-3" ; check_http
+	package_name="lxqt-qtplugin" ; package_version="${package_name}_0.9.0-5" ; check_http
 	package_name="lximage-qt" ; package_version="${package_name}_0.4.0-3" ; check_http
 	package_name="lxqt-config" ; package_version="${package_name}_0.9.0-16" ; check_http
 
