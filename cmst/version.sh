a429414e69bc1704b6f8dbcb62cd01d9f71e296e
Robert Nelson <robertcnelson@gmail.com>
2015-06-25 09:04:57 -0500

    cmst (2015.06.03-2~bpo80+20150625+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cmst/version.sh
+++ cmst/version.sh
@@ -7,7 +7,7 @@ mirror="http://packages.siduction.org"
 package_name="cmst"
 debian_pkg_name="${package_name}"
 package_version="2015.06.03"
-package_source="${package_name}_${package_version}-1.tar.gz"
+package_source="${package_name}_${package_version}-2.tar.gz"
 src_dir="checkout"
 
 git_repo=""
@@ -15,7 +15,7 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="extra/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-2"
 debian_patch=""
 
-jessie_version="~bpo80+20150615+1"
+jessie_version="~bpo80+20150625+1"
