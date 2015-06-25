5171267dcdcd8d73f66cd99e9457ca92bfc42190
Robert Nelson <robertcnelson@gmail.com>
2015-06-25 09:28:20 -0500

    qterminal (0.6.0-10~bpo80+20150625+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- qterminal/version.sh
+++ qterminal/version.sh
@@ -7,7 +7,7 @@ mirror="http://packages.siduction.org"
 package_name="qterminal"
 debian_pkg_name="${package_name}"
 package_version="0.6.0"
-dl_package_source="${package_name}_${package_version}-8.tar.gz"
+dl_package_source="${package_name}_${package_version}-10.tar.gz"
 package_source="${dl_package_source}"
 src_dir="checkout"
 
@@ -16,7 +16,7 @@ git_sha=""
 reprepro_dir="q/${package_name}"
 dl_path="lxqt/pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-8"
+debian_version="${package_version}-10"
 debian_patch=""
 
-jessie_version="~bpo80+20150407+1"
+jessie_version="~bpo80+20150625+1"
