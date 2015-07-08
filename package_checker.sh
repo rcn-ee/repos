57e094341998a2ad867691728bbb699d77c40b76
Robert Nelson <robertcnelson@gmail.com>
2015-07-08 18:35:44 -0500

    liblxqt (0.9.0-20~bpo80+20150708+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -132,7 +132,7 @@ qt5_lxqt () {
 	package_name="libqtxdg" ; package_version="${package_name}_1.2.0-6" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/libl"
-	package_name="liblxqt" ; package_version="${package_name}_0.9.0-19" ; check_http
+	package_name="liblxqt" ; package_version="${package_name}_0.9.0-20" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/l"
 	package_name="lxqt-globalkeys" ; package_version="${package_name}_0.9.0-14" ; check_http
