41fc646a7fd733354758c08b50c645618ef23de0
Robert Nelson <robertcnelson@gmail.com>
2015-07-08 21:32:27 -0500

    lxqt-policykit (0.9.0-9~bpo80+20150708+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -151,8 +151,8 @@ qt5_lxqt () {
 	site="http://packages.siduction.org/lxqt/pool/main/l"
 	package_name="lxqt-session" ; package_version="${package_name}_0.9.0-15" ; check_http
 	package_name="lxqt-runner" ; package_version="${package_name}_0.9.0-8" ; check_http
+	package_name="lxqt-policykit" ; package_version="${package_name}_0.9.0-9" ; check_http
 	echo "lxqt: stretch (place holder for dependices)"
-	package_name="lxqt-policykit" ; package_version="${package_name}_0.9.0-8" ; check_http
 	package_name="lxqt-powermanagement" ; package_version="${package_name}_0.9.0-11" ; check_http
 	package_name="lxqt-qtplugin" ; package_version="${package_name}_0.9.0-5" ; check_http
 	package_name="lximage-qt" ; package_version="${package_name}_0.4.0-3" ; check_http
