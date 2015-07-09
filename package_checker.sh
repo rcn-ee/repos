3e529e56bd43352ed2c5974d4b6512fa46e1461b
Robert Nelson <robertcnelson@gmail.com>
2015-07-08 22:25:03 -0500

    lximage-qt (0.4.0-6~bpo80+20150708+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -154,8 +154,8 @@ qt5_lxqt () {
 	package_name="lxqt-policykit" ; package_version="${package_name}_0.9.0-9" ; check_http
 	package_name="lxqt-powermanagement" ; package_version="${package_name}_0.9.0-13" ; check_http
 	package_name="lxqt-qtplugin" ; package_version="${package_name}_0.9.0-6" ; check_http
+	package_name="lximage-qt" ; package_version="${package_name}_0.4.0-6" ; check_http
 	echo "lxqt: stretch (place holder for dependices)"
-	package_name="lximage-qt" ; package_version="${package_name}_0.4.0-3" ; check_http
 	package_name="lxqt-config" ; package_version="${package_name}_0.9.0-18" ; check_http
 
 	site="http://packages.siduction.org/lxqt/pool/main/o"
