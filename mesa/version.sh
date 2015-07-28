75cb6bbfc0eadc4fc4d249d619db7af4b6a1582d
Robert Nelson <robertcnelson@gmail.com>
2015-07-28 06:09:54 -0500

    mesa (10.6.3-1~bpo80+20150728+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mesa/version.sh
+++ mesa/version.sh
@@ -4,7 +4,7 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="10.5.9"
+package_version="10.6.3"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,8 +13,8 @@ git_sha=""
 reprepro_dir="m/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
 
-jessie_version="~bpo80+20150716+1"
+jessie_version="~bpo80+20150728+1"
--- package_checker.sh
+++ package_checker.sh
@@ -81,7 +81,7 @@ mesa () {
 	package_name="libdrm" ; package_version="${package_name}_2.4.62-1" ; check_http
 
 	site="http://ftp.de.debian.org/debian/pool/main/m"
-	package_name="mesa" ; package_version="${package_name}_10.5.9-2" ; check_http
+	package_name="mesa" ; package_version="${package_name}_10.6.3-1" ; check_http
 }
 
 qt5_kde () {
