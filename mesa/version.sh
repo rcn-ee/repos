33ae553ef48c717431c4d6f9fd33318876ef2e3d
Robert Nelson <robertcnelson@gmail.com>
2015-08-24 15:46:25 -0500

    mesa: 1.1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mesa/version.sh
+++ mesa/version.sh
@@ -13,7 +13,7 @@ git_sha=""
 reprepro_dir="m/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1.1"
+debian_version="${package_version}-1"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
 
