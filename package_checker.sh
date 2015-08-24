f252bde534010bf9085b5a9e5b016ce37502cbb0
Robert Nelson <robertcnelson@gmail.com>
2015-08-24 10:41:21 -0500

    libdrm (2.4.64-1~bpo80+20150824+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -79,7 +79,7 @@ mesa () {
 	package_name="llvm-toolchain-3.7" ; package_version="${package_name}_3.7~+rc3-1" ; check_http
 
 	site="http://ftp.de.debian.org/debian/pool/main/libd"
-	package_name="libdrm" ; package_version="${package_name}_2.4.62-1" ; check_http
+	package_name="libdrm" ; package_version="${package_name}_2.4.64-1" ; check_http
 
 	site="http://ftp.de.debian.org/debian/pool/main/m"
 	package_name="mesa" ; package_version="${package_name}_10.6.3-1" ; check_http
