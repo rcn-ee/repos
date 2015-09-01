7a5fa8cce33102ba5f5a01d7525027dcc8b1ddf6
Robert Nelson <robertcnelson@gmail.com>
2015-09-01 16:24:40 -0500

    kwindowsystem (5.13.0-3~bpo80+20150901+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- kwindowsystem/version.sh
+++ kwindowsystem/version.sh
@@ -4,7 +4,7 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="kwindowsystem"
 debian_pkg_name="${package_name}"
-package_version="5.12.0"
+package_version="5.13.0"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,8 +13,8 @@ git_sha=""
 reprepro_dir="k/${debian_pkg_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-3"
 debian_untar="${debian_pkg_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
-jessie_version="~bpo80+20150716+1"
+jessie_version="~bpo80+20150901+1"
