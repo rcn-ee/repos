9a1e20b93a0fe2cd945ee654030ebcf909202777
Robert Nelson <robertcnelson@gmail.com>
2015-09-14 20:40:34 -0500

    mesa (11.0.0-1~bpo80+20150914+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- mesa/version.sh
+++ mesa/version.sh
@@ -4,11 +4,9 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="11.0.0~rc3"
+package_version="11.0.0"
 package_source="${package_name}_${package_version}.orig.tar.gz"
-#src_dir="${package_name}-${package_version}"
-src_dir="${package_name}-11.0.0-rc3"
-
+src_dir="${package_name}-${package_version}"
 
 git_repo=""
 git_sha=""
@@ -19,4 +17,4 @@ debian_version="${package_version}-1"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
 
-jessie_version="~bpo80+20150907+1"
+jessie_version="~bpo80+20150914+1"
--- package_checker.sh
+++ package_checker.sh
@@ -87,7 +87,7 @@ mesa () {
 	package_name="libvdpau" ; package_version="${package_name}_1.1.1-1" ; check_http
 
 	site="${debian_pool}/main/m"
-	package_name="mesa" ; package_version="${package_name}_11.0.0~rc3-1" ; check_http
+	package_name="mesa" ; package_version="${package_name}_11.0.0-1" ; check_http
 }
 
 qt5_kde () {
