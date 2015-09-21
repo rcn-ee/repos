986566b8a0e211440618b2172d1cc1b53d26e853
Robert Nelson <robertcnelson@gmail.com>
2015-09-21 11:48:37 -0500

    kcoreaddons (5.14.0-1~bpo80+20150921+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- kcoreaddons/version.sh
+++ kcoreaddons/version.sh
@@ -4,7 +4,7 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="kcoreaddons"
 debian_pkg_name="${package_name}"
-package_version="5.13.0"
+package_version="5.14.0"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -17,4 +17,4 @@ debian_version="${package_version}-1"
 debian_patch="${debian_pkg_name}_${debian_version}.debian.tar.xz"
 debian_diff=""
 
-jessie_version="~bpo80+20150901+1"
+jessie_version="~bpo80+20150921+1"
