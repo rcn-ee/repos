520295965952110be58df1604bfcb02881feaf6a
Robert Nelson <robertcnelson@gmail.com>
2015-09-21 09:50:31 -0500

    cmake (3.3.2-1~bpo80+20150921+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cmake/version.sh
+++ cmake/version.sh
@@ -4,7 +4,7 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="cmake"
 debian_pkg_name="${package_name}"
-package_version="3.2.2"
+package_version="3.3.2"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,8 +13,8 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
-jessie_version="~bpo80+20150708+1"
+jessie_version="~bpo80+20150921+1"
