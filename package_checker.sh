7065246b4f8a6f267eada60c4ed5f21384f0dfae
Robert Nelson <robertcnelson@gmail.com>
2015-09-29 18:18:11 -0500

    libclc (0.2.0+git20150813-1~bpo80+20150929+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -80,6 +80,9 @@ mesa () {
 	package_name="llvm-toolchain-3.6" ; package_version="${package_name}_3.6.2-1" ; check_http
 	package_name="llvm-toolchain-3.7" ; package_version="${package_name}_3.7-2" ; check_http
 
+	site="${debian_pool}/main/libc"
+	package_name="libclc" ; package_version="${package_name}_0.2.0+git20150813-1" ; check_http
+
 	site="${debian_pool}/main/libd"
 	package_name="libdrm" ; package_version="${package_name}_2.4.64-1" ; check_http
 
