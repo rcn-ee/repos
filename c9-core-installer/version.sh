0b927e63c51759ac6a60777c02e9c4e807391599
Robert Nelson <robertcnelson@gmail.com>
2015-10-12 11:10:35 -0500

    c9-core-installer (3.0.2886-git20151011-1~bpo80+20151012+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- c9-core-installer/version.sh
+++ c9-core-installer/version.sh
@@ -4,7 +4,7 @@
 
 package_name="c9-core-installer"
 debian_pkg_name="${package_name}"
-package_version="3.0.2745-git20150920"
+package_version="3.0.2886-git20151011"
 package_source=""
 src_dir=""
 
@@ -17,6 +17,6 @@ debian_version="${package_version}-1"
 debian_untar=""
 debian_patch=""
 
-wheezy_version="~bpo70+20150923+1"
-jessie_version="~bpo80+20150923+1"
-stretch_version="~bpo90+20150923+1"
+wheezy_version="~bpo70+20151012+1"
+jessie_version="~bpo80+20151012+1"
+stretch_version="~bpo90+20151012+1"
