ca19596c6286066365211c41f5151e3011790ad3
Robert Nelson <robertcnelson@gmail.com>
2015-10-13 12:16:11 -0500

    cmst (2015.10.07-1rcnee1~bpo80+20151013+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cmst/version.sh
+++ cmst/version.sh
@@ -18,6 +18,7 @@ dl_path="extra/pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
+local_patch="rcnee1"
 
 jessie_version="~bpo80+20151013+1"
 stretch_version="~bpo90+20151013+1"
