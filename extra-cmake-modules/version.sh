af85c7ff0de80f49aa98aa0c88b1dd0a99073691
Robert Nelson <robertcnelson@gmail.com>
2015-10-13 10:35:05 -0500

    extra-cmake-modules (5.15.0-1~bpo80+20151013+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- extra-cmake-modules/version.sh
+++ extra-cmake-modules/version.sh
@@ -4,7 +4,7 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="extra-cmake-modules"
 debian_pkg_name="${package_name}"
-package_version="5.14.0"
+package_version="5.15.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,8 +13,8 @@ git_sha=""
 reprepro_dir="e/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
-jessie_version="~bpo80+20150921+1"
+jessie_version="~bpo80+20151013+1"
