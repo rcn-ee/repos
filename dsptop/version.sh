79963cdef12d74dec2831e7698518dc376d7e797
Robert Nelson <robertcnelson@gmail.com>
2015-10-15 16:14:29 -0500

    dsptop (1.1.5-git20150710-1~bpo80+20151015+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- dsptop/version.sh
+++ dsptop/version.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 
-mirror="http://archive.canonical.com"
+#http://git.ti.com/gitweb/?p=sdo-emu/dsptop.git;a=summary
 
 package_name="dsptop"
 debian_pkg_name="${package_name}"
-package_version="1.1.5"
-package_source="${package_name}_${package_version}-0ubuntu1.tar.gz"
-src_dir="${package_name}-${package_version}"
+package_version="1.1.5-git20150710"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
 
-git_repo=""
-git_sha=""
+git_repo="git://git.ti.com/sdo-emu/dsptop.git"
+git_sha="edf5670075cf6cf42f46654d284f9f78c1c123a1"
 reprepro_dir="d/${package_name}"
-dl_path="pool/partner/${reprepro_dir}/"
+dl_path=""
 
-debian_version="${package_version}-0ubuntu1"
-debian_untar=""
+debian_version="${package_version}-1"
 debian_patch=""
+debian_diff=""
 
-wheezy_version="~bpo70+20150723+1"
-jessie_version="~bpo80+20150723+1"
-stretch_version="~bpo90+20150723+1"
+wheezy_version="~bpo70+20151015+1"
+jessie_version="~bpo80+20151015+1"
+stretch_version="~bpo90+20151015+1"
