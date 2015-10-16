68414b1e10268c5644d663baa26c659a0e956aad
Robert Nelson <robertcnelson@gmail.com>
2015-10-16 13:23:09 -0500

    gdbproxy-mod-dkms (1.0.1-git20150805-1~bpo80+20151016+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- gdbproxy-mod-dkms/version.sh
+++ gdbproxy-mod-dkms/version.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 
-mirror="http://archive.canonical.com"
+#http://git.ti.com/gitweb/?p=sdo-emu/gdbc6x.git;a=summary
 
 package_name="gdbproxy-mod-dkms"
 debian_pkg_name="${package_name}"
-package_version="1.0.1"
-package_source="${package_name}_${package_version}-0ubuntu1.tar.gz"
-src_dir="${package_name}-${package_version}"
+package_version="1.0.1-git20150805"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
 
-git_repo=""
-git_sha=""
+git_repo="git://git.ti.com/sdo-emu/gdbc6x.git"
+git_sha="653f7d0a345bede0542326090b622b885545c712"
 reprepro_dir="g/${package_name}"
-dl_path="pool/partner/${reprepro_dir}/"
+dl_path=""
 
-debian_version="${package_version}-0ubuntu1"
-debian_untar=""
+debian_version="${package_version}-1"
 debian_patch=""
+debian_diff=""
 
-wheezy_version="~bpo70+20150723+1"
-jessie_version="~bpo80+20150723+1"
-stretch_version="~bpo90+20150723+1"
+wheezy_version="~bpo70+20151016+1"
+jessie_version="~bpo80+20151016+1"
+stretch_version="~bpo90+20151016+1"
