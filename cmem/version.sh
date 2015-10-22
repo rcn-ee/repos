5f8af292574ba64ec86d5b056b308a8da3033a95
Robert Nelson <robertcnelson@gmail.com>
2015-10-22 10:32:07 -0500

    cmem (4.10.02.04-git20151016-1~bpo80+20151022+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- cmem/version.sh
+++ cmem/version.sh
@@ -4,19 +4,26 @@
 
 package_name="cmem"
 debian_pkg_name="${package_name}"
-package_version="4.10.02.03-git20150921"
+package_version="4.10.02.04-git20151016"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/ipc/ludev.git"
-git_sha="d47ac635699da6bee6daff31b87b0123e13af4ac"
+git_sha="00241c45f810c8b8e4f5ac66c3323fec8e43b621"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-1.1"
+debian_version="${package_version}-1"
 debian_patch=""
 debian_diff=""
 
-wheezy_version="~bpo70+20151015+1"
-jessie_version="~bpo80+20151015+1"
-stretch_version="~bpo90+20151015+1"
+#sed -i -e 's:4.10.02.03:4.10.02.04:g' ./debian/cmem-mod-dkms.install
+#sed -i -e 's:4.10.02.03:4.10.02.04:g' ./debian/control
+#sed -i -e 's:4.10.02.03:4.10.02.04:g' ./debian/cmem-mod-dkms.dkms
+
+wheezy_version="~bpo70+20151022+1"
+jessie_version="~bpo80+20151022+1"
+stretch_version="~bpo90+20151022+1"
+trusty_version="~bpo1404+20151022+1"
+vivid_version="~bpo1504+20151022+1"
+wily_version="~bpo1510+20151022+1"
