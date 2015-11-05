f04eb8648d783c0ceeef702b62293fb29e4855fe
Robert Nelson <robertcnelson@gmail.com>
2015-11-05 14:23:38 -0600

    dart-1.13
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- dart-1.13/generate_source.sh
+++ dart-1.13/generate_source.sh
@@ -6,7 +6,7 @@
 server="https://gsdview.appspot.com/dart-archive/channels/dev/release"
 #1.13.0-dev.7.9/linux_packages/debian_wheezy/dart_1.13.0-dev.7.9.orig.tar.gz
 
-wget -c ${server}/${package_version}/linux_packages/debian_wheezy/dart_${package_version}.orig.tar.gz
-cp dart_${package_version}.orig.tar.gz ${package_name}_${package_version}.orig.tar.gz
-wget -c ${server}/${package_version}/linux_packages/debian_wheezy/dart_${debian_version}.debian.tar.gz
-
+wget -c ${server}/${real_package_version}/linux_packages/debian_wheezy/dart_${real_package_version}.orig.tar.gz
+cp dart_${real_package_version}.orig.tar.gz ${package_name}_${package_version}.orig.tar.gz
+wget -c ${server}/${real_package_version}/linux_packages/debian_wheezy/dart_${real_package_version}-1.debian.tar.gz
+cp dart_${real_package_version}-1.debian.tar.gz dart_${package_version}-1.debian.tar.gz
