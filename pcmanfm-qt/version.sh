f3c794da48a294349e92310e43e6b3920df8232a
Robert Nelson <robertcnelson@gmail.com>
2015-11-08 22:26:44 -0600

    pcmanfm-qt (0.10.0-1rcnee3~bpo80+20151108+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pcmanfm-qt/version.sh
+++ pcmanfm-qt/version.sh
@@ -16,7 +16,7 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee2"
+local_patch="rcnee3"
 
 jessie_version="~bpo80+20151108+1"
 stretch_version="~bpo90+20151108+1"
