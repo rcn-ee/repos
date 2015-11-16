f8486b90d7f1e6f04f88c1dc77facb80704a3b7d
Robert Nelson <robertcnelson@gmail.com>
2015-11-16 14:25:29 -0600

    add cdbs
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/first_time.sh
+++ tools/first_time.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install sbuild dh-autoreconf quilt gobject-introspection scons xserver-xorg-dev pkg-kde-tools dos2unix sphinx-common dh-systemd
+sudo apt-get install sbuild dh-autoreconf quilt gobject-introspection scons xserver-xorg-dev pkg-kde-tools dos2unix sphinx-common dh-systemd cdbs
