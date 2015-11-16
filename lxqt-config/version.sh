16775df70ee7606285cd4f52c2f4840776d30adf
Robert Nelson <robertcnelson@gmail.com>
2015-11-16 07:12:59 -0600

    lxqt-config (0.10.0-1~bpo80+20151116+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- lxqt-config/version.sh
+++ lxqt-config/version.sh
@@ -17,5 +17,5 @@ debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
-jessie_version="~bpo80+20151110+1"
-stretch_version="~bpo90+20151110+1"
+jessie_version="~bpo80+20151116+1"
+stretch_version="~bpo90+20151116+1"
