c7e1b0a2d5891c8a81c30988b512d33b5656162e
Robert Nelson <robertcnelson@gmail.com>
2015-11-18 13:24:51 -0600

    nodejs: we are staying with v4.2.x lts in jessie
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-nodejs/version.sh
+++ debian-8-nodejs/version.sh
@@ -5,7 +5,8 @@ mirror="http://ftp.de.debian.org/debian"
 package_name="nodejs"
 debian_pkg_name="${package_name}"
 package_version="4.2.2~dfsg"
-package_source="${package_name}_${package_version}.orig.tar.gz"
+real_package_version="4.2.2"
+package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="node-v4.2.2"
 
 git_repo=""
@@ -14,7 +15,7 @@ reprepro_dir="n/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1"
-debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_untar=""
 debian_patch=""
 
 jessie_version="~bpo80+20151117+1"
