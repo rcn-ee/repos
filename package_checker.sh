5a6940f6f24f8bd7811df838d80fbcd18e05d90d
Robert Nelson <robertcnelson@gmail.com>
2015-11-20 11:46:59 -0600

    lxqt-qtplugin (0.10.0-2~bpo80+20151120+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -192,7 +192,7 @@ qt5_lxqt () {
 
 	#https://tracker.debian.org/pkg/lxqt-qtplugin
 	site="${debian_pool}/main/l"
-	package_name="lxqt-qtplugin" ; package_version="${package_name}_0.10.0-1" ; check_http
+	package_name="lxqt-qtplugin" ; package_version="${package_name}_0.10.0-2" ; check_http
 
 	#https://tracker.debian.org/pkg/lxqt-session
 	site="${debian_pool}/main/l"
