b63aa2dab17aac3f1fa072e5b2a7b00678b82324
Robert Nelson <robertcnelson@gmail.com>
2015-11-20 14:28:14 -0600

    lxqt-runner (0.10.0-2~bpo80+20151120+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -208,7 +208,7 @@ qt5_lxqt () {
 
 	#https://tracker.debian.org/pkg/lxqt-runner
 	site="${debian_pool}/main/l"
-	package_name="lxqt-runner" ; package_version="${package_name}_0.10.0-1" ; check_http
+	package_name="lxqt-runner" ; package_version="${package_name}_0.10.0-2" ; check_http
 
 	#https://tracker.debian.org/pkg/lxqt-config
 	site="${debian_pool}/main/l"
