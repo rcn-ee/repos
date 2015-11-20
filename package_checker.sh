f556b0781583661f73401728e265065a6fca070b
Robert Nelson <robertcnelson@gmail.com>
2015-11-20 10:33:29 -0600

    lxqt-openssh-askpass (0.10.0-2~bpo80+20151120+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -176,7 +176,7 @@ qt5_lxqt () {
 
 	#https://tracker.debian.org/pkg/lxqt-openssh-askpass
 	site="${debian_pool}/main/l"
-	package_name="lxqt-openssh-askpass" ; package_version="${package_name}_0.10.0-1" ; check_http
+	package_name="lxqt-openssh-askpass" ; package_version="${package_name}_0.10.0-2" ; check_http
 
 	#https://tracker.debian.org/pkg/lxqt-notificationd
 	site="${debian_pool}/main/l"
