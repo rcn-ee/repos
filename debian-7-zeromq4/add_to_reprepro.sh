be17f9b3369fbee22009c8b13ed5c1bb9e4d8e32
Robert Nelson <robertcnelson@gmail.com>
2015-11-20 19:23:47 -0600

    debian-7-zeromq4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-7-zeromq4/add_to_reprepro.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+. version.sh
+
+base="/var/www/html/repos.rcn-ee.net/"
+outgoing="/var/www/html/farm/outgoing"
+
+run () {
+	changes_file=$(ls ${outgoing}/${suite}/${deb_arch}/${debian_pkg_name}_${debian_version}/ | grep changes)
+
+	if [ ! "x${changes_file}" = "x" ] ; then
+		if [ -f ${outgoing}/${suite}/${deb_arch}/${debian_pkg_name}_${debian_version}/${changes_file} ] ; then
+			wfile="${outgoing}/${suite}/${deb_arch}/${debian_pkg_name}_${debian_version}/${changes_file}"
+			reprepro -b ${repo} -C main include ${suite} ${wfile}
+		fi
+	fi
+}
+
+dist="debian"
+suite="wheezy"
+#if [ -d suite/${suite}/ ] ; then
+	repo="${base}${dist}/"
+	deb_arch=armhf
+	run
+#fi
+#if [ -d suite/${suite}/ ] ; then
+	repo="${base}${dist}-machinekit/"
+	deb_arch="armhf"
+	run
+#fi
+#
