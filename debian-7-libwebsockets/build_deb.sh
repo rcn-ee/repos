da2dfffc95bdc859e1b1f397c9833aed7bdfe860
Robert Nelson <robertcnelson@gmail.com>
2015-11-20 19:11:32 -0600

    back to old
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-7-libwebsockets/build_deb.sh
+++ debian-7-libwebsockets/build_deb.sh
@@ -2,10 +2,12 @@
 
 . version.sh
 
+sudo mount -a
+
 localdir="/mnt/farm"
 
 run () {
-	out_dir="${localdir}/outgoing/${suite}/${deb_arch}/${debian_pkg_name}_${debian_version}/"
+	out_dir="${localdir}/outgoing/${suite}/${deb_arch}/${debian_pkg_name}_${debian_version}"
 	if [ -f /var/lib/sbuild/${suite}-${deb_arch}.tar.gz ] ; then
 		if [ -d ./${suite} ] ; then
 			rm -rf ./${suite}/
@@ -14,29 +16,18 @@ run () {
 		mkdir ./${suite}
 		cd ./${suite}
 
-		dsc_file=$(ls ${localdir}/incoming/${suite}/${debian_pkg_name}_${debian_version}/ | grep dsc)
 		options="--arch=${deb_arch} -A -s --force-orig-source --dist=${suite}"
+		sbuild ${options} ${mirror}/${dl_path}${package_name}_${debian_version}.dsc
 
-		if [ ! "x${dsc_file}" = "x" ] ; then
-			if [ -f ${localdir}/incoming/${suite}/${debian_pkg_name}_${debian_version}/${dsc_file} ] ; then
-				sbuild ${options} http://httphost/farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/${dsc_file}
-
-				if [ -f *.changes ] ; then
-					if [ -d ${out_dir} ] ; then
-						rm -rf ${out_dir}
-					fi
-					mkdir -p ${out_dir}
-					cp -v *orig* ${out_dir} || true
-					cp -v *debian* ${out_dir} || true
-					cp -v *tar* ${out_dir} || true
-					cp -v *.changes ${out_dir} || true
-					cp -v *.deb ${out_dir} || true
-					cp -v *.dsc ${out_dir} || true
-					cp -v *.udeb ${out_dir} || true
-					cp -v *.diff.gz ${out_dir} || true
-					cp -v *${deb_arch}.build ${out_dir} || true
-				fi
-			fi
+		if [ -f *.changes ] ; then
+			mkdir -p ${out_dir}/
+			cp -v *orig* ${out_dir}/ || true
+			cp -v *debian* ${out_dir}/ || true
+			cp -v *.changes ${out_dir}/ || true
+			cp -v *.deb ${out_dir}/ || true
+			cp -v *.dsc ${out_dir}/ || true
+			cp -v *.udeb ${out_dir}/ || true
+			cp -v *${deb_arch}.build ${out_dir} || true
 		fi
 
 		cd ../
@@ -45,8 +36,6 @@ run () {
 
 dist="debian"
 suite="wheezy"
-#if [ -d suite/${suite}/ ] ; then
-	deb_arch="armhf"
-	run
-#fi
+deb_arch="armhf"
+run
 
