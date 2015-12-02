64fe0b7c271a9cc0ae0b0ba538dd3e785670b772
Robert Nelson <robertcnelson@gmail.com>
2015-12-02 10:36:04 -0600

    kcoreaddons (5.16.0-1~bpo80+20151202+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -100,7 +100,7 @@ qt5_kde () {
 	package_name="solid" ; package_version="${package_name}_5.16.0-1" ; check_http
 
 	site="${debian_pool}/main/k"
-	package_name="kcoreaddons" ; package_version="${package_name}_5.15.0-1" ; check_http
+	package_name="kcoreaddons" ; package_version="${package_name}_5.16.0-1" ; check_http
 	package_name="kguiaddons" ; package_version="${package_name}_5.15.0-1" ; check_http
 	package_name="kwindowsystem" ; package_version="${package_name}_5.15.0-1" ; check_http
 }
