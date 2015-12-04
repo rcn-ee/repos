e8014779bf940f2850067df615e3ffe7cda83d74
Robert Nelson <robertcnelson@gmail.com>
2015-12-04 15:02:32 -0600

    nodejs (0.10.41~dfsg-1~bpo80+20151204+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-7-nodejs/version.sh
+++ debian-7-nodejs/version.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -e
 
-mirror="http://ftp.de.debian.org/debian"
+#mirror="http://ftp.de.debian.org/debian"
+#0.10.38
+mirror="http://snapshot.debian.org/archive/debian/20150505T035629Z"
 
 package_name="nodejs"
 debian_pkg_name="${package_name}"
-package_version="0.10.38~dfsg"
-package_source="${package_name}_${package_version}.orig.tar.gz"
-src_dir="node-v0.10.38"
+package_version="0.10.41~dfsg"
+real_package_version="0.10.41"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="node-v0.10.41"
 
 git_repo=""
 git_sha=""
@@ -14,8 +17,9 @@ reprepro_dir="n/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1"
-debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_untar=""
+#debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
-wheezy_version="~bpo70+20150526+1"
-jessie_version="~bpo80+20150526+1"
+wheezy_version="~bpo70+20151204+1"
+jessie_version="~bpo80+20151204+1"
