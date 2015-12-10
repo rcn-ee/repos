650a6a2dda4afa7a2702690959b8e21523dbea72
Robert Nelson <robertcnelson@gmail.com>
2015-12-10 16:22:20 -0600

    initramfs-tools (0.120ubuntu6-1rcnee2~bpo1510+20151210+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ubuntu-1510-initramfs-tools/version.sh
+++ ubuntu-1510-initramfs-tools/version.sh
@@ -16,9 +16,9 @@ git_sha=""
 reprepro_dir="i/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1rcnee1"
+debian_version="${package_version}-1"
 debian_untar=""
 debian_patch=""
+local_patch="rcnee2"
 
-
-wily_version="~bpo1510+20151007+1"
+wily_version="~bpo1510+20151210+1"
