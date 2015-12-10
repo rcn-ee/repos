f271beb83c2c811813092d789ae2a465455dddea
Robert Nelson <robertcnelson@gmail.com>
2015-12-10 11:28:37 -0600

    llvm-toolchain-3.7 1:3.7.1-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-llvm-toolchain-3.7/version.sh
+++ debian-8-llvm-toolchain-3.7/version.sh
@@ -4,13 +4,13 @@ mirror="http://ftp.de.debian.org/debian"
 
 package_name="llvm-toolchain-3.7"
 debian_pkg_name="${package_name}"
-package_version="3.7"
+package_version="3.7.1"
 
 git_repo=""
 git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-4"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
