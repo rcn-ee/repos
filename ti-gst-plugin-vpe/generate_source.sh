bbbaf91f74cf79a94c149e442bd6b386a6baa468
Robert Nelson <robertcnelson@gmail.com>
2015-12-16 14:57:55 -0600

    run autogen from source packer
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-gst-plugin-vpe/generate_source.sh
+++ ti-gst-plugin-vpe/generate_source.sh
@@ -2,12 +2,16 @@
 
 . version.sh
 
+if [ -d ./${package_name}_${package_version} ] ; then
+	rm -rf ./${package_name}_${package_version} || true
+fi
+
 mkdir -p ./${package_name}_${package_version}
 
 cd ./${package_name}_${package_version}
 git clone ${git_repo} .
 git checkout ${git_sha}
-git clone git://anongit.freedesktop.org/gstreamer/common
+./autogen.sh
 cd ..
 tar -cJf ${package_name}_${package_version}.orig.tar.xz ./${package_name}_${package_version}
 
