df84ad2d57020aeae620ddbcc1051ad64a6997b9
Robert Nelson <robertcnelson@gmail.com>
2015-12-16 12:30:55 -0600

    dce (3.00.09.00-1rcnee1~bpo80+20151216+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-libdce/version.sh
+++ ti-libdce/version.sh
@@ -3,7 +3,7 @@
 #Library used for remotely invoking the hw accelerated codec on IVA-HD
 #http://omapzoom.org/?p=repo/libdce.git;a=summary
 
-package_name="libdce"
+package_name="dce"
 debian_pkg_name="${package_name}"
 package_version="3.00.09.00"
 package_source="${package_name}_${package_version}.orig.tar.xz"
@@ -11,7 +11,7 @@ src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.omapzoom.org/repo/libdce.git"
 git_sha="3040e7e3f7d1ed42a1273ccc6b912328b90b16db"
-reprepro_dir="libd/${package_name}"
+reprepro_dir="d/${package_name}"
 dl_path=""
 
 debian_version="${package_version}-1"
@@ -19,4 +19,4 @@ debian_patch=""
 debian_diff=""
 local_patch="rcnee1"
 
-jessie_version="~bpo80+20151210+1"
+jessie_version="~bpo80+20151216+1"
