b5dd1d9fc8de00e556d48f772e608eb637c426a9
Robert Nelson <robertcnelson@gmail.com>
2015-12-17 20:01:41 -0600

    mesa-etnaviv mesa (11.1.0-1rcnee1etnaviv20151216~bpo80+20151217+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ mesa-etnaviv/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+mirror="http://ftp.de.debian.org/debian"
+
+package_name="mesa"
+debian_pkg_name="${package_name}"
+package_version="11.1.0"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="git://anongit.freedesktop.org/mesa/mesa"
+git_sha="0d19dc302f21d00b88ccb7b70e5a110d17ea4fdf"
+reprepro_dir="m/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-1"
+debian_untar=""
+debian_patch=""
+local_patch="rcnee1etnaviv20151216"
+
+jessie_version="~bpo80+20151217+1"
