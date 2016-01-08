2ae5e6b30a4eb64b129b5e760009040efa02bc50
Robert Nelson <robertcnelson@gmail.com>
2016-01-08 15:59:12 -0600

    fix dir
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-pkg-kde-tools/version.sh
+++ debian-8-pkg-kde-tools/version.sh
@@ -6,7 +6,7 @@ package_name="pkg-kde-tools"
 debian_pkg_name="${package_name}"
 package_version="0.15.20"
 package_source="${package_name}_${package_version}.tar.xz"
-src_dir="${package_name}-${package_version}"
+src_dir="${package_name}"
 
 git_repo=""
 git_sha=""
