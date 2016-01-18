ecfa80f431a2b7e41ebbcc9b22d1358170c22e41
Robert Nelson <robertcnelson@gmail.com>
2016-01-18 13:40:32 -0600

    ros-angles (1.9.10-1~bpo80+20160118+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -306,6 +306,7 @@ ros () {
 	package_name="ros-roslisp"; package_version="${package_name}_1.9.19-1"; check_http
 	package_name="ros-actionlib"; package_version="${package_name}_1.11.4-2"; check_http
 	package_name="ros-dynamic-reconfigure"; package_version="${package_name}_1.5.39-2"; check_http
+	package_name="ros-angles"; package_version="${package_name}_1.9.10-1"; check_http
 }
 
 ros
