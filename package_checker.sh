c669c31c8905cc85b2fdb062890e5db3493891ab
Robert Nelson <robertcnelson@gmail.com>
2016-01-18 09:47:20 -0600

    ros-message-runtime (0.4.12-3~bpo80+20160118+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -291,6 +291,7 @@ ros () {
 	package_name="ros-catkin"; package_version="${package_name}_0.6.16-3"; check_http
 	package_name="ros-cmake-modules"; package_version="${package_name}_0.4.0-2"; check_http
 	package_name="ros-message-generation"; package_version="${package_name}_0.3.0-3"; check_http
+	package_name="ros-message-runtime"; package_version="${package_name}_0.4.12-3"; check_http
 }
 
 ros
