a5a43cece18c9b51b7fd645f4d7e9c4e482db59b
Robert Nelson <robertcnelson@gmail.com>
2016-01-18 15:12:34 -0600

    ros-bond-core (1.7.16-3~bpo80+20160118+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -315,6 +315,7 @@ ros () {
 	site="${debian_pool}/main/r"
 	package_name="ros-geometry-experimental"; package_version="${package_name}_0.5.12-2"; check_http
 	package_name="ros-geometry"; package_version="${package_name}_1.11.7-1"; check_http
+	package_name="ros-bond-core"; package_version="${package_name}_1.7.16-3"; check_http
 }
 
 ros
