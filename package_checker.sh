165e39305c84f51e26da4f39782855f6a4baea52
Robert Nelson <robertcnelson@gmail.com>
2016-01-18 15:48:31 -0600

    ros-random-numbers (0.3.0-2~bpo80+20160118+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -319,6 +319,7 @@ ros () {
 	package_name="ros-class-loader"; package_version="${package_name}_0.3.2-1"; check_http
 	package_name="ros-eigen-stl-containers"; package_version="${package_name}_0.1.4-1"; check_http
 	package_name="ros-resource-retriever"; package_version="${package_name}_1.11.6-2"; check_http
+	package_name="ros-random-numbers"; package_version="${package_name}_0.3.0-2"; check_http
 }
 
 ros
