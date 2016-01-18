f13aaa26eba2f0fabf6ba18e2f242506935689ed
Robert Nelson <robertcnelson@gmail.com>
2016-01-18 15:32:05 -0600

    ros-eigen-stl-containers (0.1.4-1~bpo80+20160118+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -317,6 +317,7 @@ ros () {
 	package_name="ros-geometry"; package_version="${package_name}_1.11.7-1"; check_http
 	package_name="ros-bond-core"; package_version="${package_name}_1.7.16-3"; check_http
 	package_name="ros-class-loader"; package_version="${package_name}_0.3.2-1"; check_http
+	package_name="ros-eigen-stl-containers"; package_version="${package_name}_0.1.4-1"; check_http
 }
 
 ros
