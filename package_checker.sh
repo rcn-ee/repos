468140fdc5f59681b01ed20656b402a62c1b89ed
Robert Nelson <robertcnelson@gmail.com>
2016-01-19 09:27:26 -0600

    ros-laser-geometry (1.6.4-2~bpo80+20160119+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -324,6 +324,7 @@ ros () {
 	package_name="ros-pluginlib"; package_version="${package_name}_1.10.1-2"; check_http
 	package_name="ros-image-common"; package_version="${package_name}_1.11.8-2"; check_http
 	package_name="ros-interactive-markers"; package_version="${package_name}_1.11.1-2"; check_http
+	package_name="ros-laser-geometry"; package_version="${package_name}_1.6.4-2"; check_http
 }
 
 ros
