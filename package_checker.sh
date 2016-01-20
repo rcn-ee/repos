62accbe8bf71cba9892e723741073fa41677c371
Robert Nelson <robertcnelson@gmail.com>
2016-01-20 10:24:07 -0600

    lxqt-globalkeys (0.10.0-5~bpo80+20160120+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -189,7 +189,7 @@ qt5_lxqt () {
 
 	#https://tracker.debian.org/pkg/lxqt-globalkeys
 	site="${debian_pool}/main/l"
-	package_name="lxqt-globalkeys" ; package_version="${package_name}_0.10.0-2" ; check_http
+	package_name="lxqt-globalkeys" ; package_version="${package_name}_0.10.0-5" ; check_http
 
 	#https://tracker.debian.org/pkg/lxqt-openssh-askpass
 	site="${debian_pool}/main/l"
