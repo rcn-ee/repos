2b1f19cd5ecaa07037c6393362db8826b924b841
Robert Nelson <robertcnelson@gmail.com>
2016-01-20 10:05:57 -0600

    ros-vcstools (0.1.38-1~bpo80+20160120+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -352,6 +352,7 @@ ros () {
 	package_name="ros-rosdistro"; package_version="${package_name}_0.4.2-1"; check_http
 	package_name="ros-rosinstall"; package_version="${package_name}_0.7.7-1"; check_http
 	package_name="ros-rosinstall-generator"; package_version="${package_name}_0.1.11-1"; check_http
+	package_name="ros-vcstools"; package_version="${package_name}_0.1.38-1"; check_http
 }
 
 ros
