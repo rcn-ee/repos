f143b1056a85f612fd9d4ba8271aa88e73fd67ca
Robert Nelson <robertcnelson@gmail.com>
2016-01-22 09:18:14 -0600

    lxqt-policykit (0.10.0-3~bpo80+20160122+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -205,7 +205,7 @@ qt5_lxqt () {
 
 	#https://tracker.debian.org/pkg/lxqt-policykit
 	site="${debian_pool}/main/l"
-	package_name="lxqt-policykit" ; package_version="${package_name}_0.10.0-2" ; check_http
+	package_name="lxqt-policykit" ; package_version="${package_name}_0.10.0-3" ; check_http
 
 	#https://tracker.debian.org/pkg/lxqt-qtplugin
 	site="${debian_pool}/main/l"
