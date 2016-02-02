26ea393d21942418cc53f12b1bec3c7051d9f0ea
Robert Nelson <robertcnelson@gmail.com>
2016-02-02 16:48:31 -0600

    drop: vivid
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- oracle-java9-installer/suite/vivid/debian/jdk.sh
+++
@@ -1,5 +0,0 @@
-export J2SDKDIR=/usr/lib/jvm/java-9-oracle
-export J2REDIR=/usr/lib/jvm/java-9-oracle
-export PATH=$PATH:/usr/lib/jvm/java-9-oracle/bin:/usr/lib/jvm/java-9-oracle/db/bin
-export JAVA_HOME=/usr/lib/jvm/java-9-oracle
-export DERBY_HOME=/usr/lib/jvm/java-9-oracle/db
