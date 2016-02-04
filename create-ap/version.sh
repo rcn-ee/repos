9854c5d1dfa841a1a4c1752bbadc235792583b2c
Robert Nelson <robertcnelson@gmail.com>
2016-02-04 16:42:36 -0600

    create-ap (0.2.0-0rcnee1~bpo80+20160204+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ create-ap/version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+package_name="create-ap"
+debian_pkg_name="${package_name}"
+package_version="0.2.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+local_patch="rcnee1"
+jessie_version="~bpo80+20160204+1"
