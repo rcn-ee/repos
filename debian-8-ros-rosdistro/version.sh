375d196fb0441337334a85345e3ca9ff9381c0c4
Robert Nelson <robertcnelson@gmail.com>
2016-02-07 11:05:33 -0600

    fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-ros-rosdistro/version.sh
+++ debian-8-ros-rosdistro/version.sh
@@ -6,7 +6,7 @@ package_name="ros-rosdistro"
 debian_pkg_name="${package_name}"
 package_version="0.4.3"
 package_source="${package_name}_${package_version}.orig.tar.gz"
-src_dir="${package_name}-${package_version}"
+src_dir="rosdistro-${package_version}"
 
 git_repo=""
 git_sha=""
