f684bd3b4ff6b363d63143c11d703e1195e9a520
Robert Nelson <robertcnelson@gmail.com>
2016-02-11 10:01:08 -0600

    nodejs-v0.12.x (0.12.10-0rcnee2~bpo80+20160211+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- nodejs-v0.12.x/version.sh
+++ nodejs-v0.12.x/version.sh
@@ -16,6 +16,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
-local_patch="rcnee1"
+local_patch="rcnee2"
 
-jessie_version="~bpo80+20160210+1"
+jessie_version="~bpo80+20160211+1"
