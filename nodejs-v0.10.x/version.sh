e64d6c87acd2bb05dbd4668c5d0864c2a9ed93f4
Robert Nelson <robertcnelson@gmail.com>
2016-02-10 20:02:15 -0600

    nodejs: start of crazyness
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ nodejs-v0.10.x/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+#mirror="http://http.debian.net/debian"
+
+package_name="nodejs-v0.10.x"
+debian_pkg_name="${package_name}"
+package_version="0.10.42"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="node-v${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="n/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+local_patch="rcnee1"
+
+jessie_version="~bpo80+20160210+1"
