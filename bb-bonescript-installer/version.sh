7eb87383e0da79b0c95838efe8362219cc1b3770
Robert Nelson <robertcnelson@gmail.com>
2016-02-18 10:28:59 -0600

    bb-bonescript-installer
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-bonescript-installer/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+package_name="bb-bonescript-installer"
+debian_pkg_name="${package_name}"
+package_version="0.5.0~beta3"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+local_patch="rcnee1"
+
+jessie_version="~bpo80+20160218+1"
+stretch_version="~bpo90+20160218+1"
