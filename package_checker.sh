6005b513c2784b7ff3b6019959ea12a6c395d029
Robert Nelson <robertcnelson@gmail.com>
2016-02-21 19:59:52 -0600

    ros-catkin (0.6.16-4~bpo80+20160221+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -322,7 +322,7 @@ ros () {
 	echo "ros: (jessie)"
 	site="${debian_pool}/main/r"
 	package_name="ros-catkin-pkg"; package_version="${package_name}_0.2.10-2"; check_http
-	package_name="ros-catkin"; package_version="${package_name}_0.6.16-3"; check_http
+	package_name="ros-catkin"; package_version="${package_name}_0.6.16-4"; check_http
 	package_name="ros-cmake-modules"; package_version="${package_name}_0.4.0-2"; check_http
 	package_name="ros-message-generation"; package_version="${package_name}_0.3.0-3"; check_http
 	package_name="ros-message-runtime"; package_version="${package_name}_0.4.12-3"; check_http
