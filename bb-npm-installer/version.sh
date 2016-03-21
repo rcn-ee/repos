da925cc85d06a334bd473a07facbaf7d86398f5f
Robert Nelson <robertcnelson@gmail.com>
2016-03-21 10:55:11 -0500

    bb-npm-installer (3.8.2-0rcnee1~bpo80+20160321+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-npm-installer/version.sh
+++ bb-npm-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-npm-installer"
 debian_pkg_name="${package_name}"
-package_version="3.8.0"
+package_version="3.8.2"
 package_source=""
 src_dir=""
 
@@ -14,9 +14,9 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
-local_patch="rcnee3"
+local_patch="rcnee1"
 
-wheezy_version="~bpo70+20160307+1"
-jessie_version="~bpo80+20160307+1"
-stretch_version="~bpo90+20160307+1"
-xenial_version="~bpo1604+20160307+1"
+wheezy_version="~bpo70+20160321+1"
+jessie_version="~bpo80+20160321+1"
+stretch_version="~bpo90+20160321+1"
+xenial_version="~bpo1604+20160321+1"
