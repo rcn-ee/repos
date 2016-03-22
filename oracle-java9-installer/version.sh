93b1383dca063e7dfe3a8ce4757579612b7a156c
Robert Nelson <robertcnelson@gmail.com>
2016-03-22 12:16:41 -0500

    oracle-java9-installer (9b110+9b110arm-1rcnee1~bpo80+20160322+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- oracle-java9-installer/version.sh
+++ oracle-java9-installer/version.sh
@@ -2,11 +2,11 @@
 
 #https://launchpad.net/~webupd8team/+archive/ubuntu/java/+packages
 
-#wget https://launchpad.net/~webupd8team/+archive/ubuntu/java/+files/oracle-java9-installer_9b95+9b95arm-1~webupd8~1.tar.xz
+#wget https://launchpad.net/~webupd8team/+archive/ubuntu/java/+files/oracle-java9-installer_9b110+9b110arm-1~webupd8~0.tar.xz
 
 package_name="oracle-java9-installer"
 debian_pkg_name="${package_name}"
-package_version="9b95+9b95arm"
+package_version="9b110+9b110arm"
 package_source=""
 src_dir=""
 
@@ -18,11 +18,11 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar=""
 debian_patch=""
+local_patch="rcnee1"
 
-wheezy_version="~bpo70+20160114+1"
-jessie_version="~bpo80+20160114+1"
-stretch_version="~bpo90+20160114+1"
-trusty_version="~bpo1404+20160114+1"
-vivid_version="~bpo1504+20160114+1"
-wily_version="~bpo1510+20160114+1"
-xenial_version="~bpo1604+20160114+1"
+wheezy_version="~bpo70+20160322+1"
+jessie_version="~bpo80+20160322+1"
+stretch_version="~bpo90+20160322+1"
+trusty_version="~bpo1404+20160322+1"
+wily_version="~bpo1510+20160322+1"
+xenial_version="~bpo1604+20160322+1"
