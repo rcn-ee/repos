2f3899ab4c76ecc15448d7b5bfba22dfbf3053a6
Robert Nelson <robertcnelson@gmail.com>
2016-03-24 12:58:19 -0500

    cmem (4.10.02.06-git20160318-0rcnee1~bpo80+20160324+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-cmem/version.sh
+++ ti-cmem/version.sh
@@ -4,16 +4,16 @@
 
 package_name="cmem"
 debian_pkg_name="${package_name}"
-package_version="4.10.02.04-git20151016"
+package_version="4.10.02.06-git20160318"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/ipc/ludev.git"
-git_sha="00241c45f810c8b8e4f5ac66c3323fec8e43b621"
+git_sha="4d599728974e4786bf2e0494605e9d79ebddee79"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 local_patch="rcnee1"
@@ -22,10 +22,7 @@ local_patch="rcnee1"
 #sed -i -e 's:4.10.02.03:4.10.02.04:g' ./debian/control
 #sed -i -e 's:4.10.02.03:4.10.02.04:g' ./debian/cmem-mod-dkms.dkms
 
-wheezy_version="~bpo70+20151217+1"
-jessie_version="~bpo80+20151217+1"
-stretch_version="~bpo90+20151217+1"
-trusty_version="~bpo1404+20151217+1"
-vivid_version="~bpo1504+20151217+1"
-wily_version="~bpo1510+20151217+1"
-xenial_version="~bpo1604+20151217+1"
+jessie_version="~bpo80+20160324+1"
+stretch_version="~bpo90+20160324+1"
+wily_version="~bpo1510+20160324+1"
+xenial_version="~bpo1604+20160324+1"
