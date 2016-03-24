93c62dda7eeddba775753155fd7a2606f0abcfe6
Robert Nelson <robertcnelson@gmail.com>
2016-03-24 13:21:30 -0500

    debugss-mod-dkms (1.3.0-git20160314-0rcnee1~bpo80+20160324+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-debugss-mod-dkms/version.sh
+++ ti-debugss-mod-dkms/version.sh
@@ -4,19 +4,21 @@
 
 package_name="debugss-mod-dkms"
 debian_pkg_name="${package_name}"
-package_version="1.0.1-git20150710"
+package_version="1.3.0-git20160314"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/sdo-emu/dsptop.git"
-git_sha="edf5670075cf6cf42f46654d284f9f78c1c123a1"
+git_sha="4d92879b632ba70402b61fa65cc58f5373eb5217"
 reprepro_dir="d/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
+local_patch="rcnee1"
 
-wheezy_version="~bpo70+20151015+1"
-jessie_version="~bpo80+20151015+1"
-stretch_version="~bpo90+20151015+1"
+jessie_version="~bpo80+20160324+1"
+stretch_version="~bpo90+20160324+1"
+wily_version="~bpo1510+20160324+1"
+xenial_version="~bpo1604+20160324+1"
