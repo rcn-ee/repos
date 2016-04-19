1adcdb02552f52e7604020cf273ad096fd6112c8
Robert Nelson <robertcnelson@gmail.com>
2016-04-18 21:00:51 -0500

    connman (1.32-git20160418-0rcnee1~bpo80+20160418+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-connman/version.sh
+++ debian-8-connman/version.sh
@@ -4,19 +4,19 @@
 
 package_name="connman"
 debian_pkg_name="${package_name}"
-package_version="1.31-git201604131"
+package_version="1.32-git20160418"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.kernel.org/pub/scm/network/connman/connman.git"
-git_sha="24a200efed42325b77512559dd638a548d425791"
+git_sha="57a31db51504f52a83b004dc3c2d3e012731b841"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
 debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
-local_patch="rcnee2"
+local_patch="rcnee1"
 
 jessie_version="~bpo80+20160418+1"
 stretch_version="~bpo90+20160418+1"
