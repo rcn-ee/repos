c9ce57c1a6d86f0c81742a7714b7ee9e32de6e01
Robert Nelson <robertcnelson@gmail.com>
2016-04-20 09:16:34 -0500

    libmraa (0.9.5-git20160420-0rcnee1~bpo80+20160420+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-libmraa/version.sh
+++ debian-8-libmraa/version.sh
@@ -4,12 +4,12 @@
 
 package_name="libmraa"
 debian_pkg_name="${package_name}"
-package_version="0.9.5-git20160410"
+package_version="0.9.5-git20160420"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/Pillar1989/mraa"
-git_sha="0cd186cae431d2c8fec1665b353ef22f524897b4"
+git_sha="d860fc8d4e8a38ad8cf62345dfeb6b53fe62afdb"
 reprepro_dir="libm/${package_name}"
 dl_path=""
 
@@ -18,4 +18,4 @@ debian_patch=""
 debian_diff=""
 local_patch="rcnee1"
 
-jessie_version="~bpo80+20160410+1"
+jessie_version="~bpo80+20160420+1"
