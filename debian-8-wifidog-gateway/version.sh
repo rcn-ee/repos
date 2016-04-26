df65c2df62a17de380297a9649c144b6baa584dc
Robert Nelson <robertcnelson@gmail.com>
2016-04-26 13:16:00 -0500

    wifidog-gateway (1.3.0-git20160426-0rcnee1~bpo80+20160426+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-8-wifidog-gateway/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+#https://github.com/wifidog/wifidog-gateway
+
+package_name="wifidog-gateway"
+debian_pkg_name="${package_name}"
+package_version="1.3.0-git20160426"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="https://github.com/wifidog/wifidog-gateway"
+git_sha="628b7ea360fe4b5cb676d0659e3b25eb8693f347"
+reprepro_dir="w/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_patch=""
+debian_diff=""
+local_patch="rcnee1"
+
+jessie_version="~bpo80+20160426+1"
