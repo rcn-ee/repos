baa9b942c9f1772bf5600b1a8ba20946c0384a2c
Robert Nelson <robertcnelson@gmail.com>
2016-04-27 14:59:50 -0500

    wpa (2.4~dev-git20150102-0rcnee1~bpo80+20160427+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-wpa-git/version.sh
+++ debian-8-wpa-git/version.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
 mirror="http://http.debian.net/debian"
-#https://w1.fi/cgit/hostap/log/?ofs=2650
+#https://w1.fi/cgit/hostap/log/?ofs=2600
 
 package_name="wpa"
 debian_pkg_name="${package_name}"
-package_version="2.4~dev-git20141229"
+package_version="2.4~dev-git20150102"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/rcn-ee/hostap"
-git_sha="4e70bbf1c68579b3223f1cac0ffcdbf1c52b4a55"
+git_sha="bbd89bfca0b446d605c4cc38cedf55eb81d0eed7"
 reprepro_dir="w/${package_name}"
 dl_path=""
 
