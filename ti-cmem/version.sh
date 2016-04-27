4999a2e926a561d3c2bdece1c340d8af5fbfc559
Robert Nelson <robertcnelson@gmail.com>
2016-04-27 14:45:41 -0500

    cmem (4.11.00.00-git20160427-0rcnee1~bpo80+20160427+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-cmem/version.sh
+++ ti-cmem/version.sh
@@ -4,12 +4,12 @@
 
 package_name="cmem"
 debian_pkg_name="${package_name}"
-package_version="4.10.02.06-git20160318"
+package_version="4.11.00.00-git20160427"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/ipc/ludev.git"
-git_sha="4d599728974e4786bf2e0494605e9d79ebddee79"
+git_sha="852e97b5584cbdb6aa0fdef02a7c4e245409bcb6"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
@@ -18,11 +18,11 @@ debian_patch=""
 debian_diff=""
 local_patch="rcnee1"
 
-#sed -i -e 's:4.10.02.03:4.10.02.04:g' ./debian/cmem-mod-dkms.install
-#sed -i -e 's:4.10.02.03:4.10.02.04:g' ./debian/control
-#sed -i -e 's:4.10.02.03:4.10.02.04:g' ./debian/cmem-mod-dkms.dkms
+#sed -i -e 's:4.10.02.06:4.11.00.00:g' ./debian/cmem-mod-dkms.install
+#sed -i -e 's:4.10.02.06:4.11.00.00:g' ./debian/control
+#sed -i -e 's:4.10.02.06:4.11.00.00:g' ./debian/cmem-mod-dkms.dkms
 
-jessie_version="~bpo80+20160324+1"
-stretch_version="~bpo90+20160324+1"
-wily_version="~bpo1510+20160324+1"
-xenial_version="~bpo1604+20160324+1"
+jessie_version="~bpo80+20160427+1"
+stretch_version="~bpo90+20160427+1"
+xenial_version="~bpo1604+20160427+1"
+yakkety_version="~bpo1610+20160427+1"
