e6e7e6fc0ab35c140d84e2349321143d9446f559
Robert Nelson <robertcnelson@gmail.com>
2016-05-02 18:37:48 -0500

    bb-doc-bone101-jekyll (1.20160502-0rcnee1~bpo80+20160502+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-doc-bone101-jekyll/version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+package_name="bb-doc-bone101-jekyll"
+debian_pkg_name="${package_name}"
+package_version="1.20160502"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee1"
+debian_untar=""
+debian_patch=""
+
+jessie_version="~bpo80+20160502+1"
