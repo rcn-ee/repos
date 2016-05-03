af41730c8d20bd9b5d68c9b3daf1839e2bc644ad
Robert Nelson <robertcnelson@gmail.com>
2016-05-03 10:03:50 -0500

    bb-bonescript-installer-beta (0.5.0~beta5-0rcnee1~bpo80+20160503+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-bonescript-installer-beta/version.sh
+++ bb-bonescript-installer-beta/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-bonescript-installer-beta"
 debian_pkg_name="${package_name}"
-package_version="0.5.0~beta4"
+package_version="0.5.0~beta5"
 package_source=""
 src_dir=""
 
@@ -11,11 +11,11 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
-local_patch="rcnee2"
 
-jessie_version="~bpo80+20160421+1"
-stretch_version="~bpo90+20160421+1"
-xenial_version="~bpo1604+20160421+1"
+jessie_version="~bpo80+20160503+1"
+stretch_version="~bpo90+20160503+1"
+xenial_version="~bpo1604+20160503+1"
+yakkety_version="~bpo1610+20160503+1"
