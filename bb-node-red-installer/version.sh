c00abfffc703e87f5f90122c0cb4ed7ebf66948f
Robert Nelson <robertcnelson@gmail.com>
2016-05-06 09:37:39 -0500

    bb-node-red-installer (0.13.4-0rcnee2~bpo80+20160506+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-node-red-installer/version.sh
+++ bb-node-red-installer/version.sh
@@ -11,10 +11,9 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0"
+debian_version="${package_version}-0rcnee2"
 debian_untar=""
 debian_patch=""
-local_patch="rcnee1"
 
 jessie_version="~bpo80+20160506+1"
 stretch_version="~bpo90+20160506+1"
