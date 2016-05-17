2e8b4760b874371d48429031f9b8b934ab8c42af
Robert Nelson <robertcnelson@gmail.com>
2016-05-17 09:47:41 -0500

    seeed-wificonfig-installer (0.1.5-0rcnee1~bpo80+20160517+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- seeed-wificonfig-installer/version.sh
+++ seeed-wificonfig-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="seeed-wificonfig-installer"
 debian_pkg_name="${package_name}"
-package_version="0.1.4"
+package_version="0.1.5"
 package_source=""
 src_dir=""
 
@@ -14,6 +14,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
-debian_dl_file="https://github.com/rcn-ee/npm-package-wificonfig/raw/master/deploy/wificonfig-0.1.4-bc0ac5e-v0.12.13.tar.xz"
+debian_dl_file="https://github.com/rcn-ee/npm-package-wificonfig/raw/master/deploy/wificonfig-0.1.5-8da2e95-v0.12.13.tar.xz"
 
-jessie_version="~bpo80+20160516+1"
+jessie_version="~bpo80+20160517+1"
