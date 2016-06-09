fafd94d14d3c3a6e77737dbea0b817253e83cd18
Robert Nelson <robertcnelson@gmail.com>
2016-06-09 11:15:51 -0500

    upm (0.7.0-git20160523-0rcnee1~bpo80+20160609+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-upm/version.sh
+++ debian-8-upm/version.sh
@@ -4,12 +4,12 @@
 
 package_name="upm"
 debian_pkg_name="${package_name}"
-package_version="0.5.1-git20160410"
+package_version="0.7.0-git20160523"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/Pillar1989/upm"
-git_sha="c6c0eb8907e92c805d8aee97babf8fd88ff11259"
+git_sha="34abfa9aea9423ca4832ab0fa7a2250761e0ace0"
 reprepro_dir="u/${package_name}"
 dl_path=""
 
@@ -17,5 +17,6 @@ debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 local_patch="rcnee1"
+sbuild_chroot="nodejs-v0.12.x"
 
-jessie_version="~bpo80+20160410+1"
+jessie_version="~bpo80+20160609+1"
