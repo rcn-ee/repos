105591a269bd8dd754bf5215619bb1458d937bcd
Robert Nelson <robertcnelson@gmail.com>
2016-06-10 16:29:55 -0500

    bb-node-red-installer (0.13.4-git20160531-0rcnee1~bpo80+20160610+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-node-red-installer/version.sh
+++ bb-node-red-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="0.13.4"
+package_version="0.13.4-git20160531"
 package_source=""
 src_dir=""
 
@@ -11,13 +11,13 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee7"
+debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
-debian_dl_1="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/node-red-0.13.4-3454e5a-v0.12.13.tar.xz"
-debian_dl_2="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/node-red-node-beaglebone-0.1.6-966e7a5-v0.12.13.tar.xz"
+debian_dl_1="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/node-red-0.13.4-f1ce0fa-v0.12.13.tar.xz"
+debian_dl_2="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/node-red-node-beaglebone-0.1.6-f91b7c4-v0.12.13.tar.xz"
 debian_dl_3="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/node-red-node-bb-upm-0.0.0-d817d14-v0.12.13.tar.xz"
-debian_dl_4="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/node-red-node-mongodb-0.0.5-966e7a5-v0.12.13.tar.xz"
+debian_dl_4="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/node-red-node-mongodb-0.0.5-f91b7c4-v0.12.13.tar.xz"
 
 jessie_version="~bpo80+20160610+1"
 stretch_version="~bpo90+20160610+1"
