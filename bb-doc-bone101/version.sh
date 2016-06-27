c409cfb15db3379b9fdaba5d1b3ba0b0661c7605
Jason Kridner <jkridner@beagleboard.org>
2016-06-27 00:34:22 +0000

    bone101: update to 1.1.5

--- bb-doc-bone101/version.sh
+++ bb-doc-bone101/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bone101"
 debian_pkg_name="${package_name}"
-package_version="1.1.4"
+package_version="1.1.5"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -15,4 +15,4 @@ debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~bpo80+20160622+1"
+jessie_version="~bpo80+20160626+1"
