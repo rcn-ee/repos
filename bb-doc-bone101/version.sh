d165c55846d9d4ec9fce3910905d8888c6e4e726
Robert Nelson <robertcnelson@gmail.com>
2016-06-30 15:21:04 -0500

    bone101 (1.1.6-0rcnee1~bpo80+20160630+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-doc-bone101/version.sh
+++ bb-doc-bone101/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bone101"
 debian_pkg_name="${package_name}"
-package_version="1.1.5"
+package_version="1.1.6"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -11,9 +11,9 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
 sbuild_chroot="nodejs-v0.12.x"
 
-jessie_version="~bpo80+20160626+1"
+jessie_version="~bpo80+20160630+1"
