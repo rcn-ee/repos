996f4499df4c7391858d6993fec373ff44b8ee30
Robert Nelson <robertcnelson@gmail.com>
2016-07-01 16:44:14 -0500

    bone101 (1.1.6-0rcnee10~bpo80+20160701+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-doc-bone101/version.sh
+++ bb-doc-bone101/version.sh
@@ -11,9 +11,12 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee9"
+debian_version="${package_version}-0rcnee10"
 debian_untar=""
 debian_patch=""
 sbuild_chroot="nodejs-v0.12.x"
+debian_dl_1="https://github.com/rcn-ee/npm-package-bb-doc-bone101/raw/master/deploy/async-2.0.0-rc.6-v0.12.15.tar.xz"
+debian_dl_2="https://github.com/rcn-ee/npm-package-bb-doc-bone101/raw/master/deploy/sensortag-1.2.2-v0.12.15.tar.xz"
+
 
 jessie_version="~bpo80+20160701+1"
