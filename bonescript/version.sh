bb7e4c09b5ebab24e0b1065332dd3920bbae0a87
Robert Nelson <robertcnelson@gmail.com>
2016-07-08 10:44:21 -0500

    bonescript (0.5.0-0rcnee9~bpo80+20160708+1)

--- bonescript/version.sh
+++ bonescript/version.sh
@@ -11,10 +11,10 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee8"
+debian_version="${package_version}-0rcnee9"
 debian_untar=""
 debian_patch=""
 sbuild_chroot="nodejs-v0.12.x"
-debian_dl_1="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/bonescript-0.5.0-v0.12.15.tar.xz"
+debian_dl_1="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/bonescript-0.5.0-v0.12.15-rcnee1.tar.xz"
 
-jessie_version="~bpo80+20160707+1"
+jessie_version="~bpo80+20160708+1"
