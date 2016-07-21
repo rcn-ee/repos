b4237264d17955350a1bc14eb95e412f211486b7
Robert Nelson <robertcnelson@gmail.com>
2016-07-21 10:50:46 -0500

    debian-bpo8-e2fsprogs
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo8-e2fsprogs/version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="e2fsprogs"
+debian_pkg_name="${package_name}"
+package_version="1.43"
+
+git_repo=""
+git_sha=""
+reprepro_dir="e/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-3~bpo8+1"
+debian_untar=""
+debian_patch=""
