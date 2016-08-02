722d4acde6f12a2ca76ccfef1fd975428fc97c43
Dimitar Dimitrov <dinuxbg@gmail.com>
2016-08-02 20:39:52 +0300

    Add gcc-pru package
    Note that binutils-pru must be accessible to sbuild.
    
    Commands used for building:
      cd gcc-pru
      ./generate_source.sh
      ../tools/generate_dsc.sh
      ../tools/push_dsc_out.sh
      ../tools/build_deb.sh
    
    Signed-off-by: Dimitar Dimitrov <dinuxbg@gmail.com>

---
+++ gcc-pru/version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+package_name="gcc-pru"
+debian_pkg_name="${package_name}"
+gnupru_release="2016.07-beta-rc1.1"
+package_version="7.0.0.20160801"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee0"
+debian_untar=""
+debian_patch=""
+
+jessie_version="-1"
