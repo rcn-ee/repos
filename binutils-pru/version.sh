76dc09791f8a754c7d4b0936a46f669b7972e7fd
Robert Nelson <robertcnelson@gmail.com>
2016-08-02 08:12:36 -0500

    binutils-pru (2.27.51.20160801-0rcnee0~bpo80+20160802+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- binutils-pru/version.sh
+++ binutils-pru/version.sh
@@ -12,8 +12,8 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version=""
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="-1"
+jessie_version="~bpo80+20160802+1"
