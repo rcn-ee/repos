4684ddda6d726bd06b20fa31cee90a50b168c77d
Robert Nelson <robertcnelson@gmail.com>
2016-08-05 09:50:46 -0500

    gcp-iot-demo (0.0.1-0rcnee1~bpo80+20160805+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- gcp-iot-demo/version.sh
+++ gcp-iot-demo/version.sh
@@ -11,10 +11,10 @@ git_sha=""
 reprepro_dir="g/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
 debian_dl_1="https://github.com/rcn-ee/npm-package-gcp-iot-demo/raw/a768d8dd1366726980acba4771ff7dfb3565c1c8/deploy/gcp-iot-demo-0.0.1-fb406df-v0.12.15.tar.xz"
 
 
-jessie_version="~bpo80+20160802+1"
+jessie_version="~bpo80+20160805+1"
