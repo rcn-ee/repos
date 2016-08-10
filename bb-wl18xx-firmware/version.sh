e97c08378fa8c6d2daba7ec59e4ef064f5a3e646
Robert Nelson <robertcnelson@gmail.com>
2016-08-10 14:45:43 -0500

    bb-wl18xx-firmware (1.20160810-0rcnee1~bpo80+20160810+1) (2x2 MIMO mode)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/version.sh
+++ bb-wl18xx-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wl18xx-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20160728"
+package_version="1.20160810"
 package_source=""
 src_dir=""
 
@@ -15,7 +15,7 @@ debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~bpo80+20160728+1"
-stretch_version="~bpo90+20160728+1"
-xenial_version="~bpo1604+20160728+1"
-yakkety_version="~bpo1610+20160728+1"
+jessie_version="~bpo80+20160810+1"
+stretch_version="~bpo90+20160810+1"
+xenial_version="~bpo1604+20160810+1"
+yakkety_version="~bpo1610+20160810+1"
