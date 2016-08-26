1f179d34bfe4187bc94c8fa942d3e31daf354cd9
Robert Nelson <robertcnelson@gmail.com>
2016-08-26 11:11:02 -0500

    llvm: 3.8.1-9
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -93,7 +93,8 @@ mesa () {
 	site="${debian_pool}/main/l"
 	package_name="llvm-toolchain-3.6" ; package_version="${package_name}_3.6.2-3" ; check_http
 	package_name="llvm-toolchain-3.7" ; package_version="${package_name}_3.7.1-2" ; check_http
-	package_name="llvm-toolchain-3.8" ; package_version="${package_name}_3.8.1-3" ; check_http
+	package_name="llvm-toolchain-3.8" ; package_version="${package_name}_3.8.1-9" ; check_http
+	package_name="llvm-toolchain-3.9" ; package_version="${package_name}_3.9~+rc3-1~exp1" ; check_http
 	package_name="llvm-toolchain-snapshot" ; package_version="${package_name}_3.9~svn262954-1" ; check_http
 
 	site="${debian_pool}/main/libc"
