fef37d5d48e02377492085ff9487faff7d8a0e2c
Robert Nelson <robertcnelson@gmail.com>
2016-08-26 16:07:41 -0500

    libdrm (2.4.70-1rcnee1~bpo80+20160826+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- libdrm/sync_etnaviv.sh
+++ libdrm/sync_etnaviv.sh
@@ -3,12 +3,12 @@
 mkdir -p ./ignore/etnaviv
 
 if [ ! -f ./ignore/etnaviv/.git/config ] ; then
-	git clone https://github.com/austriancoder/libdrm ./ignore/etnaviv
+	git clone -b libdrm_etnaviv_v1 https://github.com/austriancoder/libdrm ./ignore/etnaviv
 fi
 
 cd ./ignore/etnaviv/
 git pull
 
-git diff c3496167637e35cf8a52d5e7e53a412e79d80db0...HEAD > ../../0001-etnaviv-base.diff
+git diff b214b05ccd433c484a6a65e491a1a51b19e4811d...HEAD > ../../0001-etnaviv-base.diff
 
 cd ../../
