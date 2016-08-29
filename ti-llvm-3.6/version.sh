99c0295a21f19f068a959aba7ebade5cbddfe199
Robert Nelson <robertcnelson@gmail.com>
2016-08-29 10:40:28 -0500

    ti-llvm-3.6 (3.6-git20160808-0rcnee1~bpo80+20160829+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-llvm-3.6/version.sh
+++ ti-llvm-3.6/version.sh
@@ -1,22 +1,23 @@
 #!/bin/bash -e
 
+#http://git.ti.com/gitweb/?p=opencl/llvm.git;a=shortlog;h=refs/heads/release_36_ti
 #http://git.ti.com/gitweb/?p=opencl/llvm.git;a=shortlog;h=refs/tags/v01.01.07.01
 
 package_name="ti-llvm-3.6"
 debian_pkg_name="${package_name}"
-package_version="3.6-git20151112"
+package_version="3.6-git20160808"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-3.6"
 
 git_repo="git://git.ti.com/opencl/llvm.git"
-git_sha="c7f6029e454b70dffa3d04d500966a6ecf24a1fe"
+git_sha="798386e2513c812ab4b01df5b57c88b225e756bb"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee4"
+debian_version="${package_version}-0rcnee1"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~bpo80+20160707+1"
-stretch_version="~bpo90+20160707+1"
-xenial_version="~bpo1604+20160707+1"
+jessie_version="~bpo80+20160829+1"
+stretch_version="~bpo90+20160829+1"
+xenial_version="~bpo1604+20160829+1"
