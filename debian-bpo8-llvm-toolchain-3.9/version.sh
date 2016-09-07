acd23ea274d4f719bea1256bc97790c8308a7247
Robert Nelson <robertcnelson@gmail.com>
2016-09-07 13:35:15 -0500

    llvm: 3.9-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo8-llvm-toolchain-3.9/version.sh
+++ debian-bpo8-llvm-toolchain-3.9/version.sh
@@ -4,13 +4,13 @@ mirror="http://http.debian.net/debian"
 
 package_name="llvm-toolchain-3.9"
 debian_pkg_name="${package_name}"
-package_version="3.9~+rc3"
+package_version="3.9"
 
 git_repo=""
 git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1~exp1"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
