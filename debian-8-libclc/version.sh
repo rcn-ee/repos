7095df5ea30d5bdd3642148ff4e006f04af72f60
Robert Nelson <robertcnelson@gmail.com>
2016-09-13 09:38:09 -0500

    libclc (0.2.0+git20150813-3~bpo80+20160913+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-libclc/version.sh
+++ debian-8-libclc/version.sh
@@ -14,8 +14,8 @@ git_sha=""
 reprepro_dir="libc/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-3"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
-jessie_version="~bpo80+20151029+1"
+jessie_version="~bpo80+20160913+1"
