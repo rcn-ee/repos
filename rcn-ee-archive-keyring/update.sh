076511602b705acef3c101190b72a4972bfbc0ac
Robert Nelson <robertcnelson@gmail.com>
2016-10-18 09:47:11 -0500

    rcn-ee-archive-keyring (2016.10.18~bpo80+20161018+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- rcn-ee-archive-keyring/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-tar xf rcn-ee-archive-keyring_2015.10.22.orig.tar.xz
-mv rcn-ee-archive-keyring_2015.10.22 rcn-ee-archive-keyring_2016.04.24
-tar -cJf rcn-ee-archive-keyring_2016.04.24.orig.tar.xz rcn-ee-archive-keyring_2016.04.24/
