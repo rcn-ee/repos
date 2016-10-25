a8da3d2e3a6e08404897f3bdce766a8ad95ac4bf
Robert Nelson <robertcnelson@gmail.com>
2016-10-25 15:20:25 -0500

    llvm-toolchain-3.8 3.8.1-13
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo8-llvm-toolchain-3.8/version.sh
+++ debian-bpo8-llvm-toolchain-3.8/version.sh
@@ -11,6 +11,6 @@ git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-12"
+debian_version="${package_version}-13"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
