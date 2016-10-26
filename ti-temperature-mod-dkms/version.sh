af3c2f19e7922ccb8fcc2e019e83dae786b54372
Robert Nelson <robertcnelson@gmail.com>
2016-10-26 10:56:08 -0500

    temperature-mod-dkms (1.0.1-git20160915-0rcnee1~bpo80+20161026+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-temperature-mod-dkms/version.sh
+++ ti-temperature-mod-dkms/version.sh
@@ -4,12 +4,12 @@
 
 package_name="temperature-mod-dkms"
 debian_pkg_name="${package_name}"
-package_version="1.0.1-git20160314"
+package_version="1.0.1-git20160915"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/sdo-emu/dsptop.git"
-git_sha="4d92879b632ba70402b61fa65cc58f5373eb5217"
+git_sha="0aedcabdbbcbfc7558f70fc50afc95583302a087"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
@@ -18,7 +18,6 @@ debian_patch=""
 debian_diff=""
 local_patch="rcnee1"
 
-jessie_version="~bpo80+20160324+1"
-stretch_version="~bpo90+20160324+1"
-wily_version="~bpo1510+20160324+1"
-xenial_version="~bpo1604+20160324+1"
+jessie_version="~bpo80+20161026+1"
+stretch_version="~bpo90+20161026+1"
+xenial_version="~bpo1604+20161026+1"
