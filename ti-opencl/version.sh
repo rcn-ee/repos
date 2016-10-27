fa95a960f5e334d5423b85fcf0be11b589b1d9d4
Robert Nelson <robertcnelson@gmail.com>
2016-10-27 12:58:37 -0500

    ti-opencl (01.01.10.03-0rcnee1~bpo80+20161027+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-opencl/version.sh
+++ ti-opencl/version.sh
@@ -4,19 +4,19 @@
 
 package_name="ti-opencl"
 debian_pkg_name="${package_name}"
-package_version="01.01.06.00"
+package_version="01.01.10.03"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/opencl/ti-opencl.git"
-git_sha="992dfba8783af895f6d4e4877ad7c0466d085bf0"
+git_sha="108ff29cd27e9af39c1f0b4945cb5dd1be7fed41"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-0rcnee1"
 debian_patch=""
 debian_diff=""
 
-wheezy_version="~bpo70+20151020+1"
-jessie_version="~bpo80+20151020+1"
-stretch_version="~bpo90+20151020+1"
+jessie_version="~bpo80+20161027+1"
+stretch_version="~bpo90+20161027+1"
+xenial_version="~bpo1604+20161027+1"
