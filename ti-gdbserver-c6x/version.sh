9251aea2ac44ac76583ea446698146c9a656b65a
Robert Nelson <robertcnelson@gmail.com>
2016-10-27 11:51:36 -0500

    gdbserver-c6x (1.1.0-git20160327-1rcnee0~bpo80+20161027+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-gdbserver-c6x/version.sh
+++ ti-gdbserver-c6x/version.sh
@@ -4,19 +4,19 @@
 
 package_name="gdbserver-c6x"
 debian_pkg_name="${package_name}"
-package_version="1.1.0-git20150805"
+package_version="1.1.0-git20160327"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/sdo-emu/gdbc6x.git"
-git_sha="653f7d0a345bede0542326090b622b885545c712"
+git_sha="df0b8f6f4e9ba5b9c94ea8c17c6dfeae37c1c9d1"
 reprepro_dir="g/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-1.1"
+debian_version="${package_version}-1rcnee0"
 debian_patch=""
 debian_diff=""
 
-wheezy_version="~bpo70+20151016+1"
-jessie_version="~bpo80+20151016+1"
-stretch_version="~bpo90+20151016+1"
+jessie_version="~bpo80+20161027+1"
+stretch_version="~bpo90+20161027+1"
+xenial_version="~bpo1604+20161027+1"
