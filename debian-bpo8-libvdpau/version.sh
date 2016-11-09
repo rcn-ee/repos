311b72343fc420b0cee579a7216f87ec57accea2
Robert Nelson <robertcnelson@gmail.com>
2016-11-09 15:35:00 -0600

    libvdpau 1.1.1-5
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo8-libvdpau/version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="libvdpau"
+debian_pkg_name="${package_name}"
+package_version="1.1.1-5"
+
+git_repo=""
+git_sha=""
+reprepro_dir="libv/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}"
+debian_untar=""
+debian_patch=""
--- mesa/version.sh
+++ mesa/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="12.0.3"
+package_version="13.0.0"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -16,7 +16,7 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
-local_patch="rcnee1"
+local_patch="rcnee0"
 
 jessie_version="~bpo80+20160921+1"
 stretch_version="~bpo90+20160921+1"
