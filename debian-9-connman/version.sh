76d4895dfad7adc8c0162b48e12fe7a08a2b825e
Robert Nelson <robertcnelson@gmail.com>
2016-11-11 15:18:07 -0600

    add connman 1.33-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-9-connman/version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="connman"
+debian_pkg_name="${package_name}"
+package_version="1.33"
+package_source="${package_name}_${package_version}.orig.tar.gz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
