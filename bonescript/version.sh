de5f2a040a912f473d16039b6fe306355bb236c9
Robert Nelson <robertcnelson@gmail.com>
2016-11-15 12:04:16 -0600

    bonescript (0.5.1-0rcnee1~bpo80+20161115+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bonescript/version.sh
+++ bonescript/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bonescript"
 debian_pkg_name="${package_name}"
-package_version="0.5.0"
+package_version="0.5.1"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -11,10 +11,12 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee17"
+debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
 sbuild_chroot="nodejs-v0.12.x"
-debian_dl_1="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/bonescript-0.5.0-v0.12.15-rcnee1.tar.xz"
+debian_dl_1="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/bonescript-0.5.2-491eff8-v0.12.17.tar.xz"
+debian_dl_2="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/bonescript-0.5.2-491eff8-v4.6.2.tar.xz"
+debian_dl_3="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/bonescript-0.5.2-491eff8-v6.9.1.tar.xz"
 
-jessie_version="~bpo80+20160709+1"
+jessie_version="~bpo80+20161115+1"
