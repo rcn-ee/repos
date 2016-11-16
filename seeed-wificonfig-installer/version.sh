74180686d77d411faf182e870f84b4b81dcaa687
Robert Nelson <robertcnelson@gmail.com>
2016-11-16 15:12:19 -0600

    seeed-wificonfig-installer (0.1.5-0rcnee11~bpo80+20161116+1)

--- seeed-wificonfig-installer/version.sh
+++ seeed-wificonfig-installer/version.sh
@@ -11,7 +11,7 @@ git_sha=""
 reprepro_dir="s/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee10"
+debian_version="${package_version}-0rcnee11"
 debian_untar=""
 debian_patch=""
 
