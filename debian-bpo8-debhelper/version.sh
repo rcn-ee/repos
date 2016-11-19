1ef2f0057ad673e5e8bbb7f357617a67a109ae45
Robert Nelson <robertcnelson@gmail.com>
2016-11-19 17:49:15 -0600

    debhelper 10.2.2~bpo8+1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo8-debhelper/version.sh
+++ debian-bpo8-debhelper/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="debhelper"
 debian_pkg_name="${package_name}"
-package_version="9.20160403~bpo8+1"
+package_version="10.2.2~bpo8+1"
 
 git_repo=""
 git_sha=""
