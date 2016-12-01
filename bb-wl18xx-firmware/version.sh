94fa31caa09e4132194b39e0a3df67c85c581cfa
Robert Nelson <robertcnelson@gmail.com>
2016-12-01 11:15:23 -0600

    bb-wl18xx-firmware (1.20161201-0rcnee1~bpo80+20161201+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/version.sh
+++ bb-wl18xx-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wl18xx-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20161130"
+package_version="1.20161201"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,8 @@ debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~bpo80+20161130+1"
-stretch_version="~bpo90+20161130+1"
-xenial_version="~bpo1604+20161130+1"
-yakkety_version="~bpo1610+20161130+1"
-zesty_version="~bpo1704+20161130+1"
+jessie_version="~bpo80+20161201+1"
+stretch_version="~bpo90+20161201+1"
+xenial_version="~bpo1604+20161201+1"
+yakkety_version="~bpo1610+20161201+1"
+zesty_version="~bpo1704+20161201+1"
