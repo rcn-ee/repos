8e46a404eec805027d6e258aa87dc3f080a93cc0
Robert Nelson <robertcnelson@gmail.com>
2016-12-07 11:15:54 -0600

    v6
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-c9-core-installer-test/version.sh
+++ bb-c9-core-installer-test/version.sh
@@ -21,6 +21,7 @@ local_patch="rcnee0"
 debian_dl_1="https://rcn-ee.com/repos/git/archive/c9-npmbox/virtualenv-12.0.7.tar.gz"
 debian_dl_2="https://rcn-ee.net/repos/git/archive/c9-core_v0.12.x/c9-core_${package_version}-v0.12.17-build.tar.xz"
 debian_dl_3="https://rcn-ee.net/repos/git/archive/c9-core_v4.x/c9-core_${package_version}-v4.7.0-build.tar.xz"
+debian_dl_3="https://rcn-ee.net/repos/git/archive/c9-core_v6.x/c9-core_${package_version}-v6.9.2-build.tar.xz"
 
 stretch_version="~bpo90+20161114+1"
 xenial_version="~bpo1604+20161114+1"
