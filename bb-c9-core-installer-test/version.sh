6bb119bc023fd20088898e8c01d33d6fdb408a6e
Robert Nelson <robertcnelson@gmail.com>
2016-12-07 10:04:56 -0600

    test: 3.1.3306
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-c9-core-installer-test/version.sh
+++ bb-c9-core-installer-test/version.sh
@@ -4,7 +4,7 @@
 
 package_name="c9-core-installer"
 debian_pkg_name="${package_name}"
-package_version="3.1.3168+git20161016"
+package_version="3.1.3306+git20161130"
 package_source=""
 src_dir=""
 
@@ -18,6 +18,9 @@ debian_untar=""
 debian_patch=""
 local_patch="rcnee0"
 
-jessie_version="~bpo80+20161114+1"
+debian_dl_1="https://rcn-ee.com/repos/git/archive/c9-npmbox/virtualenv-12.0.7.tar.gz"
+debian_dl_2="https://rcn-ee.net/repos/git/archive/c9-core_v0.12.x/c9-core_3.1.3306+git20161130-v0.12.17-build.tar.xz"
+
 stretch_version="~bpo90+20161114+1"
 xenial_version="~bpo1604+20161114+1"
+jessie_version="~bpo80+20161207+1"
