ba38300ff5bb6972cf5df61f4436327f2ed346fa
Robert Nelson <robertcnelson@gmail.com>
2016-12-07 10:12:44 -0600

    drop gzip
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-c9-core-installer-test/version.sh
+++ bb-c9-core-installer-test/version.sh
@@ -19,7 +19,7 @@ debian_patch=""
 local_patch="rcnee0"
 
 debian_dl_1="https://rcn-ee.com/repos/git/archive/c9-npmbox/virtualenv-12.0.7.tar.gz"
-debian_dl_2="https://rcn-ee.net/repos/git/archive/c9-core_v0.12.x/c9-core_3.1.3306+git20161130-v0.12.17-build.tar.xz"
+debian_dl_2="https://rcn-ee.net/repos/git/archive/c9-core_v0.12.x/c9-core_${package_version}-v0.12.17-build.tar.xz"
 
 stretch_version="~bpo90+20161114+1"
 xenial_version="~bpo1604+20161114+1"
