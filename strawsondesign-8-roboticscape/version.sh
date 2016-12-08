5f72616e1b1605b38a6f316e69f801a293650042
Robert Nelson <robertcnelson@gmail.com>
2016-12-08 13:47:47 -0600

    roboticscape (0.2.2-git20161122-0rcnee1~bpo80+20161208+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- strawsondesign-8-roboticscape/version.sh
+++ strawsondesign-8-roboticscape/version.sh
@@ -13,8 +13,8 @@ git_sha="8a6ec2d140cf718e7aa7f24b935c34edb7cf7cc3"
 reprepro_dir="r/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-0rcnee1"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~bpo80+20161122+1"
+jessie_version="~bpo80+20161208+1"
