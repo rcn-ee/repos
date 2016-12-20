eb526d5b16a6b05867328d3690434ffbbd2bccc0
Robert Nelson <robertcnelson@gmail.com>
2016-12-20 16:54:00 -0600

    ros-roslisp 1.9.20-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo8-ros-roslisp/version.sh
+++ debian-bpo8-ros-roslisp/version.sh
@@ -4,9 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="ros-roslisp"
 debian_pkg_name="${package_name}"
-package_version="1.9.19"
-package_source="${package_name}_${package_version}.orig.tar.gz"
-src_dir="${package_name}-${package_version}"
+package_version="1.9.20"
 
 git_repo=""
 git_sha=""
@@ -14,8 +12,10 @@ reprepro_dir="r/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1"
-debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_untar=""
 debian_patch=""
-local_patch="rcnee1"
 
-jessie_version="~bpo80+20160224+1"
+dist="debian"
+suite="jessie"
+deb_arch="armhf"
+
