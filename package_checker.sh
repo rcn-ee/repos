3baf57e68f7824d6338308ab1fd8b91c09be5124
Robert Nelson <robertcnelson@gmail.com>
2016-12-20 14:50:42 -0600

    ros-ros 1.13.4-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -351,7 +351,7 @@ ros () {
 	package_name="ros-common-msgs"; package_version="${package_name}_1.12.5-2"; check_http
 	package_name="ros-ros-comm-msgs"; package_version="${package_name}_1.11.2-5"; check_http
 	package_name="ros-rospack"; package_version="${package_name}_2.3.1-1"; check_http
-	package_name="ros-ros"; package_version="${package_name}_1.12.5-4"; check_http
+	package_name="ros-ros"; package_version="${package_name}_1.13.4-1"; check_http
 
 	site="${debian_pool}/main/l"
 	package_name="log4cxx"; package_version="${package_name}_0.10.0-10"; check_http
