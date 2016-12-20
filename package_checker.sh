3504f8cd77b5db70ec28f0f9a0db159d348cc4d3
Robert Nelson <robertcnelson@gmail.com>
2016-12-20 14:05:38 -0600

    ros-roscpp-core 0.6.1-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -346,7 +346,7 @@ ros () {
 	package_name="ros-genpy"; package_version="${package_name}_0.6.3-1"; check_http
 	package_name="ros-gencpp"; package_version="${package_name}_0.5.5-2"; check_http
 	package_name="ros-genlisp"; package_version="${package_name}_0.4.16-1"; check_http
-	package_name="ros-roscpp-core"; package_version="${package_name}_0.5.6-2"; check_http
+	package_name="ros-roscpp-core"; package_version="${package_name}_0.6.1-1"; check_http
 	package_name="ros-std-msgs"; package_version="${package_name}_0.5.9-2"; check_http
 	package_name="ros-common-msgs"; package_version="${package_name}_1.12.4-1"; check_http
 	package_name="ros-ros-comm-msgs"; package_version="${package_name}_1.11.1-3"; check_http
