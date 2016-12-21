0f11421efc690d446cd3234043264abd1b34e9e5
Robert Nelson <robertcnelson@gmail.com>
2016-12-21 09:45:45 -0600

    orocos-kdl 1.3.1+dfsg-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo8-orocos-kdl/version.sh
+++ debian-bpo8-orocos-kdl/version.sh
@@ -4,9 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="orocos-kdl"
 debian_pkg_name="${package_name}"
-package_version="1.3.0+dfsg"
-package_source="${package_name}_${package_version}.orig.tar.xz"
-src_dir="orocos_kinematics_dynamics-1.3.0"
+package_version="1.3.1+dfsg"
 
 git_repo=""
 git_sha=""
@@ -14,8 +12,10 @@ reprepro_dir="o/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1"
-debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_untar=""
 debian_patch=""
-local_patch=""
 
-jessie_version="~bpo80+20160118+1"
+dist="debian"
+suite="jessie"
+deb_arch="armhf"
+
--- package_checker.sh
+++ package_checker.sh
@@ -365,7 +365,7 @@ ros () {
 	package_name="ros-rosconsole-bridge"; package_version="${package_name}_0.4.4-1"; check_http
 
 	site="${debian_pool}/main/o"
-	package_name="orocos-kdl"; package_version="${package_name}_1.3.0+dfsg-1"; check_http
+	package_name="orocos-kdl"; package_version="${package_name}_1.3.1+dfsg-1"; check_http
 
 	site="${debian_pool}/main/r"
 	package_name="ros-geometry-experimental"; package_version="${package_name}_0.5.12-2"; check_http
