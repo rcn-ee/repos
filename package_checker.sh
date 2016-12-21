f829abe3a9dc0dc063ab8cff1960d601685127f2
Robert Nelson <robertcnelson@gmail.com>
2016-12-21 08:59:54 -0600

    ros-actionlib 1.11.7-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -359,7 +359,7 @@ ros () {
 	site="${debian_pool}/main/r"
 	package_name="ros-ros-comm"; package_version="${package_name}_1.12.6-2"; check_http
 	package_name="ros-roslisp"; package_version="${package_name}_1.9.20-1"; check_http
-	package_name="ros-actionlib"; package_version="${package_name}_1.11.4-3"; check_http
+	package_name="ros-actionlib"; package_version="${package_name}_1.11.7-1"; check_http
 	package_name="ros-dynamic-reconfigure"; package_version="${package_name}_1.5.39-3"; check_http
 	package_name="ros-angles"; package_version="${package_name}_1.9.10-1"; check_http
 	package_name="ros-rosconsole-bridge"; package_version="${package_name}_0.4.2-2"; check_http
