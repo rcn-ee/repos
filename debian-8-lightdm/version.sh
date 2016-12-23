e811043cb3afa121b19406b784363b509ea57162
Robert Nelson <robertcnelson@gmail.com>
2016-12-23 09:37:57 -0600

    lightdm (1.10.3-3rcnee1~bpo80+20161223+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-8-lightdm/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="lightdm"
+debian_pkg_name="${package_name}"
+package_version="1.10.3"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="l/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-3"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="rcnee1"
+
+jessie_version="~bpo80+20161223+1"
