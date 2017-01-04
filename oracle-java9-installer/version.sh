ba5d63f271700b23de4a66ba7abd0e24452562e5
Robert Nelson <robertcnelson@gmail.com>
2017-01-04 09:19:52 -0600

    oracle-java9-installer (9b149+9b149arm-0rcnee0~bpo80+20170104+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- oracle-java9-installer/version.sh
+++ oracle-java9-installer/version.sh
@@ -2,9 +2,11 @@
 
 #https://launchpad.net/~webupd8team/+archive/ubuntu/java/+packages
 
+#wget https://launchpad.net/~webupd8team/+archive/ubuntu/java/+files/oracle-java9-installer_9b149+9b149arm-1~webupd8~0.tar.xz
+
 package_name="oracle-java9-installer"
 debian_pkg_name="${package_name}"
-package_version="9b112+9b112arm"
+package_version="9b149+9b149arm"
 package_source=""
 src_dir=""
 
@@ -16,11 +18,9 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
-local_patch="rcnee1"
+local_patch="rcnee0"
 
-wheezy_version="~bpo70+20160420+1"
-jessie_version="~bpo80+20160420+1"
-stretch_version="~bpo90+20160420+1"
-trusty_version="~bpo1404+20160420+1"
-wily_version="~bpo1510+20160420+1"
-xenial_version="~bpo1604+20160420+1"
+jessie_version="~bpo80+20170104+1"
+stretch_version="~bpo90+20170104+1"
+trusty_version="~bpo1404+20170104+1"
+xenial_version="~bpo1604+20170104+1"
