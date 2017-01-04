b813610aabf253093dfa4bc454956811eb383fa6
Robert Nelson <robertcnelson@gmail.com>
2017-01-04 09:14:16 -0600

    oracle-java8-installer (8u111+8u111-0rcnee0~bpo80+20170104+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- oracle-java8-installer/version.sh
+++ oracle-java8-installer/version.sh
@@ -2,11 +2,11 @@
 
 #https://launchpad.net/~webupd8team/+archive/ubuntu/java/+packages
 
-#wget https://launchpad.net/~webupd8team/+archive/ubuntu/java/+files/oracle-java8-installer_8u74+8u73arm-1~webupd8~1.tar.xz
+#wget https://launchpad.net/~webupd8team/+archive/ubuntu/java/+files/oracle-java8-installer_8u111+8u111arm-1~webupd8~0.tar.xz
 
 package_name="oracle-java8-installer"
 debian_pkg_name="${package_name}"
-package_version="8u77+8u77arm"
+package_version="8u111+8u111"
 package_source=""
 src_dir=""
 
@@ -18,11 +18,9 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
-local_patch="rcnee1"
+local_patch="rcnee0"
 
-wheezy_version="~bpo70+20160420+1"
-jessie_version="~bpo80+20160420+1"
-stretch_version="~bpo90+20160420+1"
-trusty_version="~bpo1404+20160420+1"
-wily_version="~bpo1510+20160420+1"
-xenial_version="~bpo1604+20160420+1"
+jessie_version="~bpo80+20170104+1"
+stretch_version="~bpo90+20170104+1"
+trusty_version="~bpo1404+20170104+1"
+xenial_version="~bpo1604+20170104+1"
