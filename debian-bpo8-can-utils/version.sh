832c77a4aa6390ce6471f60fb552107740fbb4e1
Robert Nelson <robertcnelson@gmail.com>
2017-01-16 14:21:25 -0600

    can-utils (0.0+git20160831-1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo8-can-utils/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="can-utils"
+debian_pkg_name="${package_name}"
+package_version="0.0+git20160831"
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar=""
+debian_patch=""
+
+dist="debian"
+suite="jessie"
+deb_arch="armhf"
+
