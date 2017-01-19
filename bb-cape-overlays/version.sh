4f8800760eab527acd4f2fb68a3fba9c60073a9c
Robert Nelson <robertcnelson@gmail.com>
2017-01-19 10:56:34 -0600

    bb-cape-overlays (4.1.20170119-0rcnee1~bpo80+20170119+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-cape-overlays/version.sh
+++ bb-cape-overlays/version.sh
@@ -4,12 +4,12 @@
 
 package_name="bb-cape-overlays"
 debian_pkg_name="${package_name}"
-package_version="4.1.20170117"
+package_version="4.1.20170119"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/beagleboard/bb.org-overlays"
-git_sha="8383799707842f864f69fbd68f332d9c58fa3e2e"
+git_sha="38fe4bfb6d99c24e8a368a73869389cfc9485229"
 reprepro_dir="b/${package_name}"
 dl_path=""
 
