11f8bfab37728b7a837f3a78573fe040a784bc85
Robert Nelson <robertcnelson@gmail.com>
2017-01-24 10:41:14 -0600

    rcn-ee-archive-keyring (2017.01.24~bpo1704+20170124+1) zesty
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- rcn-ee-archive-keyring/version.sh
+++ rcn-ee-archive-keyring/version.sh
@@ -2,9 +2,9 @@
 
 package_name="rcn-ee-archive-keyring"
 debian_pkg_name="${package_name}"
-package_version="2016.10.24"
-package_source=""
-src_dir=""
+package_version="2017.01.24"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
 
 git_repo=""
 git_sha=""
@@ -15,4 +15,4 @@ debian_version="${package_version}"
 debian_untar=""
 debian_patch=""
 
-zesty_version="~bpo1704+20161024+1"
+zesty_version="~bpo1704+20170124+1"
