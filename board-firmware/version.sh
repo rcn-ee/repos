f6a5095cf839faa75c3da6d78c5f49dbf6c9a857
Robert Nelson <robertcnelson@gmail.com>
2017-01-30 15:43:43 -0600

    and this
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ board-firmware/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+package_name="board-firmware"
+debian_pkg_name="${package_name}"
+package_version="1.20170130"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee1"
+debian_untar=""
+debian_patch=""
+
+jessie_version="~bpo80+20170130+1"
+stretch_version="~bpo90+20170130+1"
+xenial_version="~bpo1604+20170130+1"
