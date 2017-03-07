85cc7184c916be98513d20b00bd700fd3bd9aac2
Robert Nelson <robertcnelson@gmail.com>
2017-03-07 08:55:04 -0600

    roboticscape (0.3.4-git20170307-0rcnee2~bpo80+20170307)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- strawsondesign-8-roboticscape/version.sh
+++ strawsondesign-8-roboticscape/version.sh
@@ -4,17 +4,17 @@
 
 package_name="roboticscape"
 debian_pkg_name="${package_name}"
-package_version="0.3.4-git20170306"
+package_version="0.3.4-git20170307"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/StrawsonDesign/Robotics_Cape_Installer"
-git_sha="10d203fb4f4fd973e1fbcc4182e6ea123929c441"
+git_sha="628d329da7bfc6fb5a37b79664ebbd558310e856"
 reprepro_dir="r/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee2"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~bpo80+20170306"
+jessie_version="~bpo80+20170307"
