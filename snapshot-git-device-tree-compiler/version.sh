84dc9205d59627f6cbd2e9fc98cdf02ddc5b927c
Robert Nelson <robertcnelson@gmail.com>
2017-03-17 14:39:59 -0500

    device-tree-compiler (1.4.4-0rcnee0~bpo90+20170317) stretch
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ snapshot-git-device-tree-compiler/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+#https://git.kernel.org/pub/scm/utils/dtc/dtc.git/
+
+package_name="device-tree-compiler"
+debian_pkg_name="${package_name}"
+package_version="1.4.4"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="git://git.kernel.org/pub/scm/utils/dtc/dtc.git"
+git_sha="558cd81bdd432769b59bff01240c44f82cfb1a9d"
+reprepro_dir="d/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_patch=""
+debian_diff=""
+local_patch="rcnee0"
+
+stretch_version="~bpo90+20170317"
