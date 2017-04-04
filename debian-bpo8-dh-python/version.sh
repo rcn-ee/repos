4904797670991b2a0c655d5c1cd4ba89cf09855c
Robert Nelson <robertcnelson@gmail.com>
2017-04-04 12:09:08 -0500

    debian-bpo8-dh-python
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo8-dh-python/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="dh-python"
+debian_pkg_name="${package_name}"
+package_version="2.20170125~bpo8+1"
+
+git_repo=""
+git_sha=""
+reprepro_dir="d/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}"
+debian_untar=""
+debian_patch=""
+
+dist="debian"
+suite="jessie"
+deb_arch="armhf"
+
