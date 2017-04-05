e0ee517ab84a82926684de674fab0e074319de29
Robert Nelson <robertcnelson@gmail.com>
2017-04-05 12:19:44 -0500

    libdrm (2.4.77-1rcnee0~bpo80+20170405)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -100,7 +100,7 @@ mesa () {
 	package_name="libclc" ; package_version="${package_name}_0.2.0+git20170213-1" ; check_http
 
 	site="${debian_pool}/main/libd"
-	package_name="libdrm" ; package_version="${package_name}_2.4.76-1" ; check_http
+	package_name="libdrm" ; package_version="${package_name}_2.4.77-1" ; check_http
 
 	site="${debian_pool}/main/libv"
 	package_name="libvdpau" ; package_version="${package_name}_1.1.1-6" ; check_http
