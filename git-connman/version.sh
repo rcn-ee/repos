536bc9540b3bbb92e25e33ae0ffdcbf6fb975e82
Robert Nelson <robertcnelson@gmail.com>
2017-04-06 11:47:13 -0500

    connman (1.33-git20170404-0rcnee0~bpo90+20170406)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-connman/version.sh
+++ git-connman/version.sh
@@ -4,19 +4,19 @@
 
 package_name="connman"
 debian_pkg_name="${package_name}"
-package_version="1.33-git20170122"
+package_version="1.33-git20170404"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.kernel.org/pub/scm/network/connman/connman.git"
-git_sha="9b07d2ac1c6f4581afd82fc79f1c0ba6ca1424cd"
+git_sha="5afdf69554c12d240d55fe02f9b9107edd0bbea8"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
-local_patch="rcnee0"
+#local_patch="rcnee0"
 
-stretch_version="~bpo90+20170123+1"
-xenial_version="~bpo1604+20170123+1"
+stretch_version="~bpo90+20170406"
+xenial_version="~bpo1604+20170406"
