6f5ef5a8879666b11975ba3a6cd7d48c39c85d39
Robert Nelson <robertcnelson@gmail.com>
2017-04-07 09:28:11 -0500

    device-tree-compiler (1.4.4-0rcnee1~bpo90+20170407)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device-tree-compiler-v1.4.4/version.sh
+++ device-tree-compiler-v1.4.4/version.sh
@@ -13,9 +13,10 @@ git_sha="558cd81bdd432769b59bff01240c44f82cfb1a9d"
 reprepro_dir="d/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0"
+debian_version="${package_version}-0rcnee1"
 debian_patch=""
 debian_diff=""
-local_patch="rcnee0"
+#local_patch="rcnee0"
 
-stretch_version="~bpo90+20170317"
+stretch_version="~bpo90+20170407"
+xenial_version="~bpo1604+20170407"
