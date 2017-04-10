a8c46b0649425ca277b0434cb7260c325c8ff699
Robert Nelson <robertcnelson@gmail.com>
2017-04-10 11:15:59 -0500

    chromium-browser (57.0.2987.98-0ubuntu0.16.04.1276rcnee0~bpo80+20170410)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-chromium-browser/version.sh
+++ debian-8-chromium-browser/version.sh
@@ -5,7 +5,7 @@ mirror="http://ports.ubuntu.com"
 
 package_name="chromium-browser"
 debian_pkg_name="${package_name}"
-package_version="56.0.2924.76"
+package_version="57.0.2987.98"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="chromium-${package_version}"
 
@@ -14,9 +14,9 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/universe/${reprepro_dir}/"
 
-debian_version="${package_version}-0ubuntu0.16.04.1268"
+debian_version="${package_version}-0ubuntu0.16.04.1276"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee1"
+local_patch="rcnee0"
 
-jessie_version="~bpo80+20170308"
+jessie_version="~bpo80+20170410"
