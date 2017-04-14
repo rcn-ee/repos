69d190c764efaa36de4813c9dc868b6f82c2c69e
Robert Nelson <robertcnelson@gmail.com>
2017-04-14 14:49:09 -0500

    xf86-video-armada (0.0.1-r16-git20170211-0rcnee5~jessie+20170414)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-xf86-video-armada/version.sh
+++ debian-8-xf86-video-armada/version.sh
@@ -4,16 +4,16 @@
 
 package_name="xf86-video-armada"
 debian_pkg_name="${package_name}"
-package_version="0.0.1-r16-git20170316"
+package_version="0.0.1-r16-git20170211"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://ftp.arm.linux.org.uk/~rmk/xf86-video-armada.git"
-git_sha="fc0502197b821f601e8598c6465d4d2e1c8533fb"
+git_sha="fc12661c74819266f7c497ea04540cfb24760e07"
 reprepro_dir="x/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee4"
+debian_version="${package_version}-0rcnee5"
 debian_patch=""
 debian_diff=""
 
