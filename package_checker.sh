4c480a08ea45334dc67402dbddfe71ad0dca2b0f
Robert Nelson <robertcnelson@gmail.com>
2017-05-11 10:16:33 -0500

    mesa (17.1.0-1rcnee0~jessie+20170511)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -117,7 +117,7 @@ mesa () {
 	package_name="wayland" ; package_version="${package_name}_1.12.0-1" ; check_http
 
 	site="${debian_pool}/main/m"
-	package_name="mesa" ; package_version="${package_name}_17.0.4-1" ; check_http
+	package_name="mesa" ; package_version="${package_name}_17.1.0-1" ; check_http
 	package_name="mesa-demos" ; package_version="${package_name}_8.3.0-3" ; check_http
 }
 
