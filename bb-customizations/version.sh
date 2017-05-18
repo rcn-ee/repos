2922f56ce8ab70771944f7eb99d7fd10f8a52b6a
Robert Nelson <robertcnelson@gmail.com>
2017-05-18 09:24:58 -0500

    bb-customizations (1.20170518-0rcnee0~jessie+20170518)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20170501"
+package_version="1.20170518"
 package_source=""
 src_dir=""
 
@@ -11,11 +11,11 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~jessie+20170501"
-stretch_version="~stretch+20170501"
-xenial_version="~xenial+20170501"
-zesty_version="~zesty+20170501"
+jessie_version="~jessie+20170518"
+stretch_version="~stretch+20170518"
+xenial_version="~xenial+20170518"
+zesty_version="~zesty+20170518"
