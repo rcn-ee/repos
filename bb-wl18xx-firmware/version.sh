5e56d8c913d9bdc2f2b029a7567d56217998ccb5
Robert Nelson <robertcnelson@gmail.com>
2017-05-21 11:35:44 -0500

    bb-wl18xx-firmware (1.20170521-0rcnee0~jessie+20170521)
    fix: bb-wl18xx-wlan0 when connman isnt installed
    
    from: Juliusz Chroboczek <jch@irif.fr>
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/version.sh
+++ bb-wl18xx-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wl18xx-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20170406"
+package_version="1.20170521"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,8 @@ debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~bpo80+20170406"
-stretch_version="~bpo90+20170406"
-xenial_version="~bpo1604+20170406"
-yakkety_version="~bpo1610+20170406"
-zesty_version="~bpo1704+20170406"
+jessie_version="~jessie+20170521"
+stretch_version="~stretch+20170521"
+xenial_version="~xenial+20170521"
+yakkety_version="~yakkety+20170521"
+zesty_version="~zesty+20170521"
