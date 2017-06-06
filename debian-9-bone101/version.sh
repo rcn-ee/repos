24f8ece7aff82083fe09e6b023f35a69ed3391c3
Robert Nelson <robertcnelson@gmail.com>
2017-06-06 16:16:10 -0500

    bone101 (1.1.7-0rcnee0~stretch+20170606) stretch
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bone101/version.sh
+++ debian-9-bone101/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bone101"
 debian_pkg_name="${package_name}"
-package_version="1.1.6"
+package_version="1.1.7"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -11,14 +11,14 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee31"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 sbuild_chroot=""
 
 dl_mirror="https://github.com/rcn-ee/npm-package-bb-doc-bone101/raw/master/deploy/stretch/"
 
-v6="v6.10.2"
+v6="v6.10.3"
 
     node_async="async-2.0.0-rc.6"
 node_sensortag="sensortag-1.2.3"
@@ -26,4 +26,4 @@ node_sensortag="sensortag-1.2.3"
 debian_dl_1="${dl_mirror}/${node_async}-${v6}.tar.xz"
 debian_dl_2="${dl_mirror}/${node_sensortag}-${v6}.tar.xz"
 
-stretch_version="~stretch+20170421"
+stretch_version="~stretch+20170606"
