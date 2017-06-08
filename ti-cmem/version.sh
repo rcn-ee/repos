18754589559b8207fbd07d7a2052257d57dc2891
Robert Nelson <robertcnelson@gmail.com>
2017-06-08 14:49:55 -0500

    cmem (4.14.00.00-git20170216-0rcnee0~jessie+20170608)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-cmem/version.sh
+++ ti-cmem/version.sh
@@ -4,24 +4,23 @@
 
 package_name="cmem"
 debian_pkg_name="${package_name}"
-package_version="4.12.00.00-git20160926"
+package_version="4.14.00.00-git20170216"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/ipc/ludev.git"
-git_sha="d6d3e84c84d0e84a9b96c3f6f789ac642d03afcb"
+git_sha="b514a99ac45c64f0439282d0774d7cff5def4c6f"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
-local_patch="rcnee2"
 
 #sed -i -e 's:4.11.00.00:4.12.00.00:g' ./debian/cmem-mod-dkms.install
 #sed -i -e 's:4.11.00.00:4.12.00.00:g' ./debian/control
 #sed -i -e 's:4.11.00.00:4.12.00.00:g' ./debian/cmem-mod-dkms.dkms
 
-jessie_version="~bpo80+20161026+1"
-stretch_version="~bpo90+20161026+1"
-xenial_version="~bpo1604+20161026+1"
+jessie_version="~jessie+20170608"
+stretch_version="~stretch+20170608"
+xenial_version="~xenial+20170608"
