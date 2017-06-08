e06b063bc756d274f9ba1baf1dfc29a50084fc6c
Robert Nelson <robertcnelson@gmail.com>
2017-06-08 13:59:34 -0500

    vpdma-dra7xx-installer (03-2012-r1rcnee0~jessie+20170608)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-vpdma-dra7xx-installer/version.sh
+++ ti-vpdma-dra7xx-installer/version.sh
@@ -11,7 +11,7 @@ git_sha=""
 reprepro_dir="v/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-r2rcnee0"
+debian_version="${package_version}-r1rcnee0"
 debian_untar=""
 debian_patch=""
 
