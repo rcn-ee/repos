a89590b68d9d8a0df1a5753ee0c7c09db6bf2aa2
Robert Nelson <robertcnelson@gmail.com>
2017-06-08 14:38:55 -0500

    ti-ipc-dra7xx (3.44.01.01-r0.0rcnee0~jessie+20170608)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-ipc-dra7xx/version.sh
+++ ti-ipc-dra7xx/version.sh
@@ -4,20 +4,19 @@
 
 package_name="ti-ipc-dra7xx"
 debian_pkg_name="${package_name}"
-package_version="3.43.03.05"
+package_version="3.44.01.01"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/ipc/ipcdev.git"
-git_sha="6c2b64342cbd0317527a0924140566b2446488f5"
+git_sha="cff640a4c7e083f78116266ef681cec49d9b584b"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0"
+debian_version="${package_version}-r0.0rcnee0"
 debian_patch=""
 debian_diff=""
-local_patch="rcnee1"
 
-jessie_version="~bpo80+20161027+1"
-stretch_version="~bpo90+20161027+1"
-xenial_version="~bpo1604+20161027+1"
+jessie_version="~jessie+20170608"
+stretch_version="~stretch+20170608"
+xenial_version="~xenial+20170608"
