92d1a810987deb3f8f47fba9f386ddaf81ec19d2
Robert Nelson <robertcnelson@gmail.com>
2017-06-08 13:15:26 -0500

    ti-c6000-cgt-v8.2.x-installer (8.2.0-0rcnee0~jessie+20170608)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-c6000-cgt-v8.2.x-installer/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#http://software-dl.ti.com/codegen/non-esd/downloads/download.htm#C6000
+
+package_name="ti-c6000-cgt-v8.2.x-installer"
+debian_pkg_name="${package_name}"
+package_version="8.2.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+local_patch="rcnee0"
+
+jessie_version="~jessie+20170608"
+stretch_version="~stretch+20170608"
+xenial_version="~xenial+20170608"
