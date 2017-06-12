01123c4354f168b0b89d5b394ef9f8bd42740207
Robert Nelson <robertcnelson@gmail.com>
2017-06-12 13:55:26 -0500

    bb-wl18xx-firmware (1.20170612-0rcnee0~jessie+20170612)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/version.sh
+++ bb-wl18xx-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wl18xx-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20170521"
+package_version="1.20170612"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,9 @@ debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~jessie+20170521"
-stretch_version="~stretch+20170521"
-xenial_version="~xenial+20170521"
-yakkety_version="~yakkety+20170521"
-zesty_version="~zesty+20170521"
+jessie_version="~jessie+20170612"
+stretch_version="~stretch+20170612"
+xenial_version="~xenial+20170612"
+yakkety_version="~yakkety+20170612"
+zesty_version="~zesty+20170612"
+artful_version="~artful+20170612"
