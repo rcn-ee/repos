754d06c5f4665128d671b9e4ef0d03d9b1216214
Robert Nelson <robertcnelson@gmail.com>
2017-06-28 13:52:44 -0500

    ipumm-dra7xx-installer (3.00.13.00-r5rcnee0~jessie+20170628)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-ipumm-dra7xx-installer/version.sh
+++ ti-ipumm-dra7xx-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="ipumm-dra7xx-installer"
 debian_pkg_name="${package_name}"
-package_version="3.00.12.00"
+package_version="3.00.13.00-r5"
 package_source=""
 src_dir=""
 
@@ -11,13 +11,13 @@ git_sha=""
 reprepro_dir="i/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-r2rcnee0"
+debian_version="${package_version}rcnee0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://github.com/rcn-ee/sdk-firmware/raw/master/ipumm-fw/3.00.12.00-r2/files/dra7-ipu2-fw.xem4"
-debian_dl_2="https://github.com/rcn-ee/sdk-firmware/raw/master/ipumm-fw/3.00.12.00-r2/files/dra7-ipu2-fw.xem4.map"
+debian_dl_1="https://github.com/rcn-ee/sdk-firmware/raw/master/ipumm-fw/${package_version}/files/dra7-ipu2-fw.xem4"
+debian_dl_2="https://github.com/rcn-ee/sdk-firmware/raw/master/ipumm-fw/${package_version}/files/dra7-ipu2-fw.xem4.map"
 
-jessie_version="~jessie+20170608"
-stretch_version="~stretch+20170608"
-xenial_version="~xenial+20170608"
+jessie_version="~jessie+20170628"
+stretch_version="~stretch+20170628"
+xenial_version="~xenial+20170628"
