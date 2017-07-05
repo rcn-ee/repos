2cb8c7aeb2905a3d3e53054ff11bb8f60a5dfc51
Robert Nelson <robertcnelson@gmail.com>
2017-07-05 15:57:42 -0500

    fix this
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-libserialport/version.sh
+++ debian-8-libserialport/version.sh
@@ -5,7 +5,7 @@ mirror="https://mentors.debian.net/debian"
 package_name="libserialport"
 debian_pkg_name="${package_name}"
 package_version="0.1.1"
-package_source="${package_name}_${package_version}.orig.tar.xz"
+package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir=""
 
 git_repo=""
