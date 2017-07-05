0581fdd9800787650fc18b9a5960ebf42e5b381a
Robert Nelson <robertcnelson@gmail.com>
2017-07-05 09:59:23 -0500

    mesa (17.1.4-1rcnee0~jessie+20170705)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-mesa/version.sh
+++ git-mesa/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="17.1.3"
+package_version="17.1.4"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,10 +13,10 @@ git_sha=""
 reprepro_dir="m/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
 local_patch="rcnee0"
 
-jessie_version="~jessie+20170627"
-stretch_version="~stretch+20170627"
+jessie_version="~jessie+20170705"
+stretch_version="~stretch+20170705"
