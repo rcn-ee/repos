3307b4bf90ae73454b0e9996e8eae404a90ef36a
Robert Nelson <robertcnelson@gmail.com>
2017-07-05 17:19:32 -0500

    libserialport (0.1.1-2rcnee0~jessie+20170705)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-libserialport/generate_source.sh
+++ debian-8-libserialport/generate_source.sh
@@ -8,15 +8,7 @@ if [ ! -f ${package_name}-${package_version}.tar.gz ] ; then
 	wget -c ${server}/${package_name}/${package_name}-${package_version}.tar.gz
 fi
 
-if [ -d ${package_name}_${package_version}.orig ] ; then
-	rm -rf ${package_name}_${package_version}.orig || true
+if [ ! -f ${package_name}_${package_version}.orig.tar.gz ] ; then
+	cp ${package_name}-${package_version}.tar.gz ${package_name}_${package_version}.orig.tar.gz
 fi
-
-mkdir -p ${package_name}_${package_version}.orig
-tar xf ${package_name}-${package_version}.tar.gz -C ${package_name}_${package_version}.orig
-cd ${package_name}_${package_version}.orig
-mv ${package_name}-${package_version}/* ./
-rm -rf ${package_name}-${package_version}/
-tar cfJ ../${package_name}_${package_version}.orig.tar.xz ./*
-cd ../
 
--- debian-8-libserialport/version.sh
+++ debian-8-libserialport/version.sh
@@ -5,15 +5,15 @@ mirror="https://mentors.debian.net/debian"
 package_name="libserialport"
 debian_pkg_name="${package_name}"
 package_version="0.1.1"
-package_source="${package_name}_${package_version}.orig.tar.xz"
-src_dir=""
+package_source="${package_name}_${package_version}.orig.tar.gz"
+src_dir="${package_name}-${package_version}"
 
 git_repo=""
 git_sha=""
 reprepro_dir="libs/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-2rcnee0"
 debian_untar=""
 debian_patch=""
 
