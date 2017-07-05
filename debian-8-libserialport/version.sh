495408bdee784832ca24e03ed224a4096a500ec0
Robert Nelson <robertcnelson@gmail.com>
2017-07-05 16:37:58 -0500

    debian-8-libserialport: use upstream tar
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-libserialport/version.sh
+++ debian-8-libserialport/version.sh
@@ -5,7 +5,7 @@ mirror="https://mentors.debian.net/debian"
 package_name="libserialport"
 debian_pkg_name="${package_name}"
 package_version="0.1.1"
-package_source="${package_name}_${package_version}.orig.tar.gz"
+package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir=""
 
 git_repo=""
@@ -14,7 +14,7 @@ reprepro_dir="libs/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-2"
-debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_untar=""
 debian_patch=""
 
 jessie_version="~jessie+20170705"
