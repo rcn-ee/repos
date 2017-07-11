314bf94b5c9039af82950ec3512e037c06da2065
Robert Nelson <robertcnelson@gmail.com>
2017-07-11 16:37:03 -0500

    nodejs (6.11.1-1nodesource1~stretch1~stretch+20170711)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-nodejs/version.sh
+++ debian-9-nodejs/version.sh
@@ -4,7 +4,7 @@ mirror="https://deb.nodesource.com/node_6.x"
 
 package_name="nodejs"
 debian_pkg_name="${package_name}"
-package_version="6.11.0"
+package_version="6.11.1"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
 src_dir="node-v${package_version}"
 
@@ -13,7 +13,7 @@ git_sha=""
 reprepro_dir="n/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1nodesource1~sid1"
+debian_version="${package_version}-1nodesource1~stretch1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-stretch_version="~stretch+20170607"
+stretch_version="~stretch+20170711"
