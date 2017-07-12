5fd723c4becea6c13a09ed1e8b9f2a4f908fe463
Robert Nelson <robertcnelson@gmail.com>
2017-07-11 19:24:36 -0500

    ardupilot-copter-bbbmini (3.5.0-git20170711-0rcnee0~jessie+20170711)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-ardupilot-copter-bbbmini/version.sh
+++ git-ardupilot-copter-bbbmini/version.sh
@@ -5,12 +5,12 @@
 
 package_name="ardupilot-copter-bbbmini"
 debian_pkg_name="${package_name}"
-package_version="3.5.0-git20170614"
+package_version="3.5.0-git20170711"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/diydrones/ardupilot.git"
-git_sha="87abccef6cc9119e94d8269f30c423c8d1523568"
+git_sha="01a504ac368b59349d8c502c53d7b22da0065793"
 reprepro_dir="a/${package_name}"
 dl_path=""
 
@@ -18,5 +18,5 @@ debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20170627"
-stretch_version="~stretch+20170627"
+jessie_version="~jessie+20170711"
+stretch_version="~stretch+20170711"
