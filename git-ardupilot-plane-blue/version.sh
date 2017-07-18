01bf21e0b879a19b346adcb207c40672963f58e8
Robert Nelson <robertcnelson@gmail.com>
2017-07-18 11:25:43 -0500

    ardupilot-plane-bbbmini/blue (3.8.0-git20170717-0rcnee0~jessie+20170718)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-ardupilot-plane-blue/version.sh
+++ git-ardupilot-plane-blue/version.sh
@@ -4,12 +4,12 @@
 
 package_name="ardupilot-plane-blue"
 debian_pkg_name="${package_name}"
-package_version="3.8.0-git20170711"
+package_version="3.8.0-git20170717"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/diydrones/ardupilot.git"
-git_sha="260412bf0f32bc316d5fbb57c43a105bf4d26194"
+git_sha="d2c57860fd6c9abdc17bc8a2c6127c9cd9942cbf"
 reprepro_dir="a/${package_name}"
 dl_path=""
 
@@ -17,5 +17,5 @@ debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20170712"
-stretch_version="~stretch+20170712"
+jessie_version="~jessie+20170718"
+stretch_version="~stretch+20170718"
