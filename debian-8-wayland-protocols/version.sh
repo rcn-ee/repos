c4900962f670b44e211cb82cc403c37d7a6fdc8e
Robert Nelson <robertcnelson@gmail.com>
2017-08-14 15:19:27 -0500

    wayland-protocols (1.10-1rcnee0~jessie+20170814)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-8-wayland-protocols/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="wayland-protocols"
+debian_pkg_name="${package_name}"
+package_version="1.10"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="w/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="rcnee0"
+
+jessie_version="~jessie+20170814"
+stretch_version="~stretch+20170814"
