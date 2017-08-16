9748c6a53a7cf92412034f28125a822990503d18
Robert Nelson <robertcnelson@gmail.com>
2017-08-16 16:01:03 -0500

    bb-node-red-installer (0.17.5-0rcnee0~stretch+20170816)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bb-node-red-installer/version.sh
+++ debian-9-bb-node-red-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="0.16.2"
+package_version="0.17.5"
 package_source=""
 src_dir=""
 
@@ -11,22 +11,22 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
 dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/stretch"
 
- v6="v6.10.2"
+ v6="v6.11.2"
 
-           node_red="node-red-0.16.2-d1c4226"
-node_red_beaglebone="node-red-node-beaglebone-0.1.9-ec96d36"
+           node_red="node-red-0.17.5-e09efba"
+node_red_beaglebone="node-red-node-beaglebone-0.1.9-363bb55"
        node_red_upm="node-red-node-bb-upm-0.0.1-8d217ab"
-   node_red_mongodb="node-red-node-mongodb-0.0.11-ec96d36"
+   node_red_mongodb="node-red-node-mongodb-0.0.13-363bb55"
 
  debian_dl_1="${dl_mirror}/${node_red}-${v6}.tar.xz"
  debian_dl_2="${dl_mirror}/${node_red_beaglebone}-${v6}.tar.xz"
  debian_dl_3="${dl_mirror}/${node_red_upm}-${v6}.tar.xz"
  debian_dl_4="${dl_mirror}/${node_red_mongodb}-${v6}.tar.xz"
 
-stretch_version="~bpo90+20170412"
+stretch_version="~stretch+20170816"
