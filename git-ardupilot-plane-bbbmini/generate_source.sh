43fb999c5b13d7cc75d8dbb1da13cfd473d0c31d
Robert Nelson <robertcnelson@gmail.com>
2017-08-16 09:08:55 -0500

    ardupilot-plane-bbbmini (3.8.0-git20170803-0rcnee0~jessie+20170816)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-ardupilot-plane-bbbmini/generate_source.sh
+++ git-ardupilot-plane-bbbmini/generate_source.sh
@@ -14,6 +14,7 @@ git clone ${git_repo} ignore/${package_name}_${package_version}/
 
 if [ -f ./ignore/${package_name}_${package_version}/.git/config ] ; then
 	cd ./ignore/${package_name}_${package_version}/
+	git checkout ${git_branch}
 	git submodule update --init --recursive
 	rm -rf modules/PX4NuttX/misc/buildroot/toolchain/gcc/3.3.6/900-sx12-20101109.patch || true
 	cd ../
--- git-ardupilot-plane-bbbmini/version.sh
+++ git-ardupilot-plane-bbbmini/version.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -e
 
 #https://github.com/ArduPilot/ardupilot
+#https://github.com/ArduPilot/ardupilot/tree/plane3.8
 
 package_name="ardupilot-plane-bbbmini"
 debian_pkg_name="${package_name}"
-package_version="3.8.0-git20170717"
+package_version="3.8.0-git20170803"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/diydrones/ardupilot.git"
-git_sha="d2c57860fd6c9abdc17bc8a2c6127c9cd9942cbf"
+git_sha="e66f30eb90f75e52a3e37e25a65df4a532fdead2"
+git_branch="plane3.8"
 reprepro_dir="a/${package_name}"
 dl_path=""
 
@@ -17,5 +19,5 @@ debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20170718"
-stretch_version="~stretch+20170718"
+jessie_version="~jessie+20170816"
+stretch_version="~stretch+20170816"
