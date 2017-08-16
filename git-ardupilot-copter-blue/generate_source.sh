2946a7d5779323c3b592f958f3cb4896b19682b7
Robert Nelson <robertcnelson@gmail.com>
2017-08-16 09:00:39 -0500

    ardupilot-copter-blue (3.5.2-git20170813-0rcnee0~jessie+20170816)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-ardupilot-copter-blue/generate_source.sh
+++ git-ardupilot-copter-blue/generate_source.sh
@@ -14,7 +14,7 @@ git clone ${git_repo} ignore/${package_name}_${package_version}/
 
 if [ -f ./ignore/${package_name}_${package_version}/.git/config ] ; then
 	cd ./ignore/${package_name}_${package_version}/
-	git checkout Copter-3.5
+	git checkout ${git_branch}
 	git submodule update --init --recursive
 	rm -rf modules/PX4NuttX/misc/buildroot/toolchain/gcc/3.3.6/900-sx12-20101109.patch || true
 	cd ../
