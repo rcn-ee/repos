f4e1c01299a6434956cbdcfc4bd5e477217bb050
Robert Nelson <robertcnelson@gmail.com>
2017-08-30 11:17:22 -0500

    firmware-nonfree (20170823-1rcnee0~jessie+20170830)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-firmware-nonfree/version.sh
+++ debian-8-firmware-nonfree/version.sh
@@ -4,17 +4,20 @@ mirror="http://http.debian.net/debian"
 
 package_name="firmware-nonfree"
 debian_pkg_name="${package_name}"
-package_version="20161130"
+package_version="20170823"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${debian_pkg_name}-${package_version}"
+
 
 git_repo=""
 git_sha=""
 reprepro_dir="f/${package_name}"
 dl_path="pool/non-free/${reprepro_dir}/"
 
-debian_version="${package_version}-3"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
+local_patch="rcnee0"
 
-dist="debian"
-suite="jessie"
-deb_arch="armhf"
+jessie_version="~jessie+20170830"
+stretch_version="~stretch+20170830"
