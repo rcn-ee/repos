e27d9c540d0921dfbc82ab170ae5c2fb53fbe091
Robert Nelson <robertcnelson@gmail.com>
2017-09-13 17:14:07 -0500

    debian-9-nodejs: use system-icu
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-nodejs/version.sh
+++ debian-9-nodejs/version.sh
@@ -13,7 +13,7 @@ git_sha=""
 reprepro_dir="n/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee2"
 #debian_version="${package_version}-1nodesource1~stretch1"
 #debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
