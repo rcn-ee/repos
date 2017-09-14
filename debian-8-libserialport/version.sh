2039ced77a9cfe6c80eea7b6d3b6128d446c2c3d
Robert Nelson <robertcnelson@gmail.com>
2017-09-14 16:33:20 -0500

    libserialport (0.1.1-2rcnee1~jessie+20170914)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-libserialport/version.sh
+++ debian-8-libserialport/version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-mirror="https://mentors.debian.net/debian"
+mirror="http://http.debian.net/debian"
 
 package_name="libserialport"
 debian_pkg_name="${package_name}"
 package_version="0.1.1"
-package_source="${package_name}_${package_version}.orig.tar.gz"
+package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
 git_repo=""
@@ -13,9 +13,11 @@ git_sha=""
 reprepro_dir="libs/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2rcnee0"
-debian_untar=""
+debian_version="${package_version}-2"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
+local_patch="rcnee1"
 
-jessie_version="~jessie+20170705"
-stretch_version="~stretch+20170705"
+
+jessie_version="~jessie+20170914"
+stretch_version="~stretch+20170914"
