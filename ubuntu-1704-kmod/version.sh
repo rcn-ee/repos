6846ddeb285b690608bb8ff7aa7af8a1d3c8ec61
Robert Nelson <robertcnelson@gmail.com>
2017-10-05 13:11:48 -0500

    kmod (22-1.1ubuntu1rcnee1~zesty+20171005)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ubuntu-1704-kmod/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+mirror="http://archive.ubuntu.com/ubuntu"
+
+package_name="kmod"
+debian_pkg_name="${package_name}"
+package_version="22"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}.orig"
+
+git_repo=""
+git_sha=""
+reprepro_dir="k/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1.1ubuntu1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="rcnee1"
+
+stretch_version="~stretch+20171005"
+xenial_version="~xenial+20171005"
+zesty_version="~zesty+20171005"
