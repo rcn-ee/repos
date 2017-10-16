c1963badd3557671d5e54072c34f7ae0a5befce8
Robert Nelson <robertcnelson@gmail.com>
2017-10-16 11:14:29 -0500

    llvm-toolchain-5.0: 5.0-4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -134,7 +134,7 @@ mesa () {
 	package_name="llvm-toolchain-4.0" ; package_version="${package_name}_4.0.1-7" ; check_http
 	#https://packages.debian.org/source/sid/llvm-toolchain-5.0
 	ignore="rc"
-	package_name="llvm-toolchain-5.0" ; package_version="${package_name}_5.0-3" ; check_http
+	package_name="llvm-toolchain-5.0" ; package_version="${package_name}_5.0-4" ; check_http
 
 	site="${debian_pool}/main/libc"
 	filter="0.2"
