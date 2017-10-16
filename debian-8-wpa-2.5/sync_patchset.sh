00134d8d12e768f7a980870af3a2237c2f5ae3d6
Robert Nelson <robertcnelson@gmail.com>
2017-10-16 14:50:57 -0500

    wpa (2.5~git20150927-0rcnee2~jessie+20171016)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-wpa-2.5/sync_patchset.sh
+++ debian-8-wpa-2.5/sync_patchset.sh
@@ -37,6 +37,25 @@ number=2 ; git format-patch -${number} -o ${wdir}/2015-7/
 ${git} "${wdir}/2015-8/0001-EAP-pwd-peer-Fix-error-path-for-unexpected-Confirm-m.patch"
 number=1 ; git format-patch -${number} -o ${wdir}/2015-8/
 
+${git} "${wdir}/2016-1/0001-WPS-Reject-a-Credential-with-invalid-passphrase.patch"
+${git} "${wdir}/2016-1/0002-Reject-psk-parameter-set-with-invalid-passphrase-cha.patch"
+${git} "${wdir}/2016-1/0003-Remove-newlines-from-wpa_supplicant-config-network-o.patch"
+${git} "${wdir}/2016-1/0004-Reject-SET_CRED-commands-with-newline-characters-in-.patch"
+${git} "${wdir}/2016-1/0005-Reject-SET-commands-with-newline-characters-in-the-s.patch"
+number=5 ; git format-patch -${number} -o ${wdir}/2016-1/
+
+${git} "${wdir}/2017-1/0001-hostapd-Avoid-key-reinstallation-in-FT-handshake.patch"
+${git} "${wdir}/2017-1/0002-Prevent-reinstallation-of-an-already-in-use-group-ke.patch"
+${git} "${wdir}/2017-1/0003-Extend-protection-of-GTK-IGTK-reinstallation-of-WNM-.patch"
+${git} "${wdir}/2017-1/0004-Fix-PTK-rekeying-to-generate-a-new-ANonce.patch"
+${git} "${wdir}/2017-1/0005-TDLS-Reject-TPK-TK-reconfiguration.patch"
+${git} "${wdir}/2017-1/0006-WNM-Ignore-WNM-Sleep-Mode-Response-if-WNM-Sleep-Mode.patch"
+${git} "${wdir}/2017-1/0007-WNM-Ignore-WNM-Sleep-Mode-Response-without-pending-r.patch"
+${git} "${wdir}/2017-1/0008-FT-Do-not-allow-multiple-Reassociation-Response-fram.patch"
+${git} "${wdir}/2017-1/0009-TDLS-Ignore-incoming-TDLS-Setup-Response-retries.patch"
+number=9 ; git format-patch -${number} -o ${wdir}/2017-1/
+
+
 git checkout master -f
 git branch -D tmp
 
--- debian-8-wpa-2.5/version.sh
+++ debian-8-wpa-2.5/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 mirror="http://http.debian.net/debian"
-#https://w1.fi/cgit/hostap/log/?ofs=1100
+#https://w1.fi/cgit/hostap/log/?ofs=2900
 
 package_name="wpa"
 debian_pkg_name="${package_name}"
@@ -10,13 +10,12 @@ package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/rcn-ee/hostap"
-git_sha="1f344cd7b4461c8bae31abc3fc450407782282e2"
+git_sha="cac800c4d16189326b97bd53a169d0f9a7adfb85"
 reprepro_dir="w/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0"
+debian_version="${package_version}-0rcnee2"
 debian_untar=""
 debian_patch=""
-local_patch="rcnee1"
 
-jessie_version="~bpo80+20160428+1"
+jessie_version="~jessie+20171016"
