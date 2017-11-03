e9f73c7097b77947cda49894515143db2186060a
Robert Nelson <robertcnelson@gmail.com>
2017-11-03 10:41:35 -0500

    libgpiod
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-8-libgpiod/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+#https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/
+
+package_name="libgpiod"
+debian_pkg_name="${package_name}"
+package_version="0.3.2-git20171031"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="git://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git"
+git_sha="a029549af5d2792762286536db09b2cf564dc7a7"
+reprepro_dir="libg/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0rcnee1"
+debian_patch=""
+debian_diff=""
+
+jessie_version="~jessie+20171103"
+stretch_version="~stretch+20171103"
