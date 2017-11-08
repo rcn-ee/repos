473395004b561fd9c9ead310d19d58bf2e4ddb33
Robert Nelson <robertcnelson@gmail.com>
2017-11-08 11:16:50 -0600

    tio (1.26-1rcnee1~jessie+20171108)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-tio/version.sh
+++ debian-8-tio/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="tio"
 debian_pkg_name="${package_name}"
-package_version="1.25"
+package_version="1.26"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,10 +13,10 @@ git_sha=""
 reprepro_dir="t/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-3"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="rcnee1"
 
-jessie_version="~jessie+20171030"
-stretch_version="~stretch+20171030"
+jessie_version="~jessie+20171108"
+stretch_version="~stretch+20171108"
