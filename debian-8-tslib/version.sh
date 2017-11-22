eade3d201ab09b8d1e24e9fcaaed4f4483133409
Robert Nelson <robertcnelson@gmail.com>
2017-11-22 15:15:17 -0600

    tslib (1.14-1rcnee0~jessie+20171122)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-8-tslib/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="tslib"
+debian_pkg_name="${package_name}"
+package_version="1.14"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="rcnee0"
+
+jessie_version="~jessie+20171122"
+stretch_version="~stretch+20171122"
+buster_version="~buster+20171122"
