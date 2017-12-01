a2e33e26b933d742fbeb393d8951c10962da2381
Robert Nelson <robertcnelson@gmail.com>
2017-12-01 16:08:48 -0600

    close
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-10-nodejs-bin-x64/version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+#mirror="https://deb.nodesource.com/node_8.x"
+
+package_name="nodejs"
+debian_pkg_name="${package_name}"
+package_version="8.9.1"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="n/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee1"
+debian_patch=""
+
+buster_version="~buster+20171201"
