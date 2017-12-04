42f10f84cc04edf7bb317b5e7a781d9cd0d3d80a
Robert Nelson <robertcnelson@gmail.com>
2017-12-04 10:04:29 -0600

    chromium-browser (63.0.3239.40-1rcnee0~stretch+20171204)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-chromium-browser/version.sh
+++ debian-9-chromium-browser/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="chromium-browser"
 debian_pkg_name="${package_name}"
-package_version="63.0.3239.30"
+package_version="63.0.3239.40"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="chromium-${package_version}"
 
@@ -18,4 +18,4 @@ debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="rcnee0"
 
-stretch_version="~stretch+20171130"
+stretch_version="~stretch+20171204"
--- package_checker.sh
+++ package_checker.sh
@@ -363,7 +363,7 @@ chromium () {
 
 	echo "chromium-browser: (stretch)"
 	site="${debian_pool}/main/c"
-	package_name="chromium-browser" ; package_version="${package_name}_63.0.3239.30-1" ; check_http
+	package_name="chromium-browser" ; package_version="${package_name}_63.0.3239.40-1" ; check_http
 }
 
 important
