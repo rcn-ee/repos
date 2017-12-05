a3c3b3769bc1b078577068d2289747dca6a183fc
Robert Nelson <robertcnelson@gmail.com>
2017-12-05 16:14:57 -0600

    npm4-installer (4.6.1-0rcnee0~buster+20171205)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-10-npm4-installer/version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+package_name="npm4-installer"
+debian_pkg_name="${package_name}"
+package_version="4.6.1"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="n/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee0"
+debian_untar=""
+debian_patch=""
+
+dl_mirror="https://github.com/rcn-ee/npm-package-npm/raw/master/deploy/stretch/"
+
+v8="v8.9.1"
+
+bonescript="npm-4.6.1"
+
+debian_dl_1="${dl_mirror}/${bonescript}-${v8}.tar.xz"
+
+buster_version="~buster+20171205"
