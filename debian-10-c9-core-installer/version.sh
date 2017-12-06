d69fad859139728dad99fe2eb9b688802fb9f449
Robert Nelson <robertcnelson@gmail.com>
2017-12-06 10:17:29 -0600

    c9-core-installer (3.1.4035+git20171205-0rcnee0~buster+20171206)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-10-c9-core-installer/version.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+#https://github.com/c9/core
+
+package_name="c9-core-installer"
+debian_pkg_name="${package_name}"
+package_version="3.1.4035+git20171205"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee0"
+debian_untar=""
+debian_patch=""
+
+dl_mirror="https://rcn-ee.net/repos/git/bb.org-cloud9-core/deploy/buster/"
+
+v8="v8.9.1"
+
+debian_dl_1="${dl_mirror}/c9-core_${package_version}-${v8}-build.tar.xz"
+
+stretch_version="~stretch+20170426"
+buster_version="~buster+20171206"
