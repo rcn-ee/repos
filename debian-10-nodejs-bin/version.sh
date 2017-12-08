e70280523510dcb899afdc2998c7f8f69760ae00
Robert Nelson <robertcnelson@gmail.com>
2017-12-08 09:40:51 -0600

    nodejs (8.9.2-0rcnee1~buster+20171208)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-nodejs-bin/version.sh
+++ debian-10-nodejs-bin/version.sh
@@ -4,7 +4,7 @@
 
 package_name="nodejs"
 debian_pkg_name="${package_name}"
-package_version="8.9.1"
+package_version="8.9.2"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir=""
 
@@ -13,7 +13,7 @@ git_sha=""
 reprepro_dir="n/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee3"
+debian_version="${package_version}-0rcnee1"
 debian_patch=""
 
-buster_version="~buster+20171204"
+buster_version="~buster+20171208"
