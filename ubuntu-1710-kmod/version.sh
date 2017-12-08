b8151ee7fcf300208502993b24bf844ada6623d5
Robert Nelson <robertcnelson@gmail.com>
2017-12-08 12:56:23 -0600

    kmod (24-1ubuntu2rcnee2~artful+20171208)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ubuntu-1710-kmod/version.sh
+++ ubuntu-1710-kmod/version.sh
@@ -16,6 +16,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1ubuntu2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee1"
+local_patch="rcnee2"
 
 artful_version="~artful+20171208"
