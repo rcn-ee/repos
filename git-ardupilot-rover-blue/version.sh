cf0144a9aad72406e4e2e088806f121d43c8d48a
Robert Nelson <robertcnelson@gmail.com>
2017-12-15 14:01:42 -0600

    ardupilot-rover-blue (3.2.0-git20171205-0rcnee1~jessie+20171215)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-ardupilot-rover-blue/version.sh
+++ git-ardupilot-rover-blue/version.sh
@@ -5,12 +5,12 @@
 
 package_name="ardupilot-rover-blue"
 debian_pkg_name="${package_name}"
-package_version="3.2.0-git20170825"
+package_version="3.2.0-git20171205"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/diydrones/ardupilot.git"
-git_sha="b1dafab91b6cdc1dd5c1e11d056ce273a47a126f"
+git_sha="a830db7c4f99977d78b0cf6ffe4f8413935b0f79"
 git_branch="Rover-3.2"
 reprepro_dir="a/${package_name}"
 dl_path=""
@@ -19,5 +19,6 @@ debian_version="${package_version}-0rcnee1"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20171016"
-stretch_version="~stretch+20171016"
+jessie_version="~jessie+20171215"
+stretch_version="~stretch+20171215"
+buster_version="~buster+20171215"
