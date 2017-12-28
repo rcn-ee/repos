c2c6b1796c6b45b0cb7f11f288e6ba8146091a7e
Robert Nelson <robertcnelson@gmail.com>
2017-12-28 10:12:14 -0600

    libclc: 0.2.0+git20171106-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo9-libclc/version.sh
+++ debian-bpo9-libclc/version.sh
@@ -4,14 +4,14 @@ mirror="http://http.debian.net/debian"
 
 package_name="libclc"
 debian_pkg_name="${package_name}"
-package_version="0.2.0+git20170330"
+package_version="0.2.0+git20171106"
 
 git_repo=""
 git_sha=""
 reprepro_dir="libc/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
