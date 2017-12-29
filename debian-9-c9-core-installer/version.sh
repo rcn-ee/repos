6d9fdd856e3ffbefeecc61a45ef79d068882f108
Robert Nelson <robertcnelson@gmail.com>
2017-12-29 12:39:18 -0600

    c9-core-installer (3.1.4056.git20171215-0rcnee0~stretch+20171229)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-c9-core-installer/version.sh
+++ debian-9-c9-core-installer/version.sh
@@ -4,7 +4,7 @@
 
 package_name="c9-core-installer"
 debian_pkg_name="${package_name}"
-package_version="3.1.3555+git20170424"
+package_version="3.1.4056.git20171215"
 package_source=""
 src_dir=""
 
@@ -13,14 +13,14 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee3"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-dl_mirror="https://rcn-ee.net/repos/git/bb.org-cloud9-core/deploy/stretch"
+dl_mirror="https://github.com/rcn-ee/bb.org-cloud9-core/raw/master/deploy/stretch"
 
-v6="v6.10.2"
+v6="v6.12.2"
 
 debian_dl_1="${dl_mirror}/c9-core_${package_version}-${v6}-build.tar.xz"
 
-stretch_version="~stretch+20170426"
+stretch_version="~stretch+20171229"
