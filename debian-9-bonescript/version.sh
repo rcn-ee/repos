37f30933b1480f5773c88c93cb3ef19f9534f4a0
Robert Nelson <robertcnelson@gmail.com>
2018-01-02 09:32:19 -0600

    bonescript (0.6.2-beta4-0rcnee0~stretch+20180102)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bonescript/version.sh
+++ debian-9-bonescript/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bonescript"
 debian_pkg_name="${package_name}"
-package_version="0.6.2-beta3"
+package_version="0.6.2-beta4"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -20,7 +20,7 @@ dl_mirror="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/st
 v6="v6.12.2"
 v8="v8.9.3"
 
-bonescript="bonescript-0.6.2-beta3-08f082b"
+bonescript="bonescript-0.6.2-beta4-78a9f1e"
    winston="winston-2.1.1"
 
 debian_dl_1="${dl_mirror}/${bonescript}-${v6}.tar.xz"
@@ -28,4 +28,4 @@ debian_dl_2="${dl_mirror}/${winston}-${v6}.tar.xz"
 debian_dl_3="${dl_mirror}/${bonescript}-${v8}.tar.xz"
 debian_dl_4="${dl_mirror}/${winston}-${v8}.tar.xz"
 
-stretch_version="~stretch+20171229"
+stretch_version="~stretch+20180102"
