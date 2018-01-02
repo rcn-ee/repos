1036ccd54a3c1e1bdd062d2fb307a0b21bba9d09
Robert Nelson <robertcnelson@gmail.com>
2018-01-02 12:09:11 -0600

    libgpiod (0.3.2+git20171201-2rcnee0~buster+20180102)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-libgpiod/version.sh
+++ debian-9-libgpiod/version.sh
@@ -1,21 +1,22 @@
 #!/bin/bash -e
 
-#https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/
+mirror="http://http.debian.net/debian"
 
 package_name="libgpiod"
 debian_pkg_name="${package_name}"
-package_version="0.3.2-git20171201"
-package_source="${package_name}_${package_version}.orig.tar.xz"
+package_version="0.3.2+git20171201"
+package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}_${package_version}"
 
-git_repo="git://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git"
-git_sha="7d613094586a5a07c408e179f4fcf0243656f5bb"
+git_repo=""
+git_sha=""
 reprepro_dir="libg/${package_name}"
-dl_path=""
+dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-2"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-debian_diff=""
+local_patch="rcnee0"
 
-stretch_version="~stretch+20171206"
-buster_version="~buster+20171206"
+stretch_version="~stretch+20180102"
+buster_version="~buster+20180102"
