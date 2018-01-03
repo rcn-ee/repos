86040147f6ff76db82106c975c241d10d964e4b3
Robert Nelson <robertcnelson@gmail.com>
2018-01-03 11:29:21 -0600

    fix this
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo9-libglvnd/version.sh
+++ debian-bpo9-libglvnd/version.sh
@@ -4,14 +4,15 @@ mirror="http://http.debian.net/debian"
 
 package_name="libglvnd"
 debian_pkg_name="${package_name}"
-package_version="0.2.999+git20170802"
+package_version="1.0.0-1"
 
 git_repo=""
 git_sha=""
 reprepro_dir="libg/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}"
+debian_untar=""
 debian_patch=""
 
 dist="debian"
