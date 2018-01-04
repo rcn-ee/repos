95d2f45ca0181aa0d7120c6b6ebfbffe7d0709a9
Robert Nelson <robertcnelson@gmail.com>
2018-01-04 15:03:57 -0600

    tiomapconf (1.73.0-git20170905-0rcnee0~jessie+20180104)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-omapconf/version.sh
+++ ti-omapconf/version.sh
@@ -4,19 +4,21 @@
 
 package_name="tiomapconf"
 debian_pkg_name="${package_name}"
-package_version="1.73.0-git20170421"
+package_version="1.73.0-git20170905"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/omapconf/omapconf"
-git_sha="d341577f0d19f62a1e87bd7c21b5942dc73fac09"
+git_sha="be8626b29febb99ee9683989e9334956b4b4de2d"
 reprepro_dir="o/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20170612"
-stretch_version="~stretch+20170612"
-xenial_version="~xenial+20170612"
+jessie_version="~jessie+20180104"
+stretch_version="~stretch+20180104"
+buster_version="~buster+20180104"
+xenial_version="~xenial+20180104"
+bionic_version="~bionic+20180104"
