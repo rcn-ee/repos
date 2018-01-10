65d93205a388fdfd69474056cd718acc9fda4255
Robert Nelson <robertcnelson@gmail.com>
2018-01-10 15:01:46 -0600

    bb-node-red-installer (0.17.5-0rcnee3~buster+20180110)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bb-node-red-installer/version.sh
+++ debian-10-bb-node-red-installer/version.sh
@@ -11,7 +11,7 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0rcnee3"
 debian_untar=""
 debian_patch=""
 
@@ -21,8 +21,14 @@ v8="v8.9.4"
 
 node_red="node-red-0.17.5-915d73e"
 node_red_mongodb="node-red-node-mongodb-0.0.13-9f290f6"
+node_red_bbio="beaglebone-io-2.3.0"
+node_red_five="johnny-five-0.13.0"
+node_red_gpio="node-red-contrib-gpio-0.10.0"
 
 debian_dl_1="${dl_mirror}/${node_red}-${v8}.tar.xz"
 debian_dl_2="${dl_mirror}/${node_red_mongodb}-${v8}.tar.xz"
+debian_dl_3="${dl_mirror}/${node_red_bbio}-${v8}.tar.xz"
+debian_dl_4="${dl_mirror}/${node_red_five}-${v8}.tar.xz"
+debian_dl_5="${dl_mirror}/${node_red_gpio}-${v8}.tar.xz"
 
 buster_version="~buster+20180110"
