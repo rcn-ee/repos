9bdb2ec74cc37c473bf2e7386b0046fc603978bf
Robert Nelson <robertcnelson@gmail.com>
2018-01-11 15:38:19 -0600

    bb-beaglebone-io-installer (2.3.0-0rcnee0~buster+20180111)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-10-bb-beaglebone-io-installer/version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+package_name="bb-beaglebone-io-installer"
+debian_pkg_name="${package_name}"
+package_version="2.3.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee0"
+debian_untar=""
+debian_patch=""
+
+dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/buster"
+
+v8="v8.9.4"
+
+node_red_bbio="beaglebone-io-2.3.0-7d20557"
+
+debian_dl_1="${dl_mirror}/${node_red_bbio}-${v8}.tar.xz"
+
+buster_version="~buster+20180111"
---
+++ debian-10-bb-johnny-five-installer/version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+package_name="bb-johnny-five-installer"
+debian_pkg_name="${package_name}"
+package_version="0.13.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee0"
+debian_untar=""
+debian_patch=""
+
+dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/buster"
+
+v8="v8.9.4"
+
+node_red_five="johnny-five-0.13.0"
+
+debian_dl_1="${dl_mirror}/${node_red_five}-${v8}.tar.xz"
+
+buster_version="~buster+20180110"
