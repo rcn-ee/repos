5dc4ca479a68ff4881dab345c32174ee660c56fd
Robert Nelson <robertcnelson@gmail.com>
2018-01-12 10:31:26 -0600

    mesa (17.3.2-1rcnee0~stretch+20180112)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-mesa/version.sh
+++ debian-9-mesa/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="17.3.1"
+package_version="17.3.2"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -16,6 +16,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
-local_patch="rcnee2"
+local_patch="rcnee0"
 
-stretch_version="~stretch+20180103"
+stretch_version="~stretch+20180112"
