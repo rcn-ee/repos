4ab8e38834726902d57926837a4c017fd36d91fc
Robert Nelson <robertcnelson@gmail.com>
2018-01-16 16:28:38 -0600

    adn this
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-9-mame/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="mame"
+debian_pkg_name="${package_name}"
+package_version="0.182"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="m/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="rcnee0"
+
+stretch_version="~stretch+20180116"
