7636c1577a320968aa0891a833d06d49597e3af7
Robert Nelson <robertcnelson@gmail.com>
2018-01-19 10:25:35 -0600

    xf86-video-omap (0.4.5-1rcnee1~jessie+20180119)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- xf86-video-omap/version.sh
+++ xf86-video-omap/version.sh
@@ -1,26 +1,22 @@
 #!/bin/bash -e
 
-#http://cgit.freedesktop.org/xorg/driver/xf86-video-omap/
+mirror="http://http.debian.net/debian"
 
 package_name="xf86-video-omap"
 debian_pkg_name="${package_name}"
-package_version="0.4.4"
-package_source="${package_name}_${package_version}.orig.tar.xz"
-src_dir="${package_name}_${package_version}"
+package_version="0.4.5"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.bz2"
+src_dir="${package_name}-${package_version}"
 
-git_repo="git://anongit.freedesktop.org/xorg/driver/xf86-video-omap"
-git_sha="c734f380f1df9f21229ce741eef59955d2a8b535"
+git_repo=""
+git_sha=""
 reprepro_dir="x/${package_name}"
-dl_path=""
+dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-debian_diff=""
-local_patch="rcnee2"
+local_patch="rcnee1"
 
-wheezy_version="~bpo70+20160209+1"
-jessie_version="~bpo80+20160209+1"
-stretch_version="~bpo90+20160209+1"
-trusty_version="~bpo1404+20160209+1"
-wily_version="~bpo1510+20160209+1"
-xenial_version="~bpo1604+20160209+1"
+jessie_version="~jessie+20180119"
+xenial_version="~xenial+20180119"
