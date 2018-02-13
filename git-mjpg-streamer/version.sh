8e63b08891670e55945fdeb8ecf02b24bdd11aea
Robert Nelson <robertcnelson@gmail.com>
2018-02-13 14:15:51 -0600

    mjpg-streamer (0.0.0-git20180125-0rcnee0~jessie+20180213)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-mjpg-streamer/version.sh
+++ git-mjpg-streamer/version.sh
@@ -4,12 +4,12 @@
 
 package_name="mjpg-streamer"
 debian_pkg_name="${package_name}"
-package_version="0.0.0-git20170828"
+package_version="0.0.0-git20180125"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/jacksonliam/mjpg-streamer"
-git_sha="c1ecfaf7c5cb958cdfd251bdaa9824c3e798f890"
+git_sha="8cc9d22c1e79905d529a248ccf05bbf0625e0bf3"
 reprepro_dir="m/${package_name}"
 dl_path=""
 
@@ -17,6 +17,6 @@ debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20170914"
-stretch_version="~stretch+20170914"
-buster_version="~buster+20170914"
+jessie_version="~jessie+20180213"
+stretch_version="~stretch+20180213"
+buster_version="~buster+20180213"
