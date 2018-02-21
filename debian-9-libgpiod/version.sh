8635fceb122718ee24835e3c88409abf468607a5
Robert Nelson <robertcnelson@gmail.com>
2018-02-21 13:56:26 -0600

    libgpiod (1.0-1rcnee0~bionic+20180221)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-libgpiod/version.sh
+++ debian-9-libgpiod/version.sh
@@ -1,24 +1,25 @@
 #!/bin/bash -e
 
+#https://packages.debian.org/source/sid/libgpiod
+
 mirror="http://http.debian.net/debian"
 
 package_name="libgpiod"
 debian_pkg_name="${package_name}"
-package_version="0.3.2+git20171201"
+package_version="1.0"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 #src_dir="${package_name}_${package_version}"
-src_dir="libgpiod-486b57388683fc4f404fa6cb008c5dbae6fe6c33"
+src_dir="libgpiod-v1.0.x"
 
 git_repo=""
 git_sha=""
 reprepro_dir="libg/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee2"
+local_patch="rcnee0"
 
-stretch_version="~stretch+20180104"
-buster_version="~buster+20180104"
-bionic_version="~bionic+20180104"
+stretch_version="~stretch+20180221"
+bionic_version="~bionic+20180221"
