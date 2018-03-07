8cbfa54e88c21b72755c3486301f86864227afb5
Robert Nelson <robertcnelson@gmail.com>
2018-03-07 09:08:45 -0600

    nodejs (6.13.1-0rcnee0~stretch+20180307)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-nodejs/version.sh
+++ debian-9-nodejs/version.sh
@@ -4,7 +4,7 @@
 
 package_name="nodejs"
 debian_pkg_name="${package_name}"
-package_version="6.13.0"
+package_version="6.13.1"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
 src_dir="node-v${package_version}"
 
@@ -18,4 +18,4 @@ debian_version="${package_version}-0rcnee0"
 #debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
-stretch_version="~stretch+20180216"
+stretch_version="~stretch+20180307"
