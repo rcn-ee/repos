dc4e936427873921366564bfe00eee23e477c5d8
Robert Nelson <robertcnelson@gmail.com>
2018-03-07 12:22:24 -0600

    bb-beaglebone-io-installer (3.0.0-0rcnee2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bb-beaglebone-io-installer/version.sh
+++ debian-9-bb-beaglebone-io-installer/version.sh
@@ -11,18 +11,18 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee2"
 debian_untar=""
 debian_patch=""
 
 dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/stretch"
 
-v6="v6.13.0"
-v8="v8.9.4"
+v6="v6.13.1"
+v8="v8.10.0"
 
 node_red_bbio="beaglebone-io-3.0.0-cdc5ef2"
 
 debian_dl_1="${dl_mirror}/${node_red_bbio}-${v6}.tar.xz"
 debian_dl_2="${dl_mirror}/${node_red_bbio}-${v8}.tar.xz"
 
-stretch_version="~stretch+20180221"
+stretch_version="~stretch+20180307"
