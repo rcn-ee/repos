b7bda078d9ec33556f5e18af9c23fe61caa8a4bf
Dimitar Dimitrov <dimitar@dinux.eu>
2018-03-17 07:47:03 +0200

    Update PRU gcc and binutils
    New major version with better ABI support.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- binutils-pru/version.sh
+++ binutils-pru/version.sh
@@ -2,8 +2,8 @@
 
 package_name="binutils-pru"
 debian_pkg_name="${package_name}"
-gnupru_release="2017.09-beta-rc2.2"
-package_version="2.28.51.20170530"
+gnupru_release="2018.03-beta-rc3"
+package_version="2.30.51.20180310"
 package_source=""
 src_dir=""
 
@@ -16,5 +16,5 @@ debian_version="${package_version}"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~jessie+20170925"
-stretch_version="~stretch+20170925"
+jessie_version="~jessie+20180317"
+stretch_version="~stretch+20180317"
