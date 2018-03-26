de958abdca2092483462e5fb0a825ab296aee0eb
Robert Nelson <robertcnelson@gmail.com>
2018-03-26 15:30:55 -0500

    bump mesa
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- package_checker.sh
+++ package_checker.sh
@@ -156,7 +156,7 @@ mesa () {
 	package_name="wayland" ; package_version="${package_name}_1.12.0-1" ; check_http
 
 	site="${debian_pool}/main/m"
-	package_name="mesa" ; package_version="${package_name}_18.0.0~rc4-1" ; check_http
+	package_name="mesa" ; package_version="${package_name}_18.0.0~rc5-1" ; check_http
 	package_name="mesa-demos" ; package_version="${package_name}_8.3.0-5" ; check_http
 }
 
