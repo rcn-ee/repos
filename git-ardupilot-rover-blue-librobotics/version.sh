49dfbdead0974374353e4e1c1f2aeeafeb2ad699
Robert Nelson <robertcnelson@gmail.com>
2018-04-13 13:47:25 -0500

    ardupilot-rover-blue-librobotics (3.2.3-git20180408-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-ardupilot-rover-blue-librobotics/version.sh
+++ git-ardupilot-rover-blue-librobotics/version.sh
@@ -9,7 +9,7 @@ package_version="3.2.3-git20180408"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
-git_repo="https://github.com/diydrones/ardupilot.git"
+git_repo="https://github.com/ArduPilot/ardupilot.git"
 git_sha="261f998fe08713b40353ed6974d00e038a2db30b"
 git_branch="Rover-3.2"
 reprepro_dir="a/${package_name}"
@@ -21,4 +21,3 @@ debian_diff=""
 
 jessie_version="~jessie+20180413"
 stretch_version="~stretch+20180413"
-buster_version="~buster+20180413"
