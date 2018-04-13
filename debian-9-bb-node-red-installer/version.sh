1afd848abe472d37795c1187ab37bd501db2ecb5
Robert Nelson <robertcnelson@gmail.com>
2018-04-13 09:05:14 -0500

    bb-node-red-installer (0.18.4-0rcnee2~stretch+20180413)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bb-node-red-installer/version.sh
+++ debian-9-bb-node-red-installer/version.sh
@@ -11,16 +11,16 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee2"
 debian_untar=""
 debian_patch=""
 
 dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/stretch"
 
-v6="v6.14.0"
-v8="v8.11.0"
+v6="v6.14.1"
+v8="v8.11.1"
 
-node_red="node-red-0.18.4-ebb3fb9"
+node_red="node-red-0.18.4-838c7a5"
 node_red_dashboard="node-red-dashboard-2.8.2"
 node_red_mongodb="node-red-node-mongodb-0.0.14"
 node_red_gpio="node-red-contrib-gpio-0.14.0"
@@ -41,4 +41,4 @@ debian_dl_10="${dl_mirror}/${node_red_gpio}-${v6}.tar.xz"
 debian_dl_11="${dl_mirror}/${node_red_serial}-${v6}.tar.xz"
 debian_dl_12="${dl_mirror}/${node_red_can}-${v6}.tar.xz"
 
-stretch_version="~stretch+20180329"
+stretch_version="~stretch+20180413"
