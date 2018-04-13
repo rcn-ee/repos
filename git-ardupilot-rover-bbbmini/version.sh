e164302bcb60ab8d84424d9b378e888ec81adf7b
Robert Nelson <robertcnelson@gmail.com>
2018-04-13 10:55:30 -0500

    ardupilot-rover-bbbmini (3.2.3-git20180408-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-ardupilot-rover-bbbmini/version.sh
+++ git-ardupilot-rover-bbbmini/version.sh
@@ -5,20 +5,20 @@
 
 package_name="ardupilot-rover-bbbmini"
 debian_pkg_name="${package_name}"
-package_version="3.2.0-git20180104"
+package_version="3.2.3-git20180408"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/diydrones/ardupilot.git"
-git_sha="d7ceba8c0a904e752f98ee426f68ffd13e8958eb"
+git_sha="261f998fe08713b40353ed6974d00e038a2db30b"
 git_branch="Rover-3.2"
 reprepro_dir="a/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20180112"
-stretch_version="~stretch+20180112"
-buster_version="~buster+20180112"
+jessie_version="~jessie+20180413"
+stretch_version="~stretch+20180413"
+buster_version="~buster+20180413"
