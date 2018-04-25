297b0db3f3fbdee5d43cee93c5d4b0e7fa33fae4
Robert Nelson <robertcnelson@gmail.com>
2018-04-24 20:47:35 -0500

    roboticscape (0.4.1-git20180424.1-0rcnee2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-strawsondesign-roboticscape-master/version.sh
+++ git-strawsondesign-roboticscape-master/version.sh
@@ -4,7 +4,7 @@
 
 package_name="roboticscape"
 debian_pkg_name="${package_name}"
-package_version="0.4.1-git20180424"
+package_version="0.4.1-git20180424.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
