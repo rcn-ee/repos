1dd5442c0df682b53681aa1084b9990027e94e44
Robert Nelson <robertcnelson@gmail.com>
2018-04-24 21:01:31 -0500

    make sure to clear out
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- binutils-pru/generate_source.sh
+++ binutils-pru/generate_source.sh
@@ -6,7 +6,7 @@ for d in suite/*
 do
   OLD=`pwd`
   cd $d
-  rm *.tar.bz2
+  rm *.tar.bz2 || true
   wget https://github.com/dinuxbg/gnupru/releases/download/${gnupru_release}/${package_name}_${package_version}.tar.bz2
   cd $OLD
 done
