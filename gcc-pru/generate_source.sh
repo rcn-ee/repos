70fec84e45fbf7f2b077b60cd91350e92b6806ad
Robert Nelson <robertcnelson@gmail.com>
2018-04-25 20:58:57 -0500

    gcc-pru (8.0.1.20180310-0rcnee1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- gcc-pru/generate_source.sh
+++ gcc-pru/generate_source.sh
@@ -6,9 +6,8 @@ for d in suite/*
 do
   OLD=`pwd`
   cd $d
-  if [ ! -f ${package_name}_${package_version}.tar.bz2 ] ; then
-    wget https://github.com/dinuxbg/gnupru/releases/download/${gnupru_release}/${package_name}_${package_version}.tar.bz2
-  fi
+  rm *.tar.bz2 || true
+  wget https://github.com/dinuxbg/gnupru/releases/download/${gnupru_release}/${package_name}_${package_version}.tar.bz2
   cd $OLD
 done
 
