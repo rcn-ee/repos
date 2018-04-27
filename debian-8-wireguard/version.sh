c4a42f46cca1f96575a644a594f686852daa10cb
Robert Nelson <robertcnelson@gmail.com>
2018-04-27 14:54:32 -0500

    wireguard (0.0.20180420+git20180423-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-8-wireguard/version.sh
+++ debian-8-wireguard/version.sh
@@ -5,21 +5,21 @@
 
 package_name="wireguard"
 debian_pkg_name="${package_name}"
-package_version="0.0.20171221+git20171221"
+package_version="0.0.20180420+git20180423"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://git.zx2c4.com/WireGuard/"
-git_sha="7e945a815b896a19ecd2ff2b8937c4f90cc978d2"
+git_sha="f182b1a2a1127599da0d61af4f7ae818455ffc42"
 reprepro_dir="w/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 local_patch=""
 
-jessie_version="~jessie+20180104"
-stretch_version="~stretch+20180104"
-buster_version="~buster+20180104"
-bionic_version="~bionic+20180104"
+jessie_version="~jessie+20180427"
+stretch_version="~stretch+20180427"
+buster_version="~buster+20180427"
+bionic_version="~bionic+20180427"
