eca8ad418c9e53eb8d3618d4d8f796ad08970ae5
Robert Nelson <robertcnelson@gmail.com>
2018-05-16 11:05:53 -0500

    ardupilot-rover-3.3-pocket (3.3.1-git20180512-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-ardupilot-rover-3.3-pocket/version.sh
+++ git-ardupilot-rover-3.3-pocket/version.sh
@@ -5,21 +5,21 @@
 
 package_name="ardupilot-rover-3.3-pocket"
 debian_pkg_name="${package_name}"
-package_version="3.3.0-git20180419"
+package_version="3.3.1-git20180512"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/ArduPilot/ardupilot.git"
-git_sha="42b4991f6cd1ed7cc6313b0144aab255a04ff2b7"
+git_sha="bb43d01e5b6082875b6ce9484cbbca45b51590ff"
 git_branch="Rover-3.3"
 reprepro_dir="a/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee4"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20180427"
-stretch_version="~stretch+20180427"
-buster_version="~buster+20180427"
-bionic_version="~bionic+20180427"
+jessie_version="~jessie+20180516"
+stretch_version="~stretch+20180516"
+buster_version="~buster+20180516"
+bionic_version="~bionic+20180516"
