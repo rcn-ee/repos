77e1ba7c3de5d25dba9fc97bb9008cab9aa76b05
Robert Nelson <robertcnelson@gmail.com>
2018-05-18 09:40:58 -0500

    bb-node-red-installer (0.18.5-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bb-node-red-installer/version.sh
+++ debian-9-bb-node-red-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="0.18.4"
+package_version="0.18.5"
 package_source=""
 src_dir=""
 
@@ -11,20 +11,20 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
 dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/stretch"
 
-v6="v6.14.1"
-v8="v8.11.1"
+v6="v6.14.2"
+v8="v8.11.2"
 
-node_red="node-red-0.18.4-838c7a5"
-node_red_dashboard="node-red-dashboard-2.8.2"
+node_red="node-red-0.18.5-30c2aa9"
+node_red_dashboard="node-red-dashboard-2.9.3"
 node_red_mongodb="node-red-node-mongodb-0.0.14"
 node_red_gpio="node-red-contrib-gpio-0.14.0"
-node_red_serial="node-red-node-serialport-0.6.5"
+node_red_serial="node-red-node-serialport-0.6.6"
 node_red_can="node-red-contrib-can-1.0.6"
 
 debian_dl_1="${dl_mirror}/${node_red}-${v8}.tar.xz"
@@ -41,4 +41,4 @@ debian_dl_10="${dl_mirror}/${node_red_gpio}-${v6}.tar.xz"
 debian_dl_11="${dl_mirror}/${node_red_serial}-${v6}.tar.xz"
 debian_dl_12="${dl_mirror}/${node_red_can}-${v6}.tar.xz"
 
-stretch_version="~stretch+20180413"
+stretch_version="~stretch+20180518"
