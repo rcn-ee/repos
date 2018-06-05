c75edc75d7d8738125df7252132a512a1805924d
Robert Nelson <robertcnelson@gmail.com>
2018-06-05 18:52:07 -0500

    bb-johnny-five-installer (0.15.0-0rcnee1~bionic)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ubuntu-1804-bb-johnny-five-installer/version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+package_name="bb-johnny-five-installer"
+debian_pkg_name="${package_name}"
+package_version="0.15.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee1"
+debian_untar=""
+debian_patch=""
+
+dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/bionic"
+
+v8="v8.10.0"
+
+node_red_five="johnny-five-0.15.0"
+
+debian_dl_1="${dl_mirror}/${node_red_five}-${v8}.tar.xz"
+
+bionic_version="~bionic+20180605"
