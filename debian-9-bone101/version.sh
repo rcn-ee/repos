5c81bb685ec7828c418d4a5a8fcda3a544324711
Robert Nelson <robertcnelson@gmail.com>
2018-06-07 14:23:09 -0500

    bone101 (1.1.11-0rcnee1~stretch+20180607)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bone101/version.sh
+++ debian-9-bone101/version.sh
@@ -11,14 +11,15 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
 sbuild_chroot=""
 
 dl_mirror="https://github.com/rcn-ee/npm-package-bb-doc-bone101/raw/master/deploy/stretch/"
 
-v6="v6.10.3"
+v6="v6.14.2"
+v8="v8.11.2"
 
     node_async="async-2.0.0-rc.6"
 node_sensortag="sensortag-1.2.3"
@@ -26,4 +27,7 @@ node_sensortag="sensortag-1.2.3"
 debian_dl_1="${dl_mirror}/${node_async}-${v6}.tar.xz"
 debian_dl_2="${dl_mirror}/${node_sensortag}-${v6}.tar.xz"
 
-stretch_version="~stretch+20180328"
+debian_dl_3="${dl_mirror}/${node_async}-${v8}.tar.xz"
+debian_dl_4="${dl_mirror}/${node_sensortag}-${v8}.tar.xz"
+
+stretch_version="~stretch+20180607"
