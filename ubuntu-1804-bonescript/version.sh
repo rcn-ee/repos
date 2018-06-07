bf09395ddbcd19c8d5ba605c3b760efa84619a10
Robert Nelson <robertcnelson@gmail.com>
2018-06-07 15:58:30 -0500

    bonescript (0.6.3-0rcnee2~bionic+20180607)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ubuntu-1804-bonescript/version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+package_name="bonescript"
+debian_pkg_name="${package_name}"
+package_version="0.6.3"
+package_source="${package_name}_${package_version}.orig.tar.gz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee2"
+debian_untar=""
+debian_patch=""
+
+dl_mirror="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/bionic/"
+
+v8="v8.10.0"
+
+bonescript="bonescript-0.6.3-9221fee"
+   winston="winston-2.1.1"
+
+debian_dl_1="${dl_mirror}/${bonescript}-${v8}.tar.xz"
+debian_dl_2="${dl_mirror}/${winston}-${v8}.tar.xz"
+
+bionic_version="~bionic+20180607"
