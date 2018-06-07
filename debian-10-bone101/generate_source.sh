97fa06dd9f9ab4a121c16ee1c99e7c1d59c5964c
Robert Nelson <robertcnelson@gmail.com>
2018-06-07 14:26:10 -0500

    bone101 (1.1.11-0rcnee1~buster+20180607)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bone101/generate_source.sh
+++ debian-10-bone101/generate_source.sh
@@ -3,7 +3,11 @@
 . version.sh
 
 if [ ! -f ${package_name}_${package_version}.orig.tar.gz ] ; then
-	wget https://github.com/beagleboard/bone101/archive/${package_version}.tar.gz
+	wget -c http://repos.rcn-ee.net/debian/pool/main/b/bone101/${package_name}_${package_version}.orig.tar.gz || true
+fi
+
+if [ ! -f ${package_name}_${package_version}.orig.tar.gz ] ; then
+	wget https://github.com/jadonk/bone101/archive/${package_version}.tar.gz
 	mv ${package_version}.tar.gz ${package_name}_${package_version}.orig.tar.gz
 fi
 
--- debian-10-bone101/version.sh
+++ debian-10-bone101/version.sh
@@ -11,14 +11,14 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
 sbuild_chroot=""
 
 dl_mirror="https://github.com/rcn-ee/npm-package-bb-doc-bone101/raw/master/deploy/buster/"
 
-v8="v8.9.1"
+v8="v8.11.2"
 
     node_async="async-2.0.0-rc.6"
 node_sensortag="sensortag-1.2.3"
@@ -26,4 +26,4 @@ node_sensortag="sensortag-1.2.3"
 debian_dl_1="${dl_mirror}/${node_async}-${v8}.tar.xz"
 debian_dl_2="${dl_mirror}/${node_sensortag}-${v8}.tar.xz"
 
-buster_version="~buster+20180328"
+buster_version="~buster+20180607"
