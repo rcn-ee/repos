573fd4ea5f328cb6a8b9de923969d3cb5da4ca1d
Robert Nelson <robertcnelson@gmail.com>
2018-06-07 11:31:10 -0500

    wpa (2.6-16rcnee1~bionic+20180607)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ubuntu-1804-wpa/version.sh
+++ ubuntu-1804-wpa/version.sh
@@ -16,6 +16,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-16"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee0"
+local_patch="rcnee1"
 
 bionic_version="~bionic+20180607"
