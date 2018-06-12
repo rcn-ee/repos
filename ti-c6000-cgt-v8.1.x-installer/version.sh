658d63070c5c819e231f2bc017791d1aabc37788
Robert Nelson <robertcnelson@gmail.com>
2018-06-12 12:06:22 -0500

    ti-c6000-cgt-v8.1.x-installer (8.1.7-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-c6000-cgt-v8.1.x-installer/version.sh
+++ ti-c6000-cgt-v8.1.x-installer/version.sh
@@ -4,7 +4,7 @@
 
 package_name="ti-c6000-cgt-v8.1.x-installer"
 debian_pkg_name="${package_name}"
-package_version="8.1.5"
+package_version="8.1.7"
 package_source=""
 src_dir=""
 
@@ -13,12 +13,12 @@ git_sha=""
 reprepro_dir="t/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~jessie+20180104"
-stretch_version="~stretch+20180104"
-buster_version="~buster+20180104"
-xenial_version="~xenial+20180104"
-bionic_version="~bionic+20180104"
+jessie_version="~jessie+20180612"
+stretch_version="~stretch+20180612"
+buster_version="~buster+20180612"
+xenial_version="~xenial+20180612"
+bionic_version="~bionic+20180612"
