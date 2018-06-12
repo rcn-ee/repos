270267d6fd3acffb736d4e7fca0b5fcb2259f504
Robert Nelson <robertcnelson@gmail.com>
2018-06-11 19:53:05 -0500

    c9-core-installer (3.1.5000.git20180124-0rcnee0~bionic)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ubuntu-1804-c9-core-installer/version.sh
+++ ubuntu-1804-c9-core-installer/version.sh
@@ -4,7 +4,7 @@
 
 package_name="c9-core-installer"
 debian_pkg_name="${package_name}"
-package_version="3.1.4056.git20171215"
+package_version="3.1.5000.git20180124"
 package_source=""
 src_dir=""
 
@@ -13,7 +13,7 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
@@ -23,4 +23,4 @@ v8="v8.10.0"
 
 debian_dl_1="${dl_mirror}/c9-core_${package_version}-${v8}-build.tar.xz"
 
-bionic_version="~bionic+20180607"
+bionic_version="~bionic+20180611"
