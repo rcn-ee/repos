54709d5d341defa28588359164269c7fd00e5642
Robert Nelson <robertcnelson@gmail.com>
2018-06-12 15:34:33 -0500

    ardupilot-copter-3.6-pocket (3.6.0-git20180605)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-ardupilot-copter-3.6-pocket/version.sh
+++ git-ardupilot-copter-3.6-pocket/version.sh
@@ -5,21 +5,21 @@
 
 package_name="ardupilot-copter-3.6-pocket"
 debian_pkg_name="${package_name}"
-package_version="3.6.0-git20180425"
+package_version="3.6.0-git20180605"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/ArduPilot/ardupilot.git"
-git_sha="d846a122f0d807b1075467f7a4af78783b4c6003"
+git_sha="2a4b780594bedffaca0a78e2b2a8b8f778fa0bb8"
 git_branch="Copter-3.6"
 reprepro_dir="a/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20180516"
-stretch_version="~stretch+20180516"
-buster_version="~buster+20180516"
-bionic_version="~bionic+20180516"
+jessie_version="~jessie+20180612"
+stretch_version="~stretch+20180612"
+buster_version="~buster+20180612"
+bionic_version="~bionic+20180612"
