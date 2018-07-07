c807601c0d8ef93cbfdbcee02da93f6c3d490fb4
Robert Nelson <robertcnelson@gmail.com>
2018-07-07 07:57:53 -0500

    librobotcontrol (1.0.0-git20180706.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-librobotcontrol/version.sh
+++ git-librobotcontrol/version.sh
@@ -4,12 +4,12 @@
 
 package_name="librobotcontrol"
 debian_pkg_name="${package_name}"
-package_version="1.0.0-git20180705.0"
+package_version="1.0.0-git20180706.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/StrawsonDesign/librobotcontrol"
-git_sha="a3265e6fedc5fa05444878739d5d46ad0f819edb"
+git_sha="148ef0299bf76240807be216a7b17a7ce2a020b4"
 reprepro_dir="l/${package_name}"
 dl_path=""
 
@@ -17,4 +17,4 @@ debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-buster_version="~buster+20180706"
+buster_version="~buster+20180707"
