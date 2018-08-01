de298e2fe884c832c47c3fe2f5ac5b5f914e2671
Robert Nelson <robertcnelson@gmail.com>
2018-07-31 20:34:42 -0500

    opencv (3.2.0+dfsg-4.1rcnee2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-opencv/version.sh
+++ debian-10-opencv/version.sh
@@ -16,6 +16,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-4.1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee1"
+local_patch="rcnee2"
 
 buster_version="~buster+20180731"
