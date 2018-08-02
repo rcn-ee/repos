8f8850cfecd7ea17ab0260dc4f11a907f62080b8
Robert Nelson <robertcnelson@gmail.com>
2018-08-02 08:46:54 -0500

    split ti-llvm-3.6 into 3 build units
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-llvm-3.6-9-debian/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#http://git.ti.com/gitweb/?p=opencl/llvm.git;a=shortlog;h=refs/heads/release_36_ti
+#http://git.ti.com/gitweb/?p=opencl/llvm.git;a=shortlog;h=refs/tags/v01.01.07.01
+
+package_name="ti-llvm-3.6"
+debian_pkg_name="${package_name}"
+package_version="3.6-git20170214"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-3.6"
+
+git_repo="git://git.ti.com/opencl/llvm.git"
+git_sha="8ff2a35c586a3d3f624712e0943777613a017bc5"
+reprepro_dir="t/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-r5rcnee3"
+debian_patch=""
+debian_diff=""
+
+stretch_version="~stretch+20180801"
+buster_version="~buster+20180801"
+bionic_version="~bionic+20180801"
