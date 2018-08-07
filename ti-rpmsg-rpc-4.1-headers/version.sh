24d9cb14b01f7a4217eafd3f3a7529d5f9eee233
Robert Nelson <robertcnelson@gmail.com>
2018-08-07 14:25:31 -0500

    rpmsg-rpc-4.1-headers (4.1.20180807-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-rpmsg-rpc-4.1-headers/version.sh
+++ ti-rpmsg-rpc-4.1-headers/version.sh
@@ -2,7 +2,7 @@
 
 package_name="rpmsg-rpc-4.1-headers"
 debian_pkg_name="${package_name}"
-package_version="4.1.20160707"
+package_version="4.1.20180807"
 package_source=""
 src_dir=""
 
@@ -11,10 +11,12 @@ git_sha=""
 reprepro_dir="r/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~bpo80+20160707+1"
-stretch_version="~bpo90+20160707+1"
-xenial_version="~bpo1604+20160707+1"
+jessie_version="~jessie+20180807"
+stretch_version="~stretch+20180807"
+buster_version="~buster+20180807"
+xenial_version="~xenial+20180807"
+bionic_version="~bionic+20180807"
