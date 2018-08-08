f264fb1083fe0aa4d59b81381d3511bb8142b3a2
Robert Nelson <robertcnelson@gmail.com>
2018-08-08 09:28:04 -0500

    ti-opencl-testing
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-opencl-testing/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+#http://git.ti.com/gitweb/?p=opencl/ti-opencl.git;a=summary
+
+package_name="ti-opencl"
+debian_pkg_name="${package_name}"
+package_version="01.01.13.00"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="git://git.ti.com/opencl/ti-opencl.git"
+git_sha="c2493c7190c7e5b4135f64d2aacc2de3ff57e0bb"
+reprepro_dir="c/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-r0.0rcnee7"
+debian_patch=""
+debian_diff=""
+
+stretch_version="~stretch+20180808"
