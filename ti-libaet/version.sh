061892e6bd34e0f6f59216cea50248b5abca8a01
Robert Nelson <robertcnelson@gmail.com>
2018-08-08 11:09:05 -0500

    ti-libaet (4.19.0.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-libaet/version.sh
+++ ti-libaet/version.sh
@@ -14,9 +14,10 @@ git_sha="de7954abab0f0caa8a2c7b53095c30226d901a31"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee3"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-
 stretch_version="~stretch+20180808"
+buster_version="~buster+20180808"
+bionic_version="~bionic+20180808"
