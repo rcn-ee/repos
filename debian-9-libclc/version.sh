4811e4a1dedeafc1466cca927a2ae4e1d31b627e
Robert Nelson <robertcnelson@gmail.com>
2018-08-09 08:55:28 -0500

    no folder
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-libclc/version.sh
+++ debian-9-libclc/version.sh
@@ -7,7 +7,7 @@ package_name="libclc"
 debian_pkg_name="${package_name}"
 package_version="0.2.0+git20180518"
 package_source="${package_name}_${package_version}.orig.tar.xz"
-src_dir="${package_name}-${package_version}"
+src_dir=""
 
 git_repo=""
 git_sha=""
