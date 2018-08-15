dc71966562508aee26c26f4595a148dc19700d0a
Robert Nelson <robertcnelson@gmail.com>
2018-08-15 12:14:03 -0500

    bb-customizations (1.20180815)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20180715"
+package_version="1.20180815"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,8 @@ debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~jessie+20180715"
-stretch_version="~stretch+20180715"
-buster_version="~buster+20180715"
-xenial_version="~xenial+20180715"
-bionic_version="~bionic+20180715"
+jessie_version="~jessie+20180815"
+stretch_version="~stretch+20180815"
+buster_version="~buster+20180815"
+xenial_version="~xenial+20180815"
+bionic_version="~bionic+20180815"
