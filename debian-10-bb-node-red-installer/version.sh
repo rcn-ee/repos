1b150eb2175ed2c180c1bf9742cbf432528f9cf3
Robert Nelson <robertcnelson@gmail.com>
2018-08-16 11:34:51 -0500

    bb-node-red-installer (0.19.1-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bb-node-red-installer/version.sh
+++ debian-10-bb-node-red-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="0.18.7"
+package_version="0.19.1"
 package_source=""
 src_dir=""
 
@@ -11,7 +11,7 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee3"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
@@ -19,16 +19,20 @@ dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/bust
 
 v8="v8.11.2"
 
-node_red="node-red-0.18.7-9d219c16"
-node_red_dashboard="node-red-dashboard-2.9.4"
-node_red_gpio="node-red-contrib-gpio-0.14.0"
-node_red_serial="node-red-node-serialport-0.6.7"
-node_red_can="node-red-contrib-can-1.0.6"
+           node_red="node-red-0.19.1-28b311b7"
+       node_red_can="node-red-contrib-can-1.0.6"
+      node_red_gpio="node-red-contrib-gpio-0.14.0"
+node_red_mjpgcamera="node-red-contrib-mjpgcamera-1.0.4"
+  node_red_particle="node-red-contrib-particle-0.0.7"
+ node_red_dashboard="node-red-dashboard-2.9.6"
+    node_red_serial="node-red-node-serialport-0.6.8"
 
 debian_dl_1="${dl_mirror}/${node_red}-${v8}.tar.xz"
-debian_dl_2="${dl_mirror}/${node_red_dashboard}-${v8}.tar.xz"
+debian_dl_2="${dl_mirror}/${node_red_can}-${v8}.tar.xz"
 debian_dl_3="${dl_mirror}/${node_red_gpio}-${v8}.tar.xz"
-debian_dl_4="${dl_mirror}/${node_red_serial}-${v8}.tar.xz"
-debian_dl_5="${dl_mirror}/${node_red_can}-${v8}.tar.xz"
+debian_dl_4="${dl_mirror}/${node_red_mjpgcamera}-${v8}.tar.xz"
+debian_dl_5="${dl_mirror}/${node_red_particle}-${v8}.tar.xz"
+debian_dl_6="${dl_mirror}/${node_red_dashboard}-${v8}.tar.xz"
+debian_dl_7="${dl_mirror}/${node_red_serial}-${v8}.tar.xz"
 
-buster_version="~buster+20180618"
+buster_version="~buster+20180816"
