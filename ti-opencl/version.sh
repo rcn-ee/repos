f53d668c3112c4dee49ea5baf14fc83042573847
Robert Nelson <robertcnelson@gmail.com>
2018-08-17 10:39:38 -0500

    ti-opencl (01.01.15.03-git20180817.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-opencl/version.sh
+++ ti-opencl/version.sh
@@ -4,19 +4,19 @@
 
 package_name="ti-opencl"
 debian_pkg_name="${package_name}"
-package_version="01.01.15.03-git20180816.1"
+package_version="01.01.15.03-git20180817.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/rcn-ee/ti-opencl.git"
-git_sha="01fb2f96de6dc43e0e4a3ee56c315e7fa9ce5469"
+git_sha="b3ea7e860c9d8ead1ba0ffbb937a59342b1b05de"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-r0.0rcnee10"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-stretch_version="~stretch+20180816"
-buster_version="~buster+20180816"
-bionic_version="~bionic+20180816"
+stretch_version="~stretch+20180817"
+buster_version="~buster+20180817"
+bionic_version="~bionic+20180817"
