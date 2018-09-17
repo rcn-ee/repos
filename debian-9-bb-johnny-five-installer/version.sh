4e103a198d92d5f9bfac367fe0c595783268594b
Robert Nelson <robertcnelson@gmail.com>
2018-09-17 14:27:19 -0500

    bb-johnny-five-installer (1.0.0-0rcnee1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bb-johnny-five-installer/version.sh
+++ debian-9-bb-johnny-five-installer/version.sh
@@ -11,18 +11,18 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
 
 dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/stretch"
 
 v6="v6.14.4"
-v8="v8.11.4"
+v8="v8.12.0"
 
 node_red_five="johnny-five-1.0.0"
 
 debian_dl_1="${dl_mirror}/${node_red_five}-${v6}.tar.xz"
 debian_dl_2="${dl_mirror}/${node_red_five}-${v8}.tar.xz"
 
-stretch_version="~stretch+20180828"
+stretch_version="~stretch+20180917"
