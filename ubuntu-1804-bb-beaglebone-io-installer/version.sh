dbf38bb75a2b049c0a6ff2d8ab60c62cdb6a8e95
Robert Nelson <robertcnelson@gmail.com>
2018-12-01 20:03:55 -0600

    bb-beaglebone-io-installer (4.0.0-0rcnee4)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ubuntu-1804-bb-beaglebone-io-installer/version.sh
+++ ubuntu-1804-bb-beaglebone-io-installer/version.sh
@@ -11,16 +11,17 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee3"
+debian_version="${package_version}-0rcnee4"
 debian_untar=""
 debian_patch=""
 
-dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/bionic"
+#dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/bionic"
+dl_mirror="http://gitlab.gfnd.rcn-ee.org/RobertCNelson/npm-package-node-red/raw/master/deploy/bionic"
 
-v8="v8.10.0_0.3"
+v8="v8.10.0_0.4"
 
 node_red_bbio="beaglebone-io-4.0.0-5677917"
 
 debian_dl_1="${dl_mirror}/${node_red_bbio}-${v8}.tar.xz"
 
-bionic_version="~bionic+20181004"
+bionic_version="~bionic+20181201"
