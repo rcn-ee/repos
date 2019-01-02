f3722be2d6180c6b46b73f6669e5301fe622b673
Robert Nelson <robertcnelson@gmail.com>
2019-01-02 14:53:10 -0600

    ipumm-dra7xx-installer (3.00.15.00-r3rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-ipumm-dra7xx-installer/version.sh
+++ ti-ipumm-dra7xx-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="ipumm-dra7xx-installer"
 debian_pkg_name="${package_name}"
-package_version="3.00.15.00-r1"
+package_version="3.00.15.00-r3"
 package_source=""
 src_dir=""
 
@@ -18,8 +18,8 @@ debian_patch=""
 debian_dl_1="https://github.com/rcn-ee/sdk-firmware/raw/master/ipumm-fw/${package_version}/files/dra7-ipu2-fw.xem4"
 debian_dl_2="https://github.com/rcn-ee/sdk-firmware/raw/master/ipumm-fw/${package_version}/files/dra7-ipu2-fw.xem4.map"
 
-jessie_version="~jessie+20180731"
-stretch_version="~stretch+20180731"
-buster_version="~buster+20180731"
-xenial_version="~xenial+20180731"
-bionic_version="~bionic+20180731"
+jessie_version="~jessie+20190102"
+stretch_version="~stretch+20190102"
+buster_version="~buster+20190102"
+xenial_version="~xenial+20190102"
+bionic_version="~bionic+20190102"
