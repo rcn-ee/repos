87555fe4260cef3039877b852ba187bd5639d889
Robert Nelson <robertcnelson@gmail.com>
2019-01-03 09:10:31 -0600

    chromium-browser (71.0.3578.80-1~deb9u1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-chromium-browser/version.sh
+++ debian-9-chromium-browser/version.sh
@@ -5,11 +5,12 @@
 #mirror="http://http.debian.net/debian"
 mirror="http://security.debian.org/debian-security"
 #https://snapshot.debian.org/archive/debian-security/20180701T010208Z/pool/updates/main/c/chromium-browser/chromium_67.0.3396.87-1~deb9u1_amd64.deb
-mirror="https://snapshot.debian.org/archive/debian-security/20180701T010208Z/"
+#mirror="https://snapshot.debian.org/archive/debian-security/20180701T010208Z/"
 
 package_name="chromium-browser"
 debian_pkg_name="${package_name}"
-package_version="67.0.3396.87"
+#package_version="67.0.3396.87"
+package_version="71.0.3578.80"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="chromium-${package_version}"
 
@@ -23,4 +24,4 @@ debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="rcnee0"
 
-stretch_version="~stretch+20180925"
+stretch_version="~stretch+20190103"
