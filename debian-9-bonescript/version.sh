c09621335046db301944a9d52db9b1a46a3f45b6
Robert Nelson <robertcnelson@gmail.com>
2019-01-24 11:00:17 -0600

    bonescript (0.7.1-git20190123.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bonescript/version.sh
+++ debian-9-bonescript/version.sh
@@ -2,27 +2,28 @@
 
 package_name="bonescript"
 debian_pkg_name="${package_name}"
-package_version="0.7.0-git20180821.0"
+package_version="0.7.1-git20190123.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/jadonk/bonescript"
-git_sha="b968db7a2e051e4c7eae0accf113c13db1ef7ef7"
+git_sha="1dcc7ffa227a1e2da4655a695f3307f4c4b52719"
 reprepro_dir="b/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-dl_mirror="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/stretch/"
+#dl_mirror="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/stretch/"
+dl_mirror="http://gitlab.gfnd.rcn-ee.org/RobertCNelson/npm-package-bonescript/raw/master/deploy/stretch"
 
-v6="v6.14.4"
-v8="v8.11.4"
+v6="v6.16.0"
+v8="v8.15.0"
 
-datestamp="20180821"
+datestamp="20190124"
 
-bonescript="bonescript-0.7.0-b968db7"
+bonescript="bonescript-0.7.1-1dcc7ff"
    winston="winston-2.1.1"
 
 debian_dl_1="${dl_mirror}/${bonescript}-${v6}-${datestamp}.tar.xz"
@@ -31,4 +32,4 @@ debian_dl_2="${dl_mirror}/${winston}-${v6}.tar.xz"
 debian_dl_3="${dl_mirror}/${bonescript}-${v8}-${datestamp}.tar.xz"
 debian_dl_4="${dl_mirror}/${winston}-${v8}.tar.xz"
 
-stretch_version="~stretch+20180821"
+stretch_version="~stretch+20190124"
