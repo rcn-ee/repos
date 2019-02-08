cb04de27b3ed55275394eff4e713013b110f3c31
Robert Nelson <robertcnelson@gmail.com>
2019-02-08 16:34:27 -0600

    bb-beaglebone-io-installer (4.0.0-0rcnee6)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bb-beaglebone-io-installer/version.sh
+++ debian-10-bb-beaglebone-io-installer/version.sh
@@ -11,17 +11,17 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0rcnee6"
 debian_untar=""
 debian_patch=""
 
-dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/buster"
+#dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/buster"
+dl_mirror="http://gitlab.gfnd.rcn-ee.org/RobertCNelson/npm-package-node-red/raw/master/deploy/buster"
 
-v6="v6.14.2"
-v8="v8.11.2"
+v10="v10.15.1"
 
 node_red_bbio="beaglebone-io-4.0.0-5677917"
 
-debian_dl_1="${dl_mirror}/${node_red_bbio}-${v8}.tar.xz"
+debian_dl_1="${dl_mirror}/${node_red_bbio}-${v10}.tar.xz"
 
-buster_version="~buster+20180604"
+buster_version="~buster+20190208"
