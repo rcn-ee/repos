cc14cd3d32e1e75ce240ef71a837f5220b1b135f
Robert Nelson <robertcnelson@gmail.com>
2019-02-11 13:47:10 -0600

    bonescript (0.7.1-git20190123.0-0rcnee3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bonescript/version.sh
+++ debian-10-bonescript/version.sh
@@ -11,21 +11,21 @@ git_sha="1dcc7ffa227a1e2da4655a695f3307f4c4b52719"
 reprepro_dir="b/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0rcnee3"
 debian_patch=""
 debian_diff=""
 
 #dl_mirror="https://github.com/rcn-ee/npm-package-bonescript/raw/master/deploy/buster/"
 dl_mirror="http://gitlab.gfnd.rcn-ee.org/RobertCNelson/npm-package-bonescript/raw/master/deploy/buster"
 
-v8="v8.11.2"
+v10="v10.15.1"
 
-datestamp="20190124"
+datestamp="20190211"
 
 bonescript="bonescript-0.7.1-1dcc7ff"
    winston="winston-2.1.1"
 
-debian_dl_1="${dl_mirror}/${bonescript}-${v8}-${datestamp}.tar.xz"
-debian_dl_2="${dl_mirror}/${winston}-${v8}.tar.xz"
+debian_dl_1="${dl_mirror}/${bonescript}-${v10}-${datestamp}.tar.xz"
+debian_dl_2="${dl_mirror}/${winston}-${v10}.tar.xz"
 
-buster_version="~buster+20190127"
+buster_version="~buster+20190211"
