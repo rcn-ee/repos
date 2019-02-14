8003f697daacb2760aff5b88aae423824ed06b8c
Robert Nelson <robertcnelson@gmail.com>
2019-02-14 14:28:50 -0600

    c9-core-installer (3.1.5000.git20180223-0rcnee4~buster)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-c9-core-installer/version.sh
+++ debian-10-c9-core-installer/version.sh
@@ -13,7 +13,7 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee3"
+debian_version="${package_version}-0rcnee4"
 debian_untar=""
 debian_patch=""
 
