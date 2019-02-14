d908a8173176486e6174e6ec24a4a20bb7153b31
Robert Nelson <robertcnelson@gmail.com>
2019-02-14 11:41:41 -0600

    bone101 (2.0.1-0rcnee0~buster)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bone101/version.sh
+++ debian-10-bone101/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bone101"
 debian_pkg_name="${package_name}"
-package_version="1.1.11"
+package_version="2.0.1"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -11,19 +11,18 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
-sbuild_chroot=""
 
-dl_mirror="https://github.com/rcn-ee/npm-package-bb-doc-bone101/raw/master/deploy/buster/"
+dl_mirror="http://gitlab.gfnd.rcn-ee.org/RobertCNelson/npm-package-node-red/raw/master/deploy/buster"
 
-v8="v8.11.2"
+v10="v10.15.1"
 
-    node_async="async-2.0.0-rc.6"
-node_sensortag="sensortag-1.2.3"
+    node_async="async-2.0.1"
+#node_sensortag="sensortag-1.3.0"
 
-debian_dl_1="${dl_mirror}/${node_async}-${v8}.tar.xz"
-debian_dl_2="${dl_mirror}/${node_sensortag}-${v8}.tar.xz"
+debian_dl_1="${dl_mirror}/${node_async}-${v10}.tar.xz"
+#debian_dl_2="${dl_mirror}/${node_sensortag}-${v10}.tar.xz"
 
-buster_version="~buster+20180607"
+buster_version="~buster+20190214"
