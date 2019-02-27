b252895f54cb0ed2c03a75952426e098e5890868
Robert Nelson <robertcnelson@gmail.com>
2019-02-27 11:00:21 -0600

    bb-cape-overlays (4.4.20190227.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-cape-overlays/version.sh
+++ bb-cape-overlays/version.sh
@@ -4,12 +4,12 @@
 
 package_name="bb-cape-overlays"
 debian_pkg_name="${package_name}"
-package_version="4.4.20190215.0"
+package_version="4.4.20190227.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/beagleboard/bb.org-overlays"
-git_sha="ee0f265de6dc9aa8145bc7da3d9eeec817360cd4"
+git_sha="3eae6276f9e0ea5f9aec3be30ac1424f8abc6a7c"
 reprepro_dir="b/${package_name}"
 dl_path=""
 
@@ -17,8 +17,8 @@ debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-jessie_version="~jessie+20190215"
-stretch_version="~stretch+20190215"
-buster_version="~buster+20190215"
-xenial_version="~xenial+20190215"
-bionic_version="~bionic+20190215"
+jessie_version="~jessie+20190227"
+stretch_version="~stretch+20190227"
+buster_version="~buster+20190227"
+xenial_version="~xenial+20190227"
+bionic_version="~bionic+20190227"
