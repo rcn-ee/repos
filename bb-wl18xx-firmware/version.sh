d20f557b2b791c8d9a4ee9eb9c659ddf493d9b69
Robert Nelson <robertcnelson@gmail.com>
2019-02-27 12:12:29 -0600

    bb-wl18xx-firmware (1.20190227.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/version.sh
+++ bb-wl18xx-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wl18xx-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20180517"
+package_version="1.20190227.0"
 package_source=""
 src_dir=""
 
@@ -15,9 +15,8 @@ debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~jessie+20180517"
-stretch_version="~stretch+20180517"
-buster_version="~buster+20180517"
-xenial_version="~xenial+20180517"
-artful_version="~artful+20180517"
-bionic_version="~bionic+20180517"
+jessie_version="~jessie+20190227"
+stretch_version="~stretch+20190227"
+buster_version="~buster+20190227"
+xenial_version="~xenial+20190227"
+bionic_version="~bionic+20190227"
