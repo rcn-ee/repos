1e74bc64e3e2ab4fcd9cdbd80bb8efbd70ad16dc
Robert Nelson <robertcnelson@gmail.com>
2019-02-27 13:48:48 -0600

    bb-customizations (1.20190227.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20190201"
+package_version="1.20190227.0"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,8 @@ debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~jessie+20190201"
-stretch_version="~stretch+20190201"
-buster_version="~buster+20190201"
-xenial_version="~xenial+20190201"
-bionic_version="~bionic+20190201"
+jessie_version="~jessie+20190227"
+stretch_version="~stretch+20190227"
+buster_version="~buster+20190227"
+xenial_version="~xenial+20190227"
+bionic_version="~bionic+20190227"
