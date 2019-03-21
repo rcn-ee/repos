6492369f0f4ca700eeac1fbbbb00950f6a72579d
Robert Nelson <robertcnelson@gmail.com>
2019-03-21 13:07:53 -0500

    q
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo9-quilt/version.sh
+++ debian-bpo9-quilt/version.sh
@@ -8,7 +8,7 @@ package_version="0.65"
 
 git_repo=""
 git_sha=""
-reprepro_dir="b/${package_name}"
+reprepro_dir="q/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-2~bpo9+1"
