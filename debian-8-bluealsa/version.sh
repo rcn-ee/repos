66ab37097c5cb4937aafc09e3f2ec8f0a11efeb7
Robert Nelson <robertcnelson@gmail.com>
2019-04-10 10:45:43 -0500

    bluealsa (0.10~buster+20190410)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-8-bluealsa/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+mirror="https://archive.raspberrypi.org/debian"
+
+package_name="bluealsa"
+debian_pkg_name="${package_name}"
+package_version="0.10"
+package_source="${package_name}_${package_version}.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}"
+#debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+
+jessie_version="~jessie+20190410"
+stretch_version="~stretch+20190410"
+buster_version="~buster+20190410"
+bionic_version="~bionic+20190410"
