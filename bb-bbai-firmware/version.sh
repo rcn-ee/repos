97415bc83a625cd9012ce929343c9b6e35ab281a
Robert Nelson <robertcnelson@gmail.com>
2019-07-10 16:29:47 -0500

    bb-bbai-firmware
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-bbai-firmware/version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+package_name="bb-bbai-firmware"
+debian_pkg_name="${package_name}"
+package_version="1.20190710.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee0"
+debian_untar=""
+debian_patch=""
+
+buster_version="~buster+20190710"
