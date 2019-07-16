8b17cf7d08d5a6032d4c83ae2e29bf5fbd854c1b
Robert Nelson <robertcnelson@gmail.com>
2019-07-16 14:51:39 -0500

    add pastebin backport
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo10-pastebinit/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="pastebinit"
+debian_pkg_name="${package_name}"
+package_version="1.5"
+
+git_repo=""
+git_sha=""
+reprepro_dir="p/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-2.3"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+
+dist="debian"
+suite="buster"
+deb_arch="armhf"
