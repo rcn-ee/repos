77f5cc5c069c2772a8ae448fb6f747729fe630a1
Robert Nelson <robertcnelson@gmail.com>
2019-08-02 17:45:02 -0500

    bone101 (2.1.1-0rcnee2~stretch+20190802)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-bone101/version.sh
+++ debian-9-bone101/version.sh
@@ -11,7 +11,7 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee2"
 debian_untar=""
 debian_patch=""
 
