d8ab1aad52a8cdca6a72d5e397351f1326102eb6
Robert Nelson <robertcnelson@gmail.com>
2019-09-03 15:44:39 -0500

    Revert "libiio (0.18-1rcnee0~buster+20190903)"
    Broken on stretch
    
    This reverts commit 1f0c908773063f8e40a7cc819ae4673664048373.
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-libiio/version.sh
+++ debian-9-libiio/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="libiio"
 debian_pkg_name="${package_name}"
-package_version="0.18"
+package_version="0.16"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -18,5 +18,4 @@ debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="rcnee0"
 
-stretch_version="~stretch+20190903"
-buster_version="~buster+20190903"
+stretch_version="~stretch+20190812"
