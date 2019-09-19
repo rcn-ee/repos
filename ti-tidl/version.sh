e6825eb87b36e74f14c018a537a9b98b91386b18
Robert Nelson <robertcnelson@gmail.com>
2019-09-19 18:02:28 -0500

    ti-tidl (01.02.02-bb.org-0.2-0rcnee0~stretch+20190919)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-tidl/version.sh
+++ ti-tidl/version.sh
@@ -4,16 +4,16 @@
 
 package_name="ti-tidl"
 debian_pkg_name="${package_name}"
-package_version="01.02.02-bb.org-0.1"
+package_version="01.02.02-bb.org-0.2"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/rcn-ee/tidl-api.git"
-git_sha="e421bdacb6fdf78a2cfb20fb988163dafc09afb8"
+git_sha="b7bc34215efeeb8b2a16bb6e2c7385875aeb539f"
 reprepro_dir="o/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
