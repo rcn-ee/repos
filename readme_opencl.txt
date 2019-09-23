73d084e61a0856d5f144e81f2816fc1e525c8c0d
Robert Nelson <robertcnelson@gmail.com>
2019-09-23 13:50:49 -0500

    ti-llvm-3.6 (3.6-git20190111.0-rcnee0~buster+20190923)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ readme_opencl.txt
@@ -0,0 +1,1 @@
+llvm: http://arago-project.org/git/?p=meta-arago.git;a=tree;f=meta-arago-extras/recipes-core/llvm;hb=HEAD
--- ti-llvm-3.6-10-debian/version.sh
+++ ti-llvm-3.6-10-debian/version.sh
@@ -2,22 +2,21 @@
 
 #http://git.ti.com/gitweb/?p=opencl/llvm.git;a=shortlog;h=refs/heads/release_36_ti
 #http://git.ti.com/gitweb/?p=opencl/llvm.git;a=shortlog;h=refs/tags/v01.01.07.01
+#http://arago-project.org/git/?p=meta-arago.git;a=blob;f=meta-arago-extras/recipes-core/llvm/ti-llvm3.6_3.6.bb;hb=HEAD
 
 package_name="ti-llvm-3.6"
 debian_pkg_name="${package_name}"
-package_version="3.6-git20170214"
+package_version="3.6-git20190111.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-3.6"
 
 git_repo="git://git.ti.com/opencl/llvm.git"
-git_sha="8ff2a35c586a3d3f624712e0943777613a017bc5"
+git_sha="09780c6750b30da81e4a0a805aedf1699fbc37c7"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-r5rcnee4"
+debian_version="${package_version}-rcnee0"
 debian_patch=""
 debian_diff=""
 
-stretch_version="~stretch+20180802"
-buster_version="~buster+20180802"
-bionic_version="~bionic+20180802"
+buster_version="~buster+20190923"
