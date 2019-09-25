c03e846f95ab10eabc0d8a3db79c31560ae2500a
Robert Nelson <robertcnelson@gmail.com>
2019-09-25 10:05:15 -0500

    rpmsg-rpc-4.1-headers (4.1.20190925-0rcnee0~bionic+20190925)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-rpmsg-rpc-4.1-headers/suite/buster/debian/linux/rpmsg_rpc.h
+++ ti-rpmsg-rpc-4.1-headers/suite/buster/debian/linux/rpmsg_rpc.h
@@ -2,7 +2,7 @@
 /*
  * Remote Processor Procedure Call Driver
  *
- * Copyright (C) 2012-2018 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (C) 2012-2019 Texas Instruments Incorporated - http://www.ti.com/
  */
 
 #ifndef _LINUX_RPMSG_RPC_H_
--- ti-rpmsg-rpc-4.1-headers/version.sh
+++ ti-rpmsg-rpc-4.1-headers/version.sh
@@ -2,7 +2,7 @@
 
 package_name="rpmsg-rpc-4.1-headers"
 debian_pkg_name="${package_name}"
-package_version="4.1.20180807"
+package_version="4.1.20190925"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,6 @@ debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-jessie_version="~jessie+20180807"
-stretch_version="~stretch+20180807"
-buster_version="~buster+20180807"
-xenial_version="~xenial+20180807"
-bionic_version="~bionic+20180807"
+stretch_version="~stretch+20190925"
+buster_version="~buster+20190925"
+bionic_version="~bionic+20190925"
