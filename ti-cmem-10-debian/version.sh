390a75afb610fdbdf7b04e2534eb62e3a929f048
Robert Nelson <robertcnelson@gmail.com>
2019-09-25 09:25:18 -0500

    cmem (4.16.00.00-git20181220.0-0rcnee0~buster+20190925)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-cmem-10-debian/version.sh
+++ ti-cmem-10-debian/version.sh
@@ -6,13 +6,13 @@
 
 package_name="cmem"
 debian_pkg_name="${package_name}"
-package_version="4.15.00.02-git20180821"
+package_version="4.16.00.00-git20181220.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/ipc/ludev.git"
 #http://git.ti.com/gitweb/?p=ipc/ludev.git;a=summary
-git_sha="0f39ed469f7c327cb0b03139976b511c337c7f6a"
+git_sha="4f970f053b42e8d2a6f1a8cd4b14786196468fd3"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
@@ -24,4 +24,4 @@ debian_diff=""
 #sed -i -e 's:4.11.00.00:4.12.00.00:g' ./debian/control
 #sed -i -e 's:4.11.00.00:4.12.00.00:g' ./debian/cmem-mod-dkms.dkms
 
-buster_version="~buster+20190102"
+buster_version="~buster+20190925"
