9e66dc815ba4e1bdd34dcf379f2ff2f217fedf5f
Robert Nelson <robertcnelson@gmail.com>
2019-09-25 10:23:11 -0500

    dsptop (1.4.0-git20190509.0-0rcnee0~bionic+20190925)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-dsptop/version.sh
+++ ti-dsptop/version.sh
@@ -4,19 +4,19 @@
 
 package_name="dsptop"
 debian_pkg_name="${package_name}"
-package_version="1.4.0-git20160915"
+package_version="1.4.0-git20190509.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/sdo-emu/dsptop.git"
-git_sha="0aedcabdbbcbfc7558f70fc50afc95583302a087"
+git_sha="25e823b04e62af224dce97b90359b7569ce3fbb5"
 reprepro_dir="d/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee3"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-stretch_version="~stretch+20180807"
-buster_version="~buster+20180807"
-bionic_version="~bionic+20180807"
+stretch_version="~stretch+20190925"
+buster_version="~buster+20190925"
+bionic_version="~bionic+20190925"
