6d7ac7846b33ca65a758ed693612697d9347e10d
Robert Nelson <robertcnelson@gmail.com>
2019-10-07 13:41:37 -0500

    fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/add_to_reprepro.sh
+++ tools/add_to_reprepro.sh
@@ -6,7 +6,7 @@ base="/var/www/html/rcn-ee.us/repos/"
 outgoing="/var/www/html/farm/outgoing"
 
 run () {
-	changes_file=$(ls ${outgoing}/${suite}/${deb_arch}/${debian_pkg_name}_${debian_version}/ | grep changes)
+	changes_file=$(ls ${outgoing}/${suite}/${deb_arch}/${debian_pkg_name}_${debian_version}/ | grep changes | grep -v source)
 
 	if [ ! "x${changes_file}" = "x" ] ; then
 		if [ -f ${outgoing}/${suite}/${deb_arch}/${debian_pkg_name}_${debian_version}/${changes_file} ] ; then
