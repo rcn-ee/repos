b89d2f45a8714c91d2e083c89d5098189fa3ba3d
Robert Nelson <robertcnelson@gmail.com>
2019-10-24 17:06:04 -0500

    bb-node-red-installer (1.0.2-0rcnee0~buster+20191024)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bb-node-red-installer/version.sh
+++ debian-10-bb-node-red-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="0.19.6"
+package_version="1.0.2"
 package_source=""
 src_dir=""
 
@@ -15,17 +15,16 @@ debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-#dl_mirror="https://github.com/rcn-ee/npm-package-node-red/raw/master/deploy/buster"
-dl_mirror="http://gitlab.gfnd.rcn-ee.org/RobertCNelson/npm-package-node-red/raw/master/deploy/buster"
+dl_mirror="http://git.gfnd.rcn-ee.org:82/cgit/npm-package-node-red-scripts/.git/tree/deploy/buster"
 
-v10="v10.15.1"
+v10="v10.15.2"
 
-           node_red="node-red-0.19.6-d8b4c1e2"
+           node_red="node-red-1.0.2-0febcf4f"
       node_red_gpio="node-red-contrib-gpio-0.14.0"
 node_red_mjpgcamera="node-red-contrib-mjpgcamera-1.0.4"
   node_red_particle="node-red-contrib-particle-1.1.0"
- node_red_dashboard="node-red-dashboard-2.14.0"
-    node_red_serial="node-red-node-serialport-0.7.0"
+ node_red_dashboard="node-red-dashboard-2.17.1"
+    node_red_serial="node-red-node-serialport-0.8.8"
    node_red_blockly="node-red-contrib-blockly-1.1.0"
 
 debian_dl_1="${dl_mirror}/${node_red}-${v10}.tar.xz"
@@ -36,4 +35,4 @@ debian_dl_5="${dl_mirror}/${node_red_dashboard}-${v10}.tar.xz"
 debian_dl_6="${dl_mirror}/${node_red_serial}-${v10}.tar.xz"
 debian_dl_7="${dl_mirror}/${node_red_blockly}-${v10}.tar.xz"
 
-buster_version="~buster+20190228"
+buster_version="~buster+20191024"
