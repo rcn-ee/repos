3935d594dc795f720ad56123b6305f759bfbfcba
Robert Nelson <robertcnelson@gmail.com>
2019-10-28 15:47:54 -0500

    npm (5.8.0+ds6-4rcnee0~buster+20191028)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-npm/version.sh
+++ debian-10-npm/version.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
-mirror="http://archive.ubuntu.com/ubuntu"
+mirror="http://http.debian.net/debian"
 
 package_name="npm"
 debian_pkg_name="${package_name}"
-package_version="3.5.2"
-package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
-src_dir="${package_name}_${package_version}.orig"
+package_version="5.8.0+ds6"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-5.8.0"
 
 git_repo=""
 git_sha=""
 reprepro_dir="n/${package_name}"
-dl_path="pool/universe/${reprepro_dir}/"
+dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0ubuntu4"
+debian_version="${package_version}-4"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="rcnee0"
 
-buster_version="~buster+20180604"
+buster_version="~buster+20191028"
