86437698e11f716b0e38c4ce6ffd3199646a0253
Robert Nelson <robertcnelson@gmail.com>
2019-10-29 16:53:19 -0500

    backport: pastebinit-151-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo10-pastebinit/version.sh
+++ debian-bpo10-pastebinit/version.sh
@@ -4,14 +4,14 @@ mirror="http://http.debian.net/debian"
 
 package_name="pastebinit"
 debian_pkg_name="${package_name}"
-package_version="1.5"
+package_version="1.5.1"
 
 git_repo=""
 git_sha=""
 reprepro_dir="p/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2.3"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
