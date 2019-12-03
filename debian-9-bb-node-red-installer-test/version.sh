deee7c6fd092483c78637b3923c5f485906c006c
Robert Nelson <robertcnelson@gmail.com>
2019-12-03 14:40:30 -0600

    crazy hacks
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-9-bb-node-red-installer-test/version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+package_name="bb-node-red-installer-test"
+debian_pkg_name="${package_name}"
+package_version="0.19.7"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee0"
+debian_untar=""
+debian_patch=""
+
+stretch_version="~stretch+20191203"
