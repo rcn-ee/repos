d4f55970c993f14646f74064b70e9abe8e0ab162
Robert Nelson <robertcnelson@gmail.com>
2019-12-04 13:51:18 -0600

    push out
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bb-node-red-installer/version.sh
+++ debian-10-bb-node-red-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="1.0.2"
+package_version="1.0.3"
 package_source=""
 src_dir=""
 
@@ -11,8 +11,8 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-buster_version="~buster+20191029"
+buster_version="~buster+20191204"
--- debian-9-bb-node-red-installer/version.sh
+++ debian-9-bb-node-red-installer/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="1.0.2"
+package_version="1.0.3"
 package_source=""
 src_dir=""
 
@@ -11,7 +11,7 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee5"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
