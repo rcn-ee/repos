10a4ef2d929f4efc676408b73f7876c65af6bcd3
Robert Nelson <robertcnelson@gmail.com>
2019-12-04 16:44:52 -0600

    start hacking this up
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ c9-core-installer/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+#https://github.com/c9/core
+
+package_name="c9-core-installer"
+debian_pkg_name="${package_name}"
+package_version="3.1.5000.git20180223.1_really3.1.4056.git20171215"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee1"
+debian_untar=""
+debian_patch=""
+
+stretch_version="~stretch+20191204"
