099bb47c02e1acd9381824f896f8054a3aa65888
Robert Nelson <robertcnelson@gmail.com>
2019-12-04 15:55:14 -0600

    bb-beaglebone-io-installer (4.0.1-0rcnee2~bionic+20191204)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-beaglebone-io-installer/version.sh
+++ bb-beaglebone-io-installer/version.sh
@@ -11,10 +11,10 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee2"
 debian_untar=""
 debian_patch=""
 
 stretch_version="~stretch+20191204"
-buster_version="~buster+20191024"
-bionic_version="~bionic+20181201"
+buster_version="~buster+20191204"
+bionic_version="~bionic+20191204"
