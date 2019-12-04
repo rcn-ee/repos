dfc988b088589d12a850e14172447b739d88b290
Robert Nelson <robertcnelson@gmail.com>
2019-12-04 13:13:21 -0600

    more fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bb-node-red-installer/version.sh
+++ debian-10-bb-node-red-installer/version.sh
@@ -11,28 +11,8 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee8"
+debian_version="${package_version}-0rcnee1"
 debian_untar=""
 debian_patch=""
-
-dl_mirror="http://git.gfnd.rcn-ee.org:82/cgit/npm-package-node-red-scripts/.git/plain/deploy/buster"
-
-v10="v10.15.2"
-
-           node_red="node-red-1.0.2"
-      node_red_gpio="node-red-contrib-gpio-0.14.0"
-node_red_mjpgcamera="node-red-contrib-mjpgcamera-1.0.4"
-  node_red_particle="node-red-contrib-particle-1.1.0"
- node_red_dashboard="node-red-dashboard-2.17.1"
-    node_red_serial="node-red-node-serialport-0.8.8"
-   node_red_blockly="node-red-contrib-blockly-1.1.0"
-
-debian_dl_1="${dl_mirror}/${node_red}-${v10}.tar.xz"
-debian_dl_2="${dl_mirror}/${node_red_gpio}-${v10}.tar.xz"
-debian_dl_3="${dl_mirror}/${node_red_mjpgcamera}-${v10}.tar.xz"
-debian_dl_4="${dl_mirror}/${node_red_particle}-${v10}.tar.xz"
-debian_dl_5="${dl_mirror}/${node_red_dashboard}-${v10}.tar.xz"
-debian_dl_6="${dl_mirror}/${node_red_serial}-${v10}.tar.xz"
-debian_dl_7="${dl_mirror}/${node_red_blockly}-${v10}.tar.xz"
 
 buster_version="~buster+20191029"
--- debian-9-bb-node-red-installer/suite/stretch/debian/node-red.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export NODE_PATH=/opt/node-red/lib/node_modules/
-cd /opt/node-red/lib/node_modules/node-red/
-/opt/node-red/bin/node --max-old-space-size=128 red.js
