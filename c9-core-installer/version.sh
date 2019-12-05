191e5cec2cdfcf7ea9bddb2026e1393cf572ca1f
Robert Nelson <robertcnelson@gmail.com>
2019-12-05 06:06:12 -0600

    more tweaks
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- c9-core-installer/version.sh
+++ c9-core-installer/version.sh
@@ -4,7 +4,7 @@
 
 package_name="c9-core-installer"
 debian_pkg_name="${package_name}"
-package_version="3.1.5000.git20180223.1~really3.1.4056.git20171215"
+package_version="3.1.5000.git20180223.1"
 package_source=""
 src_dir=""
 
@@ -13,8 +13,8 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee4"
+debian_version="${package_version}-0rcnee5~really3.1.4056.git20171215"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20191204"
+stretch_version="~stretch+20191205"
