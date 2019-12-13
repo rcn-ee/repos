806745891747340c069e48b9b59987dde3fd664f
Robert Nelson <robertcnelson@gmail.com>
2019-12-13 16:49:44 -0600

    add final
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ c9-core-installer-final/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#https://github.com/c9/core
+
+package_name="c9-core-installer"
+debian_pkg_name="${package_name}"
+package_version="3.1.5000.git20180223.1"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee24"
+debian_untar=""
+debian_patch=""
+
+debian_dl_1="https://nodejs.org/dist/latest-v8.x/node-v8.16.2-linux-armv7l.tar.xz"
+debian_dl_2="https://registry.npmjs.org/npm/-/npm-4.6.1.tgz"
+
+buster_version="~buster+20191213"
