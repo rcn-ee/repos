c6841443b9321519288eae5792314a8f71f41350
Robert Nelson <robertcnelson@gmail.com>
2019-12-13 16:55:23 -0600

    c9-core-installer (3.1.5000.git20181017.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- c9-core-installer-final/version.sh
+++ c9-core-installer-final/version.sh
@@ -4,7 +4,7 @@
 
 package_name="c9-core-installer"
 debian_pkg_name="${package_name}"
-package_version="3.1.5000.git20180223.1"
+package_version="3.1.5000.git20181017.0"
 package_source=""
 src_dir=""
 
@@ -13,7 +13,7 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee24"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
