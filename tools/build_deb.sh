04d2bd6549fc2af2148f366bfbb7f0c644e252a8
Robert Nelson <robertcnelson@gmail.com>
2019-12-13 12:14:21 -0600

    back to server
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/build_deb.sh
+++ tools/build_deb.sh
@@ -8,9 +8,9 @@ builder=`cat /etc/hostname`
 build () {
 	if [ -f ${localdir}/incoming/${suite}/${debian_pkg_name}_${debian_version}/${dsc_file} ] ; then
 		echo "-----------------"
-		echo "sbuild ${options} http://192.168.1.98/farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/${dsc_file}"
+		echo "sbuild ${options} http://gfnd.rcn-ee.org//farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/${dsc_file}"
 		echo "-----------------"
-		sudo sbuild ${options} http://192.168.1.98/farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/${dsc_file}
+		sudo sbuild ${options} http://gfnd.rcn-ee.org//farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/${dsc_file}
 #		sbuild ${options} http://192.168.1.98/farm/incoming/${suite}/${debian_pkg_name}_${debian_version}/${dsc_file}
 
 		if [ -f *.changes ] ; then
