a5cbd7dadf3fc3cc803e6e11ac8c3ff8fd127d3c
Robert Nelson <robertcnelson@gmail.com>
2020-01-23 14:57:17 -0600

    more
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-bone101/generate_source.sh
+++ debian-10-bone101/generate_source.sh
@@ -6,10 +6,10 @@ if [ ! -f ${package_name}_${package_version}.orig.tar.gz ] ; then
 	wget -c http://repos.rcn-ee.net/debian/pool/main/b/bone101/${package_name}_${package_version}.orig.tar.gz || true
 fi
 
-if [ ! -f ${package_name}_${package_version}.orig.tar.gz ] ; then
-	wget https://github.com/beagleboard/bone101/archive/${package_version}.tar.gz
-	mv ${package_version}.tar.gz ${package_name}_${package_version}.orig.tar.gz
-fi
+#if [ ! -f ${package_name}_${package_version}.orig.tar.gz ] ; then
+#	wget https://github.com/beagleboard/bone101/archive/${package_version}.tar.gz
+#	mv ${package_version}.tar.gz ${package_name}_${package_version}.orig.tar.gz
+#fi
 
 #git clone --bare https://github.com/beagleboard/cloud9-examples
 #tar cvf ./suite/buster/debian/cloud9-examples.git.tar cloud9-examples.git/
