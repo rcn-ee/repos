5c7b02b3ec89034448520921f75979894a0bef1a
Robert Nelson <robertcnelson@gmail.com>
2020-02-26 14:55:49 -0600

    bb-usb-gadgets (1.20200226.1-0rcnee0)

--- bb-usb-gadgets/get_latest.sh
+++ bb-usb-gadgets/get_latest.sh
@@ -19,5 +19,5 @@ dl_src
 
 wfile="autoconfigure_usb0.sh" ; wdir="/usr/bin/" ; copy_all
 wfile="autoconfigure_usb1.sh" ; wdir="/usr/bin/" ; copy_all
+wfile="bb_dnsmasq_config.sh"  ; wdir="/usr/bin/" ; copy_all
 wfile="bb_generate_mac.sh"    ; wdir="/usr/bin/" ; copy_all
-
--- bb-usb-gadgets/version.sh
+++ bb-usb-gadgets/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-usb-gadgets"
 debian_pkg_name="${package_name}"
-package_version="1.20200226.0"
+package_version="1.20200226.1"
 package_source=""
 src_dir=""
 
