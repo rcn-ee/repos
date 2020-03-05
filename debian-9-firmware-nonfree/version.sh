579c30359f17a54ea034d8ef5fc4d2cd193cccb0
Robert Nelson <robertcnelson@gmail.com>
2020-03-05 11:15:52 -0600

    firmware-nonfree (20190717-2rcnee1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-firmware-nonfree/version.sh
+++ debian-9-firmware-nonfree/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="firmware-nonfree"
 debian_pkg_name="${package_name}"
-package_version="20180825+dfsg"
+package_version="20190717"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${debian_pkg_name}-${package_version}"
 
@@ -14,10 +14,10 @@ git_sha=""
 reprepro_dir="f/${package_name}"
 dl_path="pool/non-free/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="rcnee1"
 
-stretch_version="~stretch+20181217"
-buster_version="~buster+20181217"
+stretch_version="~stretch+20200305"
+buster_version="~buster+20200305"
