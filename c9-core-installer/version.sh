ff11496fb9e273b8589a48584047bfc5cbf33ed3
Robert Nelson <robertcnelson@gmail.com>
2020-03-06 12:52:06 -0600

    c9-core-installer (3.1.5000.git20181017.1-0rcnee3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- c9-core-installer/version.sh
+++ c9-core-installer/version.sh
@@ -13,11 +13,11 @@ git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0rcnee3"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://nodejs.org/dist/latest-v10.x/node-v10.17.0-linux-armv7l.tar.xz"
+debian_dl_1="https://nodejs.org/dist/latest-v10.x/node-v10.19.0-linux-armv7l.tar.xz"
 debian_dl_2="https://registry.npmjs.org/npm/-/npm-4.6.1.tgz"
 
-buster_version="~buster+20191216"
+buster_version="~buster+20200306"
