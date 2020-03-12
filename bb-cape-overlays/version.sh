5d0a8746abd6853fbbf123015e22bf9b137c4d5b
Robert Nelson <robertcnelson@gmail.com>
2020-03-12 14:42:06 -0500

    bb-cape-overlays (4.14.20200312.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-cape-overlays/version.sh
+++ bb-cape-overlays/version.sh
@@ -4,12 +4,12 @@
 
 package_name="bb-cape-overlays"
 debian_pkg_name="${package_name}"
-package_version="4.14.20200306.0"
+package_version="4.14.20200312.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/beagleboard/bb.org-overlays"
-git_sha="f74f42e9d0a0ecb824d0664e9734777840116501"
+git_sha="d93cd690ed2f1ef2ccbc176f4acb2868cf144b46"
 reprepro_dir="b/${package_name}"
 dl_path=""
 
@@ -17,7 +17,7 @@ debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-stretch_version="~stretch+20200306"
-buster_version="~buster+20200306"
-xenial_version="~xenial+20200306"
-bionic_version="~bionic+20200306"
+stretch_version="~stretch+20200312"
+buster_version="~buster+20200312"
+xenial_version="~xenial+20200312"
+bionic_version="~bionic+20200312"
