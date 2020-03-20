ca2c7c348cad14af6e77e4c2fe49bcd67c548d4b
Robert Nelson <robertcnelson@gmail.com>
2020-03-19 22:20:27 -0500

    cmem (4.16.00.00-git20181220.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-cmem-10-debian/version.sh
+++ ti-cmem-10-debian/version.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
 #http://git.ti.com/gitweb/?p=ipc/ludev.git;a=summary
-#http://arago-project.org/git/?p=meta-ti.git;a=blob;f=recipes-bsp/cmem/cmem.inc;h=abbf249207d5eb66d0ff799953876e7a8fc0ee26;hb=HEAD
-#http://arago-project.org/git/?p=meta-ti.git;a=commitdiff;h=c6535857d680ad644ce8d07b6e42b6b8ebe380e3
+#http://arago-project.org/git/?p=meta-ti.git;a=tree;f=recipes-bsp/cmem;h=8a9606547ae4d30b5ef0da26fdd82dd70e9977f2;hb=refs/tags/ti2019.05
 
 package_name="cmem"
 debian_pkg_name="${package_name}"
-package_version="4.16.00.00-git20181220.0"
+package_version="4.16.00.00-git20181220.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
@@ -24,4 +23,4 @@ debian_diff=""
 #sed -i -e 's:4.11.00.00:4.12.00.00:g' ./debian/control
 #sed -i -e 's:4.11.00.00:4.12.00.00:g' ./debian/cmem-mod-dkms.dkms
 
-buster_version="~buster+20190925"
+buster_version="~buster+20200319"
