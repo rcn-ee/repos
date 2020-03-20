10e2be01d467de38548d4e83d3788fd60413c7e4
Robert Nelson <robertcnelson@gmail.com>
2020-03-19 22:13:47 -0500

    ti-ipc-dra7xx (3.50.04.08-rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-ipc-dra7xx-10-debian/version.sh
+++ ti-ipc-dra7xx-10-debian/version.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -e
 
 #http://git.ti.com/gitweb/?p=ipc/ipcdev.git;a=summary
-#http://arago-project.org/git/?p=meta-ti.git;a=blob;f=recipes-ti/ipc/ti-ipc_git.bb;hb=af3c69b8e3918c5c79506c66027109685a5c9296
-#http://arago-project.org/git/?p=meta-ti.git;a=history;f=recipes-ti/ipc;h=HEAD;hb=HEAD
+#http://arago-project.org/git/?p=meta-ti.git;a=tree;f=recipes-ti/ipc;h=355cfa0aa0639b3165c12f9a1edb2ea644d34f1c;hb=refs/tags/ti2019.05
 
 package_name="ti-ipc-dra7xx"
 debian_pkg_name="${package_name}"
-package_version="3.50.03.05"
+package_version="3.50.04.08"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/ipc/ipcdev.git"
-git_sha="7007b8adff409fd8c90757c9adafd264a3ff15e3"
+git_sha="1d53b856338b86d1979fe09345c0b382913a848a"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
@@ -19,4 +18,4 @@ debian_version="${package_version}-rcnee0"
 debian_patch=""
 debian_diff=""
 
-buster_version="~buster+20190925"
+buster_version="~buster+20200319"
