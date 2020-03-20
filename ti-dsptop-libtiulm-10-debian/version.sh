a0098209f84cf6c97b3400a46d1dedd71703e34a
Robert Nelson <robertcnelson@gmail.com>
2020-03-19 21:10:03 -0500

    dsptop-libtiulm-10
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-dsptop-libtiulm-10-debian/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#http://git.ti.com/gitweb/?p=sdo-emu/dsptop.git;a=summary
+
+package_name="dsptop"
+debian_pkg_name="${package_name}"
+package_version="1.4.0-git20190509.0"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="git://git.ti.com/sdo-emu/dsptop.git"
+git_sha="25e823b04e62af224dce97b90359b7569ce3fbb5"
+reprepro_dir="d/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0rcnee0"
+debian_patch=""
+debian_diff=""
+
+stretch_version="~stretch+20190925"
+buster_version="~buster+20190925"
+bionic_version="~bionic+20190925"
