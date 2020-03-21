6459a8f512edb626c51f882368f93d6ecff7a1d1
Robert Nelson <robertcnelson@gmail.com>
2020-03-21 10:51:01 -0500

    gnuprumcu (0.2.1-git20191109.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- gnuprumcu/version.sh
+++ gnuprumcu/version.sh
@@ -2,7 +2,7 @@
 
 package_name="gnuprumcu"
 debian_pkg_name="${package_name}"
-package_version="0.2.0-git20190822.0"
+package_version="0.2.1-git20191109.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
@@ -11,10 +11,10 @@ git_sha="81efdc0d816174840c71d40664ee6410f61a770a"
 reprepro_dir="b/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-buster_version="~buster+20200318"
-bullseye_version="~bullseye+20200318"
-bionic_version="~bionic+20200318"
+buster_version="~buster+20200321"
+bullseye_version="~bullseye+20200321"
+bionic_version="~bionic+20200321"
