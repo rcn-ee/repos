0c2e8bc3b8d8f7d5bd8d40ed36858fe6076e3ad4
Robert Nelson <robertcnelson@gmail.com>
2020-03-22 13:37:54 -0500

    Revert "chromium-browser (68.0.3440.75-1~deb9u1rcnee0)"
    This reverts commit 7a4a74254d575c1d7a037d598f0ed38138e3c3e1.
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-chromium-exp/version.sh
+++ debian-9-chromium-exp/version.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -e
 
-#https://snapshot.debian.org/binary/chromium/
-#oldest: https://snapshot.debian.org/package/chromium-browser/67.0.3396.87-1~deb9u1/#chromium_67.0.3396.87-1:7e:deb9u1
-#mirror="https://snapshot.debian.org/archive/debian-security/20180701T010208Z/"
-#
-#20200321:  https://snapshot.debian.org/package/chromium-browser/68.0.3440.75-1~deb9u1/#chromium_68.0.3440.75-1:7e:deb9u1
-#mirror="https://snapshot.debian.org/archive/debian-security/20180727T051456Z/"
+#http://security.debian.org/debian-security/pool/updates/main/c/chromium-browser/chromium-browser_69.0.3497.92-1~deb9u1.debian.tar.xz
 
-mirror="https://snapshot.debian.org/archive/debian-security/20180727T051456Z/"
+#mirror="http://http.debian.net/debian"
+mirror="http://security.debian.org/debian-security"
+#https://snapshot.debian.org/archive/debian-security/20180701T010208Z/pool/updates/main/c/chromium-browser/chromium_67.0.3396.87-1~deb9u1_amd64.deb
+mirror="https://snapshot.debian.org/archive/debian-security/20180701T010208Z/"
 
 package_name="chromium-browser"
 debian_pkg_name="${package_name}"
-package_version="68.0.3440.75"
+package_version="67.0.3396.87"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="chromium-${package_version}"
 
@@ -23,6 +21,6 @@ dl_path="pool/updates/main/${reprepro_dir}/"
 debian_version="${package_version}-1~deb9u1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee0"
+local_patch="rcnee1"
 
 stretch_version="~stretch+20200321"
