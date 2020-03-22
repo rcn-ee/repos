1b601e0e4c08564eb5a75c3931d50e2eb9c5c9ca
Robert Nelson <robertcnelson@gmail.com>
2020-03-22 10:11:40 -0500

    bb-wl18xx-firmware (1.20200322.0)
    fixes: https://github.com/beagleboard/Latest-Images/issues/17
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/version.sh
+++ bb-wl18xx-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wl18xx-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20200226.1"
+package_version="1.20200322.0"
 package_source=""
 src_dir=""
 
@@ -16,7 +16,7 @@ debian_untar=""
 debian_patch=""
 
 jessie_version="~jessie+20190227"
-stretch_version="~stretch+20200226"
-buster_version="~buster+20200226"
-xenial_version="~xenial+20200226"
-bionic_version="~bionic+20200226"
+stretch_version="~stretch+20200322"
+buster_version="~buster+20200322"
+xenial_version="~xenial+20200322"
+bionic_version="~bionic+20200322"
