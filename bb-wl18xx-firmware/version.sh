fb8390a4555b1df50acc373e8d393c5a395dd2a4
Robert Nelson <robertcnelson@gmail.com>
2020-04-20 11:02:40 -0500

    bb-wl18xx-firmware (1.20200420.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/version.sh
+++ bb-wl18xx-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wl18xx-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20200322.0"
+package_version="1.20200420.0"
 package_source=""
 src_dir=""
 
@@ -16,7 +16,7 @@ debian_untar=""
 debian_patch=""
 
 jessie_version="~jessie+20190227"
-stretch_version="~stretch+20200322"
-buster_version="~buster+20200322"
-xenial_version="~xenial+20200322"
-bionic_version="~bionic+20200322"
+stretch_version="~stretch+20200420"
+buster_version="~buster+20200420"
+xenial_version="~xenial+20200420"
+bionic_version="~bionic+20200420"
