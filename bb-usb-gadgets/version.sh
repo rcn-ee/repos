c0054bfd0b3d3e8f1de790401e04884f1c4ef1c2
Robert Nelson <robertcnelson@gmail.com>
2020-04-24 10:35:27 -0500

    bb-usb-gadgets (1.20200322.1-0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-usb-gadgets/version.sh
+++ bb-usb-gadgets/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-usb-gadgets"
 debian_pkg_name="${package_name}"
-package_version="1.20200322.0"
+package_version="1.20200322.1"
 package_source=""
 src_dir=""
 
@@ -11,11 +11,12 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20200322"
-buster_version="~buster+20200322"
 xenial_version="~xenial+20200322"
-bionic_version="~bionic+20200322"
+stretch_version="~stretch+20200424"
+buster_version="~buster+20200424"
+bionic_version="~bionic+20200424"
+focal_version="~focal+20200424"
