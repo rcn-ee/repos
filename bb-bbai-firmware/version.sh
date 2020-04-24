33ed31c54c958cf5669c6e8a49bf86b425bbaf25
Robert Nelson <robertcnelson@gmail.com>
2020-04-24 10:49:34 -0500

    bb-bbai-firmware (1.20200404.1-0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-bbai-firmware/version.sh
+++ bb-bbai-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-bbai-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20200404.0"
+package_version="1.20200404.1"
 package_source=""
 src_dir=""
 
@@ -11,10 +11,11 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20200404"
-buster_version="~buster+20200404"
-bionic_version="~bionic+20200404"
+stretch_version="~stretch+20200424"
+buster_version="~buster+20200424"
+bionic_version="~bionic+20200424"
+focal_version="~focal+20200424"
