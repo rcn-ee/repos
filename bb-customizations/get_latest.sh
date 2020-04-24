d44ea9a81a265478597153bcdc480e74d1939ca0
Robert Nelson <robertcnelson@gmail.com>
2020-04-24 10:34:11 -0500

    bb-customizations (1.20200423.1-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/get_latest.sh
+++ bb-customizations/get_latest.sh
@@ -10,8 +10,8 @@ dl_src () {
 }
 
 copy_all () {
-	cp -v ./src${wdir}${wfile} ./suite/xenial/debian/
 	cp -v ./src${wdir}${wfile} ./suite/bionic/debian/
+	cp -v ./src${wdir}${wfile} ./suite/focal/debian/
 	cp -v ./src${wdir}${wfile} ./suite/stretch/debian/
 	cp -v ./src${wdir}${wfile} ./suite/buster/debian/
 }
--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20200423.0"
+package_version="1.20200423.1"
 package_source=""
 src_dir=""
 
@@ -15,7 +15,8 @@ debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20200423"
-buster_version="~buster+20200423"
 xenial_version="~xenial+20200423"
-bionic_version="~bionic+20200423"
+stretch_version="~stretch+20200424"
+buster_version="~buster+20200424"
+bionic_version="~bionic+20200424"
+focal_version="~focal+20200424"
