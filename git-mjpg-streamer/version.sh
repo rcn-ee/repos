07d92ab0820db46c367ec1e4e0968098d12527c0
Robert Nelson <robertcnelson@gmail.com>
2020-04-29 14:44:55 -0500

    mjpg-streamer (0.0.0-git20200429.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-mjpg-streamer/version.sh
+++ git-mjpg-streamer/version.sh
@@ -4,19 +4,20 @@
 
 package_name="mjpg-streamer"
 debian_pkg_name="${package_name}"
-package_version="0.0.0-git20190524.0"
+package_version="0.0.0-git20200429.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/jacksonliam/mjpg-streamer"
-git_sha="501f6362c5afddcfb41055f97ae484252c85c912"
+git_sha="5a6e0a2db163e6ae9461552b59079870d0959340"
 reprepro_dir="m/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee4"
+debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-stretch_version="~stretch+20200401"
-buster_version="~buster+20200401"
-bionic_version="~bionic+20200401"
+stretch_version="~stretch+20200429"
+buster_version="~buster+20200429"
+bionic_version="~bionic+20200429"
+focal_version="~focal+20200429"
