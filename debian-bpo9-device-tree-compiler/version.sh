92d2bd3ddb7c7073f64d61ba0007bac9ad89fe08
Robert Nelson <robertcnelson@gmail.com>
2020-04-30 18:54:35 -0500

    device-tree-compiler fix the archive locaiton
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo9-device-tree-compiler/version.sh
+++ debian-bpo9-device-tree-compiler/version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-mirror="http://http.debian.net/debian"
+mirror="http://deb.debian.org/debian"
 
 package_name="device-tree-compiler"
 debian_pkg_name="${package_name}"
@@ -8,7 +8,7 @@ package_version="1.4.7"
 
 git_repo=""
 git_sha=""
-reprepro_dir="l/${package_name}"
+reprepro_dir="d/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-3"
