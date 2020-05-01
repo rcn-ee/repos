c77ab93fbd8054104ee7528e11245dc96d42933f
Robert Nelson <robertcnelson@gmail.com>
2020-04-30 19:34:04 -0500

    device-tree-compiler (1.4.5-3rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ubuntu-1804-device-tree-compiler/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+mirror="http://archive.ubuntu.com/ubuntu"
+
+package_name="device-tree-compiler"
+debian_pkg_name="${package_name}"
+package_version="1.4.5"
+package_source="${package_name}_${package_version}.orig.tar.gz"
+src_dir="dtc-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="d/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-3"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="rcnee0"
+
+bionic_version="~bionic+20200430"
