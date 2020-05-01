9d453a1812144ff79b3935b7bc98cb85b8fd2dc1
Robert Nelson <robertcnelson@gmail.com>
2020-04-30 19:02:03 -0500

    device-tree-compiler (1.4.4-1rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-9-device-tree-compiler/version.sh
+++ debian-9-device-tree-compiler/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-mirror="http://http.debian.net/debian"
+mirror="https://snapshot.debian.org/archive/debian/20170925T220404Z"
 
 package_name="device-tree-compiler"
 debian_pkg_name="${package_name}"
-package_version="1.4.2"
+package_version="1.4.4"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="dtc-${package_version}"
 
@@ -14,8 +14,8 @@ reprepro_dir="d/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1"
-debian_untar=""
-debian_patch="${package_name}_${debian_version}.diff.gz"
-local_patch="rcnee2"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="rcnee0"
 
-stretch_version="~bpo90+20170118+1"
+stretch_version="~stretch+20200430"
