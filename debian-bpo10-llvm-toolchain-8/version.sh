c8c065556cd9ce7373e8a2ad152445f38dba6a67
Robert Nelson <robertcnelson@gmail.com>
2020-05-12 16:38:28 -0500

    fix web address
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo10-llvm-toolchain-8/version.sh
+++ debian-bpo10-llvm-toolchain-8/version.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
+#https://deb.debian.org/debian/pool/main/l/llvm-toolchain-8/llvm-toolchain-8_8.0.1-3~bpo10+1.dsc
 mirror="https://deb.debian.org/debian"
 
 package_name="llvm-toolchain-8"
 debian_pkg_name="${package_name}"
-package_version="8"
+package_version="8.0.1"
 
 git_repo=""
 git_sha=""
