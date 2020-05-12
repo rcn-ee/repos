78baa081a059c335a0e41c3b613008c9fdeffa7b
Robert Nelson <robertcnelson@gmail.com>
2020-05-12 13:49:37 -0500

    libetnaviv (0.0.0-git20141102.1-0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- etnaviv-02-libetnaviv/version.sh
+++ etnaviv-02-libetnaviv/version.sh
@@ -4,7 +4,7 @@
 
 package_name="libetnaviv"
 debian_pkg_name="${package_name}"
-package_version="0.0.0-git20141102.0"
+package_version="0.0.0-git20141102.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
@@ -16,10 +16,8 @@ dl_path=""
 debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
-local_patch="rcnee5"
 
-jessie_version="~jessie+20180831"
-stretch_version="~stretch+20180831"
-buster_version="~buster+20180831"
-xenial_version="~xenial+20180831"
-bionic_version="~bionic+20180831"
+stretch_version="~stretch+20200512"
+buster_version="~buster+20200512"
+bionic_version="~bionic+20200512"
+focal_version="~focal+20200512"
