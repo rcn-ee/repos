94f068175700f79134284229ef4f5cb31ef33634
Robert Nelson <robertcnelson@gmail.com>
2020-05-12 17:21:44 -0500

    mesa (19.0.3-1rcnee1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-mesa/version.sh
+++ debian-10-mesa/version.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
 #https://snapshot.debian.org/package/mesa/
-#https://snapshot.debian.org/archive/debian/20190511T144548Z/pool/main/m/mesa/mesa_18.3.6-2.diff.gz
+#https://snapshot.debian.org/archive/debian/20190508T162608Z/pool/main/m/mesa/mesa_19.0.3-1.diff.gz
 mirror="https://snapshot.debian.org/archive/debian/20190511T144548Z"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="18.3.6"
+package_version="19.0.3"
 package_source="${package_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -15,7 +15,7 @@ git_sha=""
 reprepro_dir="m/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
 local_patch="rcnee1"
