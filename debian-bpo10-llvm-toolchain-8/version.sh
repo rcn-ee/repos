7a4eceaeea40a100b734366f39816edd441715c0
Robert Nelson <robertcnelson@gmail.com>
2020-05-12 16:36:03 -0500

    start rebuild of llvm 8 backport
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo10-llvm-toolchain-8/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="llvm-toolchain-8"
+debian_pkg_name="${package_name}"
+package_version="8"
+
+git_repo=""
+git_sha=""
+reprepro_dir="l/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-3~bpo109+1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+
+dist="debian"
+suite="buster"
+deb_arch="armhf"
