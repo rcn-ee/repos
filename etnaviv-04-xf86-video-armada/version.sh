0e8c6d1e5e8e889c04c7e56c4139bc077b89f68a
Robert Nelson <robertcnelson@gmail.com>
2020-05-12 14:18:18 -0500

    xf86-video-armada (0.0.1-r16-git20190518.0-0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- etnaviv-04-xf86-video-armada/version.sh
+++ etnaviv-04-xf86-video-armada/version.sh
@@ -4,17 +4,19 @@
 
 package_name="xf86-video-armada"
 debian_pkg_name="${package_name}"
-package_version="0.0.1-r16-git20180829.0"
+package_version="0.0.1-r16-git20190518.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://ftp.arm.linux.org.uk/~rmk/xf86-video-armada.git"
-git_sha="78e7116a5bc6cdd9f93cbf1552d342933623ab59"
+git_sha="34a7272573431a9a0fabb94012f902198a3ac9a7"
 reprepro_dir="x/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-bionic_version="~bionic+20180831"
+buster_version="~buster+20200512"
+bionic_version="~bionic+20200512"
+focal_version="~focal+20200512"
