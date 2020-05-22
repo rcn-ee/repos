65691b7e104327e4d4a97f5d0ef34f1c9e5f6b05
Robert Nelson <robertcnelson@gmail.com>
2020-05-22 10:46:21 -0500

    bb-customizations (1.20200522.0-0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20200511.1"
+package_version="1.20200522.0"
 package_source=""
 src_dir=""
 
@@ -15,7 +15,7 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20200511"
-buster_version="~buster+20200511"
-bionic_version="~bionic+20200511"
-focal_version="~focal+20200511"
+stretch_version="~stretch+20200522"
+buster_version="~buster+20200522"
+bionic_version="~bionic+20200522"
+focal_version="~focal+20200522"
