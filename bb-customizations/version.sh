586b860fc14542f6aec871905c4c37f4bb9ccc58
Robert Nelson <robertcnelson@gmail.com>
2020-07-09 17:48:07 -0500

    bb-customizations (1.20200709.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20200522.1"
+package_version="1.20200709.0"
 package_source=""
 src_dir=""
 
@@ -15,7 +15,7 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20200522"
-buster_version="~buster+20200522"
-bionic_version="~bionic+20200522"
-focal_version="~focal+20200522"
+stretch_version="~stretch+20200709"
+buster_version="~buster+20200709"
+bionic_version="~bionic+20200709"
+focal_version="~focal+20200709"
