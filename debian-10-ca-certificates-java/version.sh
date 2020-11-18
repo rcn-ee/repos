6d2e7eb62b2ed076f374ea9e2bbaecb155136a8a
Robert Nelson <robertcnelson@gmail.com>
2020-11-18 14:22:28 -0600

    ca-certificates-java: fix jks-keystore.hook.in
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-ca-certificates-java/version.sh
+++ debian-10-ca-certificates-java/version.sh
@@ -18,6 +18,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}"
 debian_untar=""
 debian_patch=""
-local_patch="rcnee0"
+local_patch="rcnee1"
 
 buster_version="~buster+20201118"
