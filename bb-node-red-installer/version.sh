7c48ee14dfa1388954c5b61b2b798e508aaad267
Robert Nelson <robertcnelson@gmail.com>
2020-12-14 18:22:59 -0600

    bb-node-red-installer (1.2.6-0rcnee2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-node-red-installer/version.sh
+++ bb-node-red-installer/version.sh
@@ -11,12 +11,12 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee2"
 debian_untar=""
 debian_patch=""
 
 debian_dl_1="https://nodejs.org/dist/latest-v10.x/node-v10.23.0-linux-armv7l.tar.xz"
 
-stretch_version="~stretch+20201201"
-buster_version="~buster+20201201"
-bionic_version="~bionic+20201201"
+stretch_version="~stretch+20201214"
+buster_version="~buster+20201214"
+bionic_version="~bionic+20201214"
