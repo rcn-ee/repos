3413be97701d8c787a2fe3b1a10b3e41d65e7124
Robert Nelson <robertcnelson@gmail.com>
2021-02-05 15:23:56 -0600

    cockpit: 237-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo10-cockpit/version.sh
+++ debian-bpo10-cockpit/version.sh
@@ -4,14 +4,14 @@ mirror="http://http.debian.net/debian"
 
 package_name="cockpit"
 debian_pkg_name="${package_name}"
-package_version="235"
+package_version="237"
 
 git_repo=""
 git_sha=""
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1~bpo10+1"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
