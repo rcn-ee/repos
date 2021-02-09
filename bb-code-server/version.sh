ef65ed842095c9dec676b9647d011b90984d46b3
Robert Nelson <robertcnelson@gmail.com>
2021-02-09 10:58:28 -0600

    testing: bb-code-server
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-code-server/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+package_name="bb-code-server"
+debian_pkg_name="${package_name}"
+package_version="3.8.1"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee0"
+debian_untar=""
+debian_patch=""
+
+debian_dl_1="https://nodejs.org/dist/latest-v12.x/node-v12.20.1-linux-armv7l.tar.xz"
+
+bullseye_version="~bullseye+20210209"
