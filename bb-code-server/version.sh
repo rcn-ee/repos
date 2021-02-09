3c63c3ff426c0e3e145a38b18d9af3328dbbb315
Robert Nelson <robertcnelson@gmail.com>
2021-02-09 15:55:21 -0600

    bb-code-server (3.8.1-0rcnee10)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -11,10 +11,10 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee9"
+debian_version="${package_version}-0rcnee10"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://raw.githubusercontent.com/cdr/code-server/v3.8.1/install.sh"
+#debian_dl_1="https://raw.githubusercontent.com/cdr/code-server/v3.8.1/install.sh"
 
 bullseye_version="~bullseye+20210209"
