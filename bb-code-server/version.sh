d08b866c0b616e7ef2d6e20fbe4c785d88c90167
Robert Nelson <robertcnelson@gmail.com>
2021-02-10 08:55:27 -0600

    switch back to buster
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -11,10 +11,10 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee10"
+debian_version="${package_version}-0rcnee11"
 debian_untar=""
 debian_patch=""
 
-#debian_dl_1="https://raw.githubusercontent.com/cdr/code-server/v3.8.1/install.sh"
+debian_dl_1="https://nodejs.org/dist/latest-v12.x/node-v12.20.1-linux-armv7l.tar.xz"
 
-bullseye_version="~bullseye+20210209"
+buster_version="~buster+20210210"
