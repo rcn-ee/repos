7ddf658af97f60d0472d2850c7d235cb222a9fab
Robert Nelson <robertcnelson@gmail.com>
2021-02-10 12:36:30 -0600

    bb-code-server (3.8.1.20210205.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -2,16 +2,16 @@
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
-package_version="3.8.1.20210205.2"
+package_version="3.8.1.20210205.3"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/rcn-ee/code-server"
-git_sha="11f6fe9a3e0d9008da72cf9656a3a5031bfce1f1"
+git_sha="873f80a067a33c420093f54529bc7487a1fb0db6"
 reprepro_dir="b/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee3"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
