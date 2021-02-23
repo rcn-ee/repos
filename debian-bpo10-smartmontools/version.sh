b82d1181872cc2283ccffe468f37838323b98d3a
Robert Nelson <robertcnelson@gmail.com>
2021-02-23 09:58:24 -0600

    add smartmontools backport
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo10-smartmontools/version.sh
+++ debian-bpo10-smartmontools/version.sh
@@ -2,16 +2,16 @@
 
 mirror="http://http.debian.net/debian"
 
-package_name="cockpit"
+package_name="smartmontools"
 debian_pkg_name="${package_name}"
-package_version="237"
+package_version="7.2"
 
 git_repo=""
 git_sha=""
-reprepro_dir="c/${package_name}"
+reprepro_dir="s/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-1~bpo10+1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
