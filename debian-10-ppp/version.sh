a72be7ac6370b44d3302ba521c3f35a24f9bc02e
Robert Nelson <robertcnelson@gmail.com>
2021-03-26 14:03:06 -0500

    ppp (2.4.8-1+1~exp1rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-10-ppp/version.sh
+++ debian-10-ppp/version.sh
@@ -2,11 +2,12 @@
 
 #https://packages.debian.org/source/buster/systemd
 
-mirror="http://http.debian.net/debian"
+#mirror="http://http.debian.net/debian"
+mirror="https://snapshot.debian.org/archive/debian/20200215T213739Z"
 
 package_name="ppp"
 debian_pkg_name="${package_name}"
-package_version="2.4.7"
+package_version="2.4.8"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -15,7 +16,7 @@ git_sha=""
 reprepro_dir="p/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2+4.1+deb10u1"
+debian_version="${package_version}-1+1~exp1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="rcnee0"
