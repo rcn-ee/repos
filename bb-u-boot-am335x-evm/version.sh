9a0e1a133ee0df8c66b0ba049366006553e0bae3
Robert Nelson <robertcnelson@gmail.com>
2021-06-22 14:26:26 -0500

    bb-u-boot-am335x-evm (2019.04.20210622.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-am335x-evm/version.sh
+++ bb-u-boot-am335x-evm/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-am335x-evm"
 debian_pkg_name="${package_name}"
-package_version="2019.04.20210601.0"
+package_version="2019.04.20210622.0"
 package_source=""
 src_dir=""
 
@@ -17,8 +17,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20210601"
-buster_version="~buster+20210601"
-bullseye_version="~bullseye+20210601"
-bionic_version="~bionic+20210601"
-focal_version="~focal+20210601"
+stretch_version="~stretch+20210622"
+buster_version="~buster+20210622"
+bullseye_version="~bullseye+20210622"
+bionic_version="~bionic+20210622"
+focal_version="~focal+20210622"
