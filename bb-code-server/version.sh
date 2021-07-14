bb0bc823e301a97769a873e9c3cd9f13d8272d78
Robert Nelson <robertcnelson@gmail.com>
2021-07-14 17:25:04 -0500

    bb-code-server (3.11.0.20210714.0-0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -2,12 +2,12 @@
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
-package_version="3.10.2.20210521.0"
+package_version="3.11.0.20210714.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/rcn-ee/code-server"
-git_sha="7a86f9602b06b37df24c6a09a234550f11fdc24d"
+git_sha="999601fec88709e10c0950a517350de86fcf2188"
 reprepro_dir="b/${package_name}"
 dl_path=""
 
@@ -15,7 +15,7 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://nodejs.org/dist/latest-v12.x/node-v12.22.1-linux-armv7l.tar.xz"
+debian_dl_1="https://nodejs.org/dist/latest-v14.x/node-v14.17.3-linux-armv7l.tar.xz"
 
-buster_version="~buster+20210526"
-bullseye_version="~bullseye+20210526"
+buster_version="~buster+20210714"
+bullseye_version="~bullseye+20210714"
