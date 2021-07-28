8364f3c98e12f1e2644a114835388918c1ea3069
Robert Nelson <robertcnelson@gmail.com>
2021-07-28 12:22:28 -0500

    bb-beagle-config (0.0.0.20210728.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-beagle-config/version.sh
+++ bb-beagle-config/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-beagle-config"
 debian_pkg_name="${package_name}"
-package_version="0.0.0.20210728.0"
+package_version="0.0.0.20210728.1"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
+stretch_version="~stretch+20210728"
 buster_version="~buster+20210728"
+bullseye_version="~bullseye+20210728"
+bionic_version="~bionic+20210728"
+focal_version="~focal+20210728"
