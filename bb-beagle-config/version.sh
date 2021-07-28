8dbeec204599bf8f5d76e9ab1832d564ae91f307
Robert Nelson <robertcnelson@gmail.com>
2021-07-28 12:28:02 -0500

    drop stretch/bionic, cmake too old (3.11 needed)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-beagle-config/version.sh
+++ bb-beagle-config/version.sh
@@ -17,8 +17,6 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20210728"
 buster_version="~buster+20210728"
 bullseye_version="~bullseye+20210728"
-bionic_version="~bionic+20210728"
 focal_version="~focal+20210728"
