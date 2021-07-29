c328533efc13603fb8b9cee50ef80c71429f397a
Robert Nelson <robertcnelson@gmail.com>
2021-07-29 10:25:20 -0500

    bb-simppru
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-simppru/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+#https://github.com/VedantParanjape/simpPRU
+
+package_name="bb-simppru"
+debian_pkg_name="${package_name}"
+package_version="1.4.20210728.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+buster_version="~buster+20210729"
