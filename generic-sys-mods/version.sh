17f0bcccb47e7cf583d6aa6340f3212680a1fb2f
Robert Nelson <robertcnelson@gmail.com>
2021-08-10 14:57:42 -0500

    generic-sys-mods
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ generic-sys-mods/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+#https://github.com/u-boot/u-boot/commits/master
+
+package_name="generic-sys-mods"
+debian_pkg_name="${package_name}"
+package_version="1.20210809.1"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="g/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+bullseye_version="~bullseye+20210809"
