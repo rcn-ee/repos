896ab77dab71c24b303944013e2ad7799b92f4e8
Robert Nelson <robertcnelson@gmail.com>
2021-08-11 15:24:56 -0500

    bb-u-boot-am57xx-evm (2021.04.20210625.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-am57xx-evm/version.sh
+++ bb-u-boot-am57xx-evm/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-am57xx-evm"
 debian_pkg_name="${package_name}"
-package_version="2021.04.20210625.0"
+package_version="2021.04.20210625.1"
 package_source=""
 src_dir=""
 
@@ -17,8 +17,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20210625"
-buster_version="~buster+20210625"
-bullseye_version="~bullseye+20210625"
-bionic_version="~bionic+20210625"
-focal_version="~focal+20210625"
+stretch_version="~stretch+20210811"
+buster_version="~buster+20210811"
+bullseye_version="~bullseye+20210811"
+bionic_version="~bionic+20210811"
+focal_version="~focal+20210811"
