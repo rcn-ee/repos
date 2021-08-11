0419f51c0c5cca6ddcb681aa695fe52cd8597cde
Robert Nelson <robertcnelson@gmail.com>
2021-08-11 15:20:29 -0500

    bb-u-boot-am335x-evm-blank (2019.04.20210804.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-am335x-evm-blank/version.sh
+++ bb-u-boot-am335x-evm-blank/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-#https://github.com/beagleboard/u-boot/commits/v2021.01-bb.org-am335x-am57xx
+#https://github.com/beagleboard/u-boot/tree/v2019.04-bbb.io-am335x
 
 package_name="bb-u-boot-am335x-evm-blank"
 debian_pkg_name="${package_name}"
-package_version="2019.04.20210601.0"
+package_version="2019.04.20210804.1"
 package_source=""
 src_dir=""
 
@@ -17,8 +17,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20210602"
-buster_version="~buster+20210602"
-bullseye_version="~bullseye+20210602"
-bionic_version="~bionic+20210602"
-focal_version="~focal+20210602"
+stretch_version="~stretch+20210811"
+buster_version="~buster+20210811"
+bullseye_version="~bullseye+20210811"
+bionic_version="~bionic+20210811"
+focal_version="~focal+20210811"
