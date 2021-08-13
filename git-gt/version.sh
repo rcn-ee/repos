8de03a5032a0c4612662a6a84c1e9fcfb2c478e7
Robert Nelson <robertcnelson@gmail.com>
2021-08-12 22:20:06 -0500

    gt (0.0.0-git20210712.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-gt/version.sh
+++ git-gt/version.sh
@@ -4,7 +4,7 @@
 
 package_name="gt"
 debian_pkg_name="${package_name}"
-package_version="0.0.0-git20210712.0"
+package_version="0.0.0-git20210712.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
