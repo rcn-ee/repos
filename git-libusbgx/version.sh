3d37d6697bd8e97c0bee182d43764753a081033a
Robert Nelson <robertcnelson@gmail.com>
2021-08-12 20:35:19 -0500

    libusbgx (0.2.0-git20210811.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-libusbgx/version.sh
+++ git-libusbgx/version.sh
@@ -4,7 +4,7 @@
 
 package_name="libusbgx"
 debian_pkg_name="${package_name}"
-package_version="0.2.0-git20210811.0"
+package_version="0.2.0-git20210811.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
