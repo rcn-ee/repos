bccfc9a6c09ab0232360181fde2a31540e5afcfa
Robert Nelson <robertcnelson@gmail.com>
2021-08-16 20:56:49 -0500

    bb-beagle-version (1.20210816.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-beagle-version/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+#https://github.com/beagleboard/customizations
+
+package_name="bb-beagle-version"
+debian_pkg_name="${package_name}"
+package_version="1.20210816.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+bullseye_version="~bullseye+20210816"
