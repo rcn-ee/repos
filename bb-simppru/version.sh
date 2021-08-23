323a2251030469014193dc96397fcd81bef9dc02
Robert Nelson <robertcnelson@gmail.com>
2021-08-23 09:48:08 -0500

    bb-simppru (1.4.20210823.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-simppru/version.sh
+++ bb-simppru/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-simppru"
 debian_pkg_name="${package_name}"
-package_version="1.4.20210820.0"
+package_version="1.4.20210823.0"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,4 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-buster_version="~buster+20210821"
+buster_version="~buster+20210823"
