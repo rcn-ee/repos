2db7044617fdeed4d6bc9d63456e09633ca50fce
Robert Nelson <robertcnelson@gmail.com>
2021-09-22 17:34:08 -0500

    bb-wl18xx-firmware (1.20210922.1-0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wlan0-defaults/version.sh
+++ bb-wlan0-defaults/version.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-package_name="bb-wl18xx-firmware"
+package_name="bb-wlan0-defaults"
 debian_pkg_name="${package_name}"
-package_version="1.20210922.1"
+package_version="1.20210922.0"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,4 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20210922"
-buster_version="~buster+20210922"
 bullseye_version="~bullseye+20210922"
-bionic_version="~bionic+20210922"
-focal_version="~focal+20210922"
