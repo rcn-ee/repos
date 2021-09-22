ffc3e907c87d13e1399d074d423f95972ede947b
Robert Nelson <robertcnelson@gmail.com>
2021-09-22 17:06:42 -0500

    wpa (2:2.9.0-21rcnee1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-11-wpa/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="wpa"
+debian_pkg_name="${package_name}"
+epoch="2"
+package_version="2.9.0"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-2.9"
+
+git_repo=""
+git_sha=""
+reprepro_dir="w/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-21"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="rcnee1"
+
+bullseye_version="~bullseye+20210922"
