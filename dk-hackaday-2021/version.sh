901a590a5493dafc44cf2b36d91ded391e6add4f
Robert Nelson <robertcnelson@gmail.com>
2021-09-28 13:38:22 -0500

    dk-hackaday-2021 (1.20210928.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ dk-hackaday-2021/version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+package_name="dk-hackaday-2021"
+debian_pkg_name="${package_name}"
+package_version="1.20210928.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="d/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+bullseye_version="~bullseye+20210928"
