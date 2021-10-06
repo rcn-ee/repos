3357c8cce273a7e46ceb7a8bfd8dee64a5de180f
Robert Nelson <robertcnelson@gmail.com>
2021-10-06 17:02:42 -0500

    ti-pru-cgt-v2.3 (2.3.3-0rcnee2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-pru-cgt-v2.3/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://www.ti.com/tool/download/PRU-CGT-2-3
+
+package_name="ti-pru-cgt-v2.3"
+debian_pkg_name="${package_name}"
+package_version="2.3.3"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0rcnee2"
+debian_untar=""
+debian_patch=""
+
+debian_dl_1="https://software-dl.ti.com/codegen/esd/cgt_public_sw/PRU/2.3.3/ti_cgt_pru_2.3.3_armlinuxa8hf_busybox_installer.sh"
+
+bullseye_version="~bullseye+20211006"
