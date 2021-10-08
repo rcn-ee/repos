8c2bd8ec9a4913e600f0d1719f017fa2d95bb110
Robert Nelson <robertcnelson@gmail.com>
2021-10-07 20:47:28 -0500

    bb-bbai-firmware (1.20210923.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-bbai-firmware/version.sh
+++ bb-bbai-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-bbai-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20210923.1"
+package_version="1.20210923.2"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20210923"
-buster_version="~buster+20210923"
-bullseye_version="~bullseye+20210923"
-bionic_version="~bionic+20210923"
-focal_version="~focal+20210923"
+stretch_version="~stretch+20211007"
+buster_version="~buster+20211007"
+bullseye_version="~bullseye+20211007"
+bionic_version="~bionic+20211007"
+focal_version="~focal+20211007"
