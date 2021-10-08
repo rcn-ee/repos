dc6eb22f656881ea9ec6559d68df2e14256da6e2
Robert Nelson <robertcnelson@gmail.com>
2021-10-07 20:31:54 -0500

    bb-wlan0-defaults (1.20211006.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wlan0-defaults/version.sh
+++ bb-wlan0-defaults/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wlan0-defaults"
 debian_pkg_name="${package_name}"
-package_version="1.20211006.0"
+package_version="1.20211006.1"
 package_source=""
 src_dir=""
 
@@ -15,4 +15,5 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-bullseye_version="~bullseye+20211006"
+bullseye_version="~bullseye+20211007"
+focal_version="~focal+20211007"
