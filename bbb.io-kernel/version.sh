7af0c9900af5352bc16680316eb7ddb39384e220
Robert Nelson <robertcnelson@gmail.com>
2021-10-14 11:46:30 -0500

    bbb.io-kernel (1.20211014.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-kernel/version.sh
+++ bbb.io-kernel/version.sh
@@ -1,21 +1,19 @@
 #!/bin/bash -e
 
-#https://github.com/rcn-ee/bbb.io-kernel
-#https://github.com/rcn-ee/bbb.io-kernel/commits/master
-
 package_name="bbb.io-kernel"
 debian_pkg_name="${package_name}"
-package_version="20211014.5"
-package_source="${package_name}_${package_version}.orig.tar.xz"
-src_dir="${package_name}_${package_version}"
+package_version="1.20211014.0"
+package_source=""
+src_dir=""
 
-git_repo="https://github.com/rcn-ee/bbb.io-kernel"
-git_sha="135c2f046744e6aad3048a37552bb28ba60701f6"
+git_repo=""
+git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path=""
 
 debian_version="${package_version}-0"
+debian_untar=""
 debian_patch=""
-debian_diff=""
 
+buster_version="~buster+20211014"
 bullseye_version="~bullseye+20211014"
