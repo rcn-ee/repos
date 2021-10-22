5b6c4efef9a3e3f1268e122626db228f20174bdb
Robert Nelson <robertcnelson@gmail.com>
2021-10-22 09:36:47 -0500

    cockpit (255-1rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-11-cockpit/version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+#https://tracker.debian.org/pkg/cockpit
+#https://packages.debian.org/source/stable/cockpit
+
+mirror="http://http.debian.net/debian"
+
+package_name="cockpit"
+debian_pkg_name="${package_name}"
+package_version="255"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="rcnee0"
+
+bullseye_version="~bullseye+20211022"
