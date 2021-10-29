eba6a8a87a7129cfbee48de9cdc83eea7209b754
Robert Nelson <robertcnelson@gmail.com>
2021-10-29 15:50:33 -0500

    beagleconnect (1.20211029.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bbb.io-beagleconnect/version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+package_name="beagleconnect"
+debian_pkg_name="${package_name}"
+package_version="1.20211029.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="d/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+buster_version="~buster+20211029"
+bullseye_version="~bullseye+20211029"
