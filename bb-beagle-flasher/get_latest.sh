ddf38f59b4eb9b5441e97be6d627131befad1c64
Robert Nelson <robertcnelson@gmail.com>
2021-11-18 14:22:40 -0600

    beagle-flasher (1.20211117.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-beagle-flasher/get_latest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dl_src () {
+	if [ -d ./src/ ] ; then
+		rm -rf ./src/
+	fi
+
+	git clone https://github.com/rcn-ee/beagle-flasher ./src/
+}
+
+copy_all () {
+	cp -v ./src/${wfile} ./suite/bullseye/debian/
+}
+
+dl_src
+
+wfile="beagle-flasher" ; copy_all
+wfile="templates/beaglebone-black-emmc-to-microsd" ; copy_all
+wfile="templates/beaglebone-black-microsd-to-emmc" ; copy_all
