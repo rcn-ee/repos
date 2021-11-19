3355e804eada3f91a670343885ba103f64f5319d
Robert Nelson <robertcnelson@gmail.com>
2021-11-19 14:55:19 -0600

    bb-bbai-firmware (1.20211117.2)
    fixes: https://github.com/RobertCNelson/omap-image-builder/issues/184
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-bbai-firmware/version.sh
+++ bb-bbai-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-bbai-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20211117.1"
+package_version="1.20211117.2"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20211117"
-buster_version="~buster+20211117"
-bullseye_version="~bullseye+20211117"
-bionic_version="~bionic+20211117"
-focal_version="~focal+20211117"
+stretch_version="~stretch+20211119"
+buster_version="~buster+20211119"
+bullseye_version="~bullseye+20211119"
+bionic_version="~bionic+20211119"
+focal_version="~focal+20211119"
