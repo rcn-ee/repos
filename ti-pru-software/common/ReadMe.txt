10f3b1dad72061c3f230c5d26b13cb33b06757b8
Robert Nelson <robertcnelson@gmail.com>
2021-11-30 14:58:52 -0600

    ti-pru-software (6.0.1-git20211124.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-pru-software/common/ReadMe.txt
+++ ti-pru-software/common/ReadMe.txt
@@ -1,4 +1,4 @@
-Programmable Real-time Unit (PRU) Software Support Package v6.0.0
+Programmable Real-time Unit (PRU) Software Support Package v6.0.1
 
 
 DESCRIPTION
--- ti-pru-software/version.sh
+++ ti-pru-software/version.sh
@@ -4,12 +4,12 @@
 
 package_name="ti-pru-software"
 debian_pkg_name="${package_name}"
-package_version="6.0.0-git20211019.0"
+package_version="6.0.1-git20211124.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/pru-software-support-package/pru-software-support-package.git"
-git_sha="acd8479d7f3bdbb4536ceca3cd6803662babaa5a"
+git_sha="8eee431384b6a1a9c96ae91a8e99485176b4d2b3"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
@@ -17,4 +17,4 @@ debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-bullseye_version="~bullseye+20211110"
+bullseye_version="~bullseye+20211130"
