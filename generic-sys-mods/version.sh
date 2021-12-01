34f87f0570b291fb3a8873bf8c09900231b626ac
Robert Nelson <robertcnelson@gmail.com>
2021-12-01 10:57:49 -0600

    generic-sys-mods (1.20211201.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- generic-sys-mods/version.sh
+++ generic-sys-mods/version.sh
@@ -4,7 +4,7 @@
 
 package_name="generic-sys-mods"
 debian_pkg_name="${package_name}"
-package_version="1.20211103.2"
+package_version="1.20211201.0"
 package_source=""
 src_dir=""
 
@@ -17,6 +17,6 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-buster_version="~buster+20211103"
-bullseye_version="~bullseye+20211103"
-focal_version="~focal+20211103"
+buster_version="~buster+20211201"
+bullseye_version="~bullseye+20211201"
+focal_version="~focal+20211201"
