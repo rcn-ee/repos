07585044fb1989ee39b3fd84552410aa269ec735
Robert Nelson <robertcnelson@gmail.com>
2021-12-02 09:27:05 -0600

    bb-beagle-flasher (1.20211202.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-beagle-flasher/version.sh
+++ bb-beagle-flasher/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-beagle-flasher"
 debian_pkg_name="${package_name}"
-package_version="1.20211201.0"
+package_version="1.20211202.0"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20211201"
+bullseye_version="~bullseye+20211202"
