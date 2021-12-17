94a81a0ebce4e5c886a49144e607958efefa3f08
Robert Nelson <robertcnelson@gmail.com>
2021-12-17 11:22:47 -0600

    bbb.io-vsx-examples (1.20211215.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bbb.io-vsx-examples/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#https://github.com/beagleboard/VSX-examples
+
+package_name="bbb.io-vsx-examples"
+debian_pkg_name="${package_name}"
+package_version="1.20211215.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+buster_version="~buster+20211217"
+bullseye_version="~bullseye+20211217"
