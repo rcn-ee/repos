4062c175ea80c708e97766026cdafcaddce8283c
Robert Nelson <robertcnelson@gmail.com>
2021-12-22 11:23:54 -0600

    bb-wl18xx-firmware (1.20211222.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/version.sh
+++ bb-wl18xx-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wl18xx-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20210922.2"
+package_version="1.20211222.0"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,10 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-stretch_version="~stretch+20211007"
-buster_version="~buster+20211007"
-bullseye_version="~bullseye+20211007"
-bionic_version="~bionic+20211007"
-focal_version="~focal+20211007"
+clear_changelog="enable"
+
+stretch_version="~stretch+20211222"
+buster_version="~buster+20211222"
+bullseye_version="~bullseye+20211222"
+bionic_version="~bionic+20211222"
+focal_version="~focal+20211222"
