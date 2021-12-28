4bc5757e19a771d9f12f4f40803e6baee1b77ba8
Robert Nelson <robertcnelson@gmail.com>
2021-12-28 16:47:38 -0600

    bb-simppru (1.4.20211012.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-simppru/version.sh
+++ bb-simppru/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-simppru"
 debian_pkg_name="${package_name}"
-package_version="1.4.20211012.0"
+package_version="1.4.20211012.1"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,7 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-buster_version="~buster+20211012"
+clear_changelog="enable"
+
+buster_version="~buster+20211228"
+bullseye_version="~bullseye+20211228"
