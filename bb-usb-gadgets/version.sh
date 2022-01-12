9750eecd95b48af0f543e5f859d24339ada53392
Robert Nelson <robertcnelson@gmail.com>
2022-01-12 15:43:32 -0600

    bb-usb-gadgets (1.20220112.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-usb-gadgets/version.sh
+++ bb-usb-gadgets/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-usb-gadgets"
 debian_pkg_name="${package_name}"
-package_version="1.20220106.0"
+package_version="1.20220112.3"
 package_source=""
 src_dir=""
 
@@ -17,8 +17,8 @@ debian_patch=""
 
 clear_changelog="enable"
 
-stretch_version="~stretch+20220106"
-buster_version="~buster+20220106"
-bullseye_version="~bullseye+20220106"
-bionic_version="~bionic+20220106"
-focal_version="~focal+20220106"
+stretch_version="~stretch+20220112"
+buster_version="~buster+20220112"
+bullseye_version="~bullseye+20220112"
+bionic_version="~bionic+20220112"
+focal_version="~focal+20220112"
