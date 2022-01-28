c77a7ac4c771718d41ccf1ac18c2176c156ef529
Robert Nelson <robertcnelson@gmail.com>
2022-01-27 19:52:53 -0600

    bb-code-server (4.0.2.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
-package_version="4.0.2.0"
+package_version="4.0.2.1"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://github.com/cdr/code-server/releases/download/v4.0.2/code-server-4.0.2-linux-armv7l.tar.gz"
-#debian_dl_1="https://github.com/cdr/code-server/releases/download/v4.0.2/code-server-4.0.2-linux-arm64.tar.gz"
+debian_dl_1="https://github.com/cdr/code-server/releases/download/v4.0.1/code-server-4.0.1-linux-armv7l.tar.gz"
+#debian_dl_1="https://github.com/cdr/code-server/releases/download/v4.0.1/code-server-4.0.1-linux-arm64.tar.gz"
 
 clear_changelog="enable"
 
