d82802ac1a456499d1c3d9020994c123dcf1d194
Robert Nelson <robertcnelson@gmail.com>
2022-02-11 11:23:31 -0600

    mesa (21.3.5-1rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-11-mesa/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="mesa"
+debian_pkg_name="${package_name}"
+package_version="21.3.5"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="m/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar=""
+debian_patch="${package_name}_${debian_version}.diff.gz"
+local_patch="rcnee0"
+
+bullseye_version="~bullseye+20220211"
