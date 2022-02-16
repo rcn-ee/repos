961bd9e8cc6e9b8ed86ae82473db525ce94bf533
Robert Nelson <robertcnelson@gmail.com>
2022-02-16 11:20:06 -0600

    and version
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bbb.io-xfce4-desktop/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+package_name="bbb.io-xfce4-desktop"
+debian_pkg_name="${package_name}"
+package_version="1.20220118.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20220216"
