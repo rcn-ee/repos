b264255ecd1d92029b1c53807344d52eef4951d5
Robert Nelson <robertcnelson@gmail.com>
2022-02-17 14:46:43 -0600

    bb-code-server (4.0.2.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
-package_version="4.0.2.1"
+package_version="4.0.2.2"
 package_source=""
 src_dir=""
 
@@ -20,5 +20,5 @@ debian_dl_1="https://github.com/cdr/code-server/releases/download/v4.0.1/code-se
 
 clear_changelog="enable"
 
-buster_version="~buster+20220127"
-bullseye_version="~bullseye+20220127"
+buster_version="~buster+20220217"
+bullseye_version="~bullseye+20220217"
