b1191c9f0565647ed4b5511c50c149fce463dbe4
Robert Nelson <robertcnelson@gmail.com>
2022-02-17 13:31:05 -0600

    libiio (0.23-2rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-libiio/version.sh
+++ debian-11-libiio/version.sh
@@ -13,9 +13,9 @@ git_sha=""
 reprepro_dir="libi/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="rcnee0"
 
-bullseye_version="~bullseye+20211111"
+bullseye_version="~bullseye+20220217"
