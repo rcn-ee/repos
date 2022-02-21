bfe0f4cadf949a7867108ad4254fceb8eb0622a6
Robert Nelson <robertcnelson@gmail.com>
2022-02-21 09:50:23 -0600

    binutils-pru (2.38-0rcnee4)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- binutils-pru-debian-11-ubuntu-2004/version.sh
+++ binutils-pru-debian-11-ubuntu-2004/version.sh
@@ -2,7 +2,7 @@
 
 package_name="binutils-pru"
 debian_pkg_name="${package_name}"
-package_version="2.37.50.g2749ac13"
+package_version="2.38"
 package_source=""
 src_dir=""
 
@@ -11,9 +11,9 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0rcnee4"
 debian_untar=""
 debian_patch=""
 
-bullseye_version="~bullseye+20211223"
-focal_version="~focal+20211223"
+bullseye_version="~bullseye+20220221"
+focal_version="~focal+20220221"
