e92a19f01ade10f1cb6ea9b92ea41bf9bcb013f0
Robert Nelson <robertcnelson@gmail.com>
2022-03-02 19:09:12 -0600

    add:  debian-bpo10-libinih
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo10-libinih/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+#https://packages.debian.org/source/bullseye/libinih
+mirror="https://deb.debian.org/debian"
+
+package_name="libinih"
+debian_pkg_name="${package_name}"
+package_version="53"
+
+git_repo=""
+git_sha=""
+reprepro_dir="libi/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+
+dist="debian"
+suite="buster"
+deb_arch="armhf"
