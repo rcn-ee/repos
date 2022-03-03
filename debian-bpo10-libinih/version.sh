8ee849a158b00e5f87305b8b074b3a452017e80c
Robert Nelson <robertcnelson@gmail.com>
2022-03-02 19:11:00 -0600

    try 48
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo10-libinih/version.sh
+++ debian-bpo10-libinih/version.sh
@@ -5,14 +5,14 @@ mirror="https://deb.debian.org/debian"
 
 package_name="libinih"
 debian_pkg_name="${package_name}"
-package_version="53"
+package_version="48"
 
 git_repo=""
 git_sha=""
 reprepro_dir="libi/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-1~bpo10+1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
