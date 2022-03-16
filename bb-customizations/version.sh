eb03cee8aeedd020a35840f803540b954366c814
Robert Nelson <robertcnelson@gmail.com>
2022-03-16 16:25:22 -0500

    bb-customizations (1.20220316.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20220301.0"
+package_version="1.20220316.0"
 package_source=""
 src_dir=""
 
@@ -19,8 +19,8 @@ debian_patch=""
 
 clear_changelog="enable"
 
-stretch_version="~stretch+20220301"
-buster_version="~buster+20220301"
-bullseye_version="~bullseye+20220301"
-bionic_version="~bionic+20220301"
-focal_version="~focal+20220301"
+stretch_version="~stretch+20220316"
+buster_version="~buster+20220316"
+bullseye_version="~bullseye+20220316"
+bionic_version="~bionic+20220316"
+focal_version="~focal+20220316"
