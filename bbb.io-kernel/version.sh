be05bc7fd6cd9f4f30e3c48ce9e966e6416dbf84
Robert Nelson <robertcnelson@gmail.com>
2022-03-31 15:55:04 -0500

    bbb.io-kernel (1.20220331.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-kernel/version.sh
+++ bbb.io-kernel/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bbb.io-kernel"
 debian_pkg_name="${package_name}"
-package_version="1.20220325.0"
+package_version="1.20220331.0"
 package_source=""
 src_dir=""
 
@@ -17,5 +17,5 @@ debian_patch=""
 
 clear_changelog="enable"
 
-buster_version="~buster+20220325"
-bullseye_version="~bullseye+20220325"
+buster_version="~buster+20220331"
+bullseye_version="~bullseye+20220331"
--- git-mesa/version.sh
+++ git-mesa/version.sh
@@ -4,12 +4,12 @@ mirror="http://http.debian.net/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="22.1.0-git20210331.1"
+package_version="22.1.0-git20210331.2"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://gitlab.freedesktop.org/mesa/mesa"
-git_sha="23bd889541a7fb213cae52becc891c34bfc9d19d"
+git_sha="33c800bf913771ddadb5bd632df46582f4d1c9f3"
 reprepro_dir="m/${package_name}"
 dl_path=""
 
