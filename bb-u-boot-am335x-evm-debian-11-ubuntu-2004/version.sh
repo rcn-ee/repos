374b5faea643961c18a8be7f9501289545b8ca59
Robert Nelson <robertcnelson@gmail.com>
2022-06-02 15:48:08 -0500

    bb-u-boot-am335x-evm (2022.04.20220602.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-am335x-evm-debian-11-ubuntu-2004/version.sh
+++ bb-u-boot-am335x-evm-debian-11-ubuntu-2004/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-am335x-evm"
 debian_pkg_name="${package_name}"
-package_version="2022.04.20220407.1"
+package_version="2022.04.20220602.0"
 package_source=""
 src_dir=""
 
@@ -19,5 +19,5 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220408"
-focal_version="~focal+20220408"
+bullseye_version="~bullseye+20220602"
+focal_version="~focal+20220602"
