9b2b8384717ed737d1fd67543d94a0e27be604ad
Robert Nelson <robertcnelson@gmail.com>
2022-06-28 10:38:00 -0500

    bb-beagle-config (0.1.0.20220611.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-beagle-config/version.sh
+++ bb-beagle-config/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-#https://github.com/SAtacker/beagle-config
+#https://github.com/SAtacker/bb-config
 
 package_name="bb-beagle-config"
 debian_pkg_name="${package_name}"
-package_version="0.1.0.20210827.0"
+package_version="0.1.0.20220611.0"
 package_source=""
 src_dir=""
 
@@ -17,6 +17,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-buster_version="~buster+20210830"
-bullseye_version="~bullseye+20210830"
-focal_version="~focal+20210830"
+clear_changelog="enable"
+
+buster_version="~buster+20220628"
+bullseye_version="~bullseye+20220628"
+focal_version="~focal+20220628"
