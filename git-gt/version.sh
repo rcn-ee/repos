e19406068cce4ee651c1a388e18223bce90d9cac
Robert Nelson <robertcnelson@gmail.com>
2022-07-13 09:41:56 -0500

    gt (0.0.0-git20210712.8)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-gt/version.sh
+++ git-gt/version.sh
@@ -4,7 +4,7 @@
 
 package_name="gt"
 debian_pkg_name="${package_name}"
-package_version="0.0.0-git20210712.7"
+package_version="0.0.0-git20210712.8"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
@@ -17,8 +17,9 @@ debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-stretch_version="~stretch+20210826"
-buster_version="~buster+20210826"
-bullseye_version="~bullseye+20210826"
-bionic_version="~bionic+20210826"
-focal_version="~focal+20210826"
+clear_changelog="enable"
+
+buster_version="~buster+20220713"
+bullseye_version="~bullseye+20220713"
+bionic_version="~bionic+20220713"
+focal_version="~focal+20220713"
