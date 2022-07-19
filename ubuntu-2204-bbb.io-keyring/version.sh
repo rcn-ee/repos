2929722eed26e95a549c0615796e0695322323e9
Robert Nelson <robertcnelson@gmail.com>
2022-07-19 16:45:14 -0500

    bbb.io-keyring (1.20220421.0-0~jammy)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ubuntu-2204-bbb.io-keyring/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+package_name="bbb.io-keyring"
+debian_pkg_name="${package_name}"
+package_version="1.20220421.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+jammy_version="~jammy+20220719"
