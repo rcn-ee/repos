82f521ad55f3821cab7b2e271f99a90aa3f19083
Robert Nelson <robertcnelson@gmail.com>
2022-07-19 15:44:40 -0500

    add version
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ubuntu-2204-dpkg/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://packages.ubuntu.com/source/jammy/dpkg
+mirror="http://ports.ubuntu.com"
+
+package_name="dpkg"
+debian_pkg_name="${package_name}"
+package_version="1.21.1ubuntu2.1"
+package_source="${debian_pkg_name}_${package_version}.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="d/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version=""
+debian_untar=""
+debian_patch=""
+local_patch="rcnee0"
+
+jammy_version="~jammy+20220719"
