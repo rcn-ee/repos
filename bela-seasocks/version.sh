41161952cebdba762aa87015ae0cd17a03156e33
Robert Nelson <robertcnelson@gmail.com>
2022-07-20 10:23:45 -0500

    seasocks (1.4.4-git20220530.3), missing install, dont rm/ldconfig
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bela-seasocks/version.sh
+++ bela-seasocks/version.sh
@@ -4,7 +4,7 @@
 
 package_name="seasocks"
 debian_pkg_name="${package_name}"
-package_version="1.4.4-git20220530.2"
+package_version="1.4.4-git20220530.3"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
