d98d448d4f278ae788bc8f9e7d0d499f8de2dd36
Robert Nelson <robertcnelson@gmail.com>
2022-07-20 14:16:31 -0500

    libiio (0.24-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-libiio/version.sh
+++ debian-11-libiio/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="libiio"
 debian_pkg_name="${package_name}"
-package_version="0.23"
+package_version="0.24"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,9 +13,9 @@ git_sha=""
 reprepro_dir="libi/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee0"
+local_patch="bbbio0"
 
-bullseye_version="~bullseye+20220217"
+bullseye_version="~bullseye+20220720"
