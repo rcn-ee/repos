078f2f9ced1266ff276520f64b55eb6add606c50
Robert Nelson <robertcnelson@gmail.com>
2022-07-20 13:10:50 -0500

    bb-code-server-next (4.5.1.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server-next/version.sh
+++ bb-code-server-next/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-#https://github.com/coder/code-server/releases/tag/v4.4.0
+#https://github.com/coder/code-server/releases/tag/v4.5.1
 
 package_name="bb-code-server-next"
 debian_pkg_name="${package_name}"
-package_version="4.4.0.0"
+package_version="4.5.1.0"
 package_source=""
 src_dir=""
 
@@ -17,10 +17,11 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://github.com/coder/code-server/releases/download/v4.4.0/code-server-4.4.0-linux-armv7l.tar.gz"
-#debian_dl_1="https://github.com/coder/code-server/releases/download/v4.4.0/code-server-4.4.0-linux-arm64.tar.gz"
+debian_dl_1="https://github.com/coder/code-server/releases/download/v4.5.1/code-server-4.5.1-linux-armv7l.tar.gz"
+#debian_dl_1="https://github.com/coder/code-server/releases/download/v4.5.1/code-server-4.5.1-linux-arm64.tar.gz"
+
 
 clear_changelog="enable"
 
 buster_version="~buster+20220217"
-bullseye_version="~bullseye+20220506"
+bullseye_version="~bullseye+20220720"
