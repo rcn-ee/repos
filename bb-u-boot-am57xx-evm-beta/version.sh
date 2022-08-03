21ef74d2cb8585218186064505388430f3c4003d
Robert Nelson <robertcnelson@gmail.com>
2022-08-03 15:30:43 -0500

    bb-u-boot-am57xx-evm-beta (2022.04.20220803.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-am57xx-evm-beta/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/u-boot/-/tree/v2022.04-bbb.io-am335x-am57xx
+
+package_name="bb-u-boot-am57xx-evm-beta"
+debian_pkg_name="${package_name}"
+package_version="2022.04.20220803.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+buster_version="~buster+20220803"
+bullseye_version="~bullseye+20220803"
