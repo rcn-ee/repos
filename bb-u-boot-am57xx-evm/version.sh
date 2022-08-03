ab3a7d9e3e9d6a15a7a71c6cd3d97affa3d3eaeb
Robert Nelson <robertcnelson@gmail.com>
2022-08-03 16:18:56 -0500

    bb-u-boot-am57xx-evm (2022.04.20220803.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-am57xx-evm/version.sh
+++ bb-u-boot-am57xx-evm/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-#https://github.com/beagleboard/u-boot/tree/v2020.10-rc4-bbb.io-am57xx
+#https://git.beagleboard.org/beagleboard/u-boot/-/tree/v2022.04-bbb.io-am335x-am57xx
 
 package_name="bb-u-boot-am57xx-evm"
 debian_pkg_name="${package_name}"
-package_version="2021.04.20210625.3"
+package_version="2022.04.20220803.0"
 package_source=""
 src_dir=""
 
@@ -19,9 +19,8 @@ debian_patch=""
 
 clear_changelog="enable"
 
-stretch_version="~stretch+20220720"
-buster_version="~buster+20220720"
-bullseye_version="~bullseye+20220720"
-bionic_version="~bionic+20220720"
-focal_version="~focal+20220720"
-jammy_version="~jammy+20220720"
+buster_version="~buster+20220803"
+bullseye_version="~bullseye+20220803"
+bionic_version="~bionic+20220803"
+focal_version="~focal+20220803"
+jammy_version="~jammy+20220803"
