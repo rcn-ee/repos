d1fe07ce692341c8ec9e9836f2a3f552b38b1545
Robert Nelson <robertcnelson@gmail.com>
2022-08-22 14:25:58 -0500

    bb-code-server (4.6.0.1) enable https)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
-package_version="4.6.0.0"
+package_version="4.6.0.1"
 package_source=""
 src_dir=""
 
@@ -21,5 +21,5 @@ debian_dl_1="https://github.com/coder/code-server/releases/download/v4.6.0/code-
 
 clear_changelog="enable"
 
-buster_version="~buster+20220819"
-bullseye_version="~bullseye+20220819"
+buster_version="~buster+20220822"
+bullseye_version="~bullseye+20220822"
