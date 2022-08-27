a30e960da7f5b787f0ba06f54513366c8bd18311
Robert Nelson <robertcnelson@gmail.com>
2022-08-27 11:26:48 -0500

    rename package
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- xenomai-v3.0.x/version.sh
+++ xenomai-v3.0.x/version.sh
@@ -2,7 +2,7 @@
 
 mirror="http://http.debian.net/debian"
 
-package_name="xenomai"
+package_name="xenomai-v3.0"
 debian_pkg_name="${package_name}"
 package_version="3.0.13-git20220802.0"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
@@ -16,5 +16,7 @@ dl_path=""
 debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
+
+clear_changelog="enable"
 
 bullseye_version="~bullseye+20220827"
