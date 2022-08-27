3f16eb6ed7d4930367251222615f3b36efafe056
Robert Nelson <robertcnelson@gmail.com>
2022-08-27 11:18:26 -0500

    xenomai (3.0.13-git20220802.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ xenomai-v3.0.x/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="xenomai"
+debian_pkg_name="${package_name}"
+package_version="3.0.13-git20220802.0"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
+
+git_repo="https://source.denx.de/Xenomai/xenomai.git"
+git_sha="18b35f837ea7db421a132d0bfd9067ad9f9441d7"
+reprepro_dir="x/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_patch=""
+debian_diff=""
+
+bullseye_version="~bullseye+20220827"
