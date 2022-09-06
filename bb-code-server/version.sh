58c39e7ec942be298190b8808a9d7b12d21a9640
Robert Nelson <robertcnelson@gmail.com>
2022-09-06 10:34:31 -0500

    bb-code-server (4.6.1.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 
-#https://github.com/coder/code-server/releases/tag/v4.6.0
+#https://github.com/coder/code-server/releases/tag/v4.6.1
+
+arch="armv7l"
+ver="4.6.1"
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
-package_version="4.6.0.1"
+package_version="${ver}.0"
 package_source=""
 src_dir=""
 
@@ -17,9 +20,9 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://github.com/coder/code-server/releases/download/v4.6.0/code-server-4.6.0-linux-armv7l.tar.gz"
+debian_dl_1="https://github.com/coder/code-server/releases/download/v${ver}/code-server-${ver}-linux-${arch}.tar.gz"
 
 clear_changelog="enable"
 
-buster_version="~buster+20220822"
-bullseye_version="~bullseye+20220822"
+buster_version="~buster+20220906"
+bullseye_version="~bullseye+20220906"
