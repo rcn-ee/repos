a05383623960d71fed334edf6b7423ee860c8442
Robert Nelson <robertcnelson@gmail.com>
2022-09-20 11:52:08 -0500

    rcnee-internal-testlab (1.20220920.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ rcnee-internal-testlab/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+package_name="rcnee-internal-testlab"
+debian_pkg_name="${package_name}"
+package_version="1.20220920.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+buster_version="~buster+20220920"
+bullseye_version="~bullseye+20220920"
