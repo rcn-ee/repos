7bcc0d5886d06c8a7c18964fa1a2cc0e7f9fd56e
Robert Nelson <robertcnelson@gmail.com>
2022-11-18 22:16:46 -0600

    bbb.io-kernel (1.20221118.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-kernel/version.sh
+++ bbb.io-kernel/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bbb.io-kernel"
 debian_pkg_name="${package_name}"
-package_version="1.20221109.0"
+package_version="1.20221118.0"
 package_source=""
 src_dir=""
 
@@ -17,7 +17,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-buster_version="~buster+20221109"
-bullseye_version="~bullseye+20221109"
-focal_version="~focal+20221109"
-jammy_version="~jammy+20221109"
+buster_version="~buster+20221118"
+bullseye_version="~bullseye+20221118"
+focal_version="~focal+20221118"
+jammy_version="~jammy+20221118"
