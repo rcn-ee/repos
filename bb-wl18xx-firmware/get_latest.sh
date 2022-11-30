921d5573da878fb971343c67098bc493e73ec937
Robert Nelson <robertcnelson@gmail.com>
2022-11-30 10:12:33 -0600

    bb-wl18xx-firmware (1.20221130.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/get_latest.sh
+++ bb-wl18xx-firmware/get_latest.sh
@@ -8,9 +8,16 @@ dl_src () {
 	git clone https://git.beagleboard.org/beagleboard/usr-customizations.git ./src/
 }
 
+copy_old () {
+	cp -v ./src/${wfile} ./suite/bionic/debian/
+	cp -v ./src/${wfile} ./suite/buster/debian/
+}
+
 copy_all () {
 	cp -v ./src/${wfile} ./suite/bionic/debian/
 	cp -v ./src/${wfile} ./suite/buster/debian/
+	cp -v ./src/${wfile} ./suite/focal/debian/
+	cp -v ./src/${wfile} ./suite/bullseye/debian/
 }
 
 copy_some () {
@@ -20,11 +27,12 @@ copy_some () {
 
 dl_src
 
-wfile="arm32/usr/bin/bb-wl18xx-bluetooth" ; copy_all
-wfile="arm32/lib/systemd/system/bb-wl18xx-bluetooth.service" ; copy_all
-wfile="arm32/usr/bin/bb-wl18xx-tether" ; copy_all
-wfile="arm32/usr/bin/bb-wl18xx-wlan0" ; copy_all
-wfile="arm32/lib/systemd/system/bb-wl18xx-wlan0.service" ; copy_all
+wfile="arm32/usr/bin/bb-wl18xx-bluetooth" ; copy_old
+wfile="arm32/lib/systemd/system/bb-wl18xx-bluetooth.service" ; copy_old
+wfile="arm32/usr/bin/bb-wl18xx-tether" ; copy_old
+wfile="arm32/usr/bin/bb-wl18xx-wlan0" ; copy_old
+wfile="arm32/lib/systemd/system/bb-wl18xx-wlan0.service" ; copy_old
+wfile="arm32/usr/sbin/bb-wl18xx-select-fw" ; copy_all
 
 #wfile="TIInit_11.8.32.bts" ; wdir="/debian/" ; copy_all
 #wfile="wl18xx.conf" ; wdir="/debian/" ; copy_all
