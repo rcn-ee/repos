faf041dc21e76142745be0313cdd195412db3701
Robert Nelson <robertcnelson@gmail.com>
2022-12-28 09:26:28 -0600

    wpa (2:2.9.0-23rcnee2) - enable both nl80211,wext
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-wpa/version.sh
+++ debian-11-wpa/version.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
 #https://tracker.debian.org/pkg/wpa
+#https://snapshot.debian.org/package/wpa/2%3A2.9.0-23/
 
-mirror="http://http.debian.net/debian"
+#mirror="http://http.debian.net/debian"
+mirror="https://snapshot.debian.org/archive/debian/20211019T151710Z/"
 
 package_name="wpa"
 debian_pkg_name="${package_name}"
@@ -19,7 +21,7 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-23"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee1"
+local_patch="rcnee2"
 
-bullseye_version="~bullseye+20211119"
-focal_version="~focal+20211119"
+bullseye_version="~bullseye+20221228"
+focal_version="~focal+20221228"
