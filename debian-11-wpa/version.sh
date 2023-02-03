a039f2098cbc73162162335f760dd4d6c3527d6b
Robert Nelson <robertcnelson@gmail.com>
2023-02-03 11:39:50 -0600

    wpa (2:2.10-11bbbio1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-wpa/version.sh
+++ debian-11-wpa/version.sh
@@ -1,27 +1,25 @@
 #!/bin/bash -e
 
 #https://tracker.debian.org/pkg/wpa
-#https://snapshot.debian.org/package/wpa/2%3A2.9.0-23/
 
-#mirror="http://http.debian.net/debian"
-mirror="https://snapshot.debian.org/archive/debian/20211019T151710Z/"
+mirror="http://http.debian.net/debian"
 
 package_name="wpa"
 debian_pkg_name="${package_name}"
 epoch="2"
-package_version="2.9.0"
+package_version="2.10"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
-src_dir="${package_name}-2.9"
+src_dir="${package_name}-2.10"
 
 git_repo=""
 git_sha=""
 reprepro_dir="w/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-23"
+debian_version="${package_version}-11"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee2"
+local_patch="bbbio1"
 
-bullseye_version="~bullseye+20221228"
-focal_version="~focal+20221228"
+bullseye_version="~bullseye+20230203"
+focal_version="~focal+20230203"
