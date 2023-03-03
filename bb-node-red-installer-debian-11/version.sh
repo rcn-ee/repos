49b60b84bb3655744a615e3954921e31e4b75c75
Robert Nelson <robertcnelson@gmail.com>
2023-03-02 18:12:47 -0600

    bb-node-red-installer (3.0.2.3) [force node-red to use local copy of node/npm]
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-node-red-installer-debian-11/version.sh
+++ bb-node-red-installer-debian-11/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="3.0.2.2"
+package_version="3.0.2.3"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://nodejs.org/dist/latest-v14.x/node-v14.20.0-linux-armv7l.tar.xz"
+debian_dl_1="https://nodejs.org/dist/latest-v14.x/node-v14.21.3-linux-armv7l.tar.xz"
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220810"
+bullseye_version="~bullseye+20230302"
