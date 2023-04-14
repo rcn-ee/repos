3c8694fb34990d985f8db7b71c882bfca50e690a
Robert Nelson <robertcnelson@gmail.com>
2023-04-14 11:54:57 -0500

    generic-sys-mods (1.20230414.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- generic-sys-mods/version.sh
+++ generic-sys-mods/version.sh
@@ -4,7 +4,7 @@
 
 package_name="generic-sys-mods"
 debian_pkg_name="${package_name}"
-package_version="1.20220216.1"
+package_version="1.20230414.0"
 package_source=""
 src_dir=""
 
@@ -19,6 +19,8 @@ debian_patch=""
 
 clear_changelog="enable"
 
-buster_version="~buster+20220216"
-bullseye_version="~bullseye+20220216"
-focal_version="~focal+20220216"
+buster_version="~buster+20230414"
+bullseye_version="~bullseye+20230414"
+bookworm_version="~bookworm+20230414"
+focal_version="~focal+20230414"
+jammy_version="~jammy+20230414"
