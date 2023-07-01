7eba27dc57f916524a10c74eae365646cfba3242
Robert Nelson <robertcnelson@gmail.com>
2023-07-01 15:03:20 -0500

    dpkg (1.21.1ubuntu2.2rcnee1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ubuntu-2204-dpkg/version.sh
+++ ubuntu-2204-dpkg/version.sh
@@ -5,7 +5,7 @@ mirror="http://ports.ubuntu.com"
 
 package_name="dpkg"
 debian_pkg_name="${package_name}"
-package_version="1.21.1ubuntu2.1"
+package_version="1.21.1ubuntu2.2"
 package_source="${debian_pkg_name}_${package_version}.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -17,6 +17,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version=""
 debian_untar=""
 debian_patch=""
-local_patch="rcnee3"
+local_patch="rcnee1"
 
-jammy_version="~jammy+20220719"
+jammy_version="~jammy+20230701"
