fb5b50bf7b45f0ba38f3298b6db9ecec9fe263d3
Robert Nelson <robertcnelson@gmail.com>
2023-07-10 15:54:48 -0500

    bb-beagle-flasher (1.20230710.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-beagle-flasher/version.sh
+++ bb-beagle-flasher/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-beagle-flasher"
 debian_pkg_name="${package_name}"
-package_version="1.20221010.1"
+package_version="1.20230710.0"
 package_source=""
 src_dir=""
 
@@ -17,7 +17,8 @@ debian_patch=""
 
 clear_changelog="enable"
 
-buster_version="~buster+20221010"
-bullseye_version="~bullseye+20221010"
-focal_version="~focal+20221010"
-jammy_version="~jammy+20221010"
+buster_version="~buster+20230710"
+bullseye_version="~bullseye+20230710"
+bookworm_version="~bookworm+20230710"
+focal_version="~focal+20230710"
+jammy_version="~jammy+20230710"
