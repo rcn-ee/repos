f8972fff7562743e11c70b15c63d84a6925ba2ca
Robert Nelson <robertcnelson@gmail.com>
2023-08-04 16:20:29 -0500

    binutils-pru (2.41-0bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- binutils-pru-debian-11-12-ubuntu-2004-2204/version.sh
+++ binutils-pru-debian-11-12-ubuntu-2004-2204/version.sh
@@ -2,7 +2,7 @@
 
 package_name="binutils-pru"
 debian_pkg_name="${package_name}"
-package_version="2.38"
+package_version="2.41"
 package_source=""
 src_dir=""
 
@@ -11,9 +11,12 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee5"
+debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
+local_patch="bbbio0"
 
-bullseye_version="~bullseye+20220304"
-focal_version="~focal+20220304"
+bullseye_version="~bullseye+20230804"
+bookworm_version="~bookworm+20230804"
+focal_version="~focal+20230804"
+jammy_version="~jammy+20230804"
