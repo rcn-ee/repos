1300491ccda1c492602c62bc1fb3d0e4ca411620
Robert Nelson <robertcnelson@gmail.com>
2023-08-04 20:18:48 -0500

    cleanup
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- gcc-pru/version.sh
+++ gcc-pru/version.sh
@@ -13,10 +13,9 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee2"
+debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
+local_patch="bbbio0"
 
-buster_version="~buster+20200903"
 bullseye_version="~bullseye+20200903"
-bionic_version="~bionic+20200903"
