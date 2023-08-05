86b58c42928557c399e6c382fd488526d02986c3
Robert Nelson <robertcnelson@gmail.com>
2023-08-04 20:22:40 -0500

    gcc-pru (13.2.0-0bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- gcc-pru-debian-11-12-ubuntu-2004-2204/version.sh
+++ gcc-pru-debian-11-12-ubuntu-2004-2204/version.sh
@@ -2,20 +2,23 @@
 
 package_name="gcc-pru"
 debian_pkg_name="${package_name}"
-package_version="12.1.0"
+package_version="13.2.0"
 package_source=""
 src_dir=""
 
-newlib_version="4.1.0"
+newlib_version="4.2.0.20211231"
 
 git_repo=""
 git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
+local_patch="bbbio0"
 
-bullseye_version="~bullseye+20220510"
-focal_version="~focal+20220510"
+bullseye_version="~bullseye+20230804"
+bookworm_version="~bookworm+20230804"
+focal_version="~focal+20230804"
+jammy_version="~jammy+20230804"
