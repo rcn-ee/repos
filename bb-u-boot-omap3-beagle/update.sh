9cfbd31c3c8e7940a2d49e4de247555c8ade0c9d
Robert Nelson <robertcnelson@gmail.com>
2023-09-05 14:35:27 -0500

    bb-u-boot-omap3-beagle (2023.10.20230904)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-omap3-beagle/update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -i -e 's:rc3:rc4:g' ./suite/*/debian/rules
--- bb-u-boot-omap3-beagle/version.sh
+++ bb-u-boot-omap3-beagle/version.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -e
 
-#https://github.com/beagleboard/u-boot/commits/v2021.07-rc3-bbb.io-am335x-am57xx
-#
-#git checkout v2021.07-rc3 -b v2021.07-rc3-bbb.io-am335x-am57xx
-#git push origin v2021.07-rc3-bbb.io-am335x-am57xx
-
 package_name="bb-u-boot-omap3-beagle"
 debian_pkg_name="${package_name}"
-package_version="2023.10.20230821.0"
+package_version="2023.10.20230904.0"
 package_source=""
 src_dir=""
 
@@ -22,5 +17,5 @@ debian_patch=""
 
 clear_changelog="enable"
 
-buster_version="~buster+20230828"
-bullseye_version="~bullseye+20230828"
+buster_version="~buster+20230905"
+bullseye_version="~bullseye+20230905"
