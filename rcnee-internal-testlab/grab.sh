1a9592d5955f766f35ce4f8bbf314b7634f68946
Robert Nelson <robertcnelson@gmail.com>
2023-09-06 19:15:17 -0500

    rcnee-internal-testlab (1.20230906.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- rcnee-internal-testlab/grab.sh
+++ rcnee-internal-testlab/grab.sh
@@ -8,3 +8,5 @@ chmod +x check-config.sh
 cp -v check-config.sh ./suite/buster/debian/docker-check-config.sh
 cp -v check-config.sh ./suite/bullseye/debian/docker-check-config.sh
 cp -v check-config.sh ./suite/bookworm/debian/docker-check-config.sh
+cp -v check-config.sh ./suite/trixie/debian/docker-check-config.sh
+cp -v check-config.sh ./suite/jammy/debian/docker-check-config.sh
