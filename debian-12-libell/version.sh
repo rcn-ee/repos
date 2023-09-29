4bec3cd8d1651f2d3474e1f3c6c4d1298b1ea8e0
Robert Nelson <robertcnelson@gmail.com>
2023-09-29 16:34:43 -0500

    backport: debian-12-libell
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-12-libell/version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+#https://tracker.debian.org/pkg/wpa
+#https://packages.debian.org/source/trixie/ell
+
+mirror="http://http.debian.net/debian"
+
+package_name="ell"
+debian_pkg_name="${package_name}"
+package_version="0.58"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="e/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-2"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="bbbio1"
+
+bookworm_version="~bookworm+20230925"
