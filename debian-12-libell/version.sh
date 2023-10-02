b99d0bd35a9d00ed161c5303ec52dfee6b4532ba
Robert Nelson <robertcnelson@gmail.com>
2023-10-02 13:54:29 -0500

    ell (0.58-2bbbio1~bookworm)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-libell/version.sh
+++ debian-12-libell/version.sh
@@ -19,6 +19,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio2"
+local_patch="bbbio1"
 
-bookworm_version="~bookworm+20230930"
+bookworm_version="~bookworm+20231002"
