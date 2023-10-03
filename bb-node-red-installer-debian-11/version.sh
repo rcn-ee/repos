193098daa987e3baefd190d9a5941924852137d0
Robert Nelson <robertcnelson@gmail.com>
2023-10-03 16:17:53 -0500

    bb-node-red-installer (3.1.0.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-node-red-installer-debian-11/version.sh
+++ bb-node-red-installer-debian-11/version.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
+#https://www.npmjs.com/package/node-red
+
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="3.0.2.3"
+package_version="3.1.0.0"
 package_source=""
 src_dir=""
 
@@ -19,4 +21,5 @@ debian_dl_1="https://nodejs.org/dist/latest-v14.x/node-v14.21.3-linux-armv7l.tar
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230302"
+bullseye_version="~bullseye+20231003"
+bookworm_version="~bookworm+20231003"
