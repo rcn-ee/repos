a77486016fd5b0e356a0d99bd97152da61936f74
Robert Nelson <robertcnelson@gmail.com>
2023-11-28 09:09:16 -0600

    next
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ apindex/version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/u-boot/-/tree/v2022.04-bbb.io-am335x-am57xx
+
+package_name="bb-u-boot-am335x-evm"
+debian_pkg_name="${package_name}"
+package_version="2022.04.20220602.2"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20230804"
+bookworm_version="~bookworm+20230804"
+focal_version="~focal+20230804"
+jammy_version="~jammy+20230804"
