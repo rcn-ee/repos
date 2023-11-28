2f4131eaa6d16568db00edc5f41a8c42156a5ed3
Robert Nelson <robertcnelson@gmail.com>
2023-11-28 09:15:11 -0600

    apindex (2.2-1.20230319.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- apindex/version.sh
+++ apindex/version.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -e
 
-#https://git.beagleboard.org/beagleboard/u-boot/-/tree/v2022.04-bbb.io-am335x-am57xx
-
-package_name="bb-u-boot-am335x-evm"
+package_name="apindex"
 debian_pkg_name="${package_name}"
-package_version="2022.04.20220602.2"
+package_version="2.2-1.20230319.0"
 package_source=""
 src_dir=""
 
 git_repo=""
 git_sha=""
-reprepro_dir="b/${package_name}"
+reprepro_dir="a/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-0"
@@ -20,6 +18,6 @@ debian_patch=""
 clear_changelog="enable"
 
 bullseye_version="~bullseye+20230804"
-bookworm_version="~bookworm+20230804"
 focal_version="~focal+20230804"
 jammy_version="~jammy+20230804"
+bookworm_version="~bookworm+20231128"
