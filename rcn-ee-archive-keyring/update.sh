a1cca4a4ad09f70129d10cf55798afef2cb6b4f0
Robert Nelson <robertcnelson@gmail.com>
2023-12-06 12:29:11 -0600

    remove bionic/zesty old package
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- rcn-ee-archive-keyring/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-old="2016.04.24"
-new="2017.01.24"
-
-tar xf rcn-ee-archive-keyring_${old}.orig.tar.xz
-mv rcn-ee-archive-keyring_${old} rcn-ee-archive-keyring_${new}
-tar -cJf rcn-ee-archive-keyring_${new}.orig.tar.xz rcn-ee-archive-keyring_${new}/
