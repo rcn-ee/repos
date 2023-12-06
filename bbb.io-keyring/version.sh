42d77da0a29ffe04c24af214035371ba17c254e8
Robert Nelson <robertcnelson@gmail.com>
2023-12-06 13:58:58 -0600

    bbb.io-keyring (1.20231206.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-keyring/version.sh
+++ bbb.io-keyring/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bbb.io-keyring"
 debian_pkg_name="${package_name}"
-package_version="1.20230414.0"
+package_version="1.20231206.0"
 package_source=""
 src_dir=""
 
@@ -17,9 +17,11 @@ debian_patch=""
 
 clear_changelog="enable"
 
-buster_version="~buster+20230414"
-bullseye_version="~bullseye+20230414"
-bookworm_version="~bookworm+20230414"
 bionic_version="~bionic+20230414"
-focal_version="~focal+20230414"
-jammy_version="~jammy+20230414"
+buster_version="~buster+20231206"
+bullseye_version="~bullseye+20231206"
+bookworm_version="~bookworm+20231206"
+trixie_version="~trixie+20231206"
+focal_version="~focal+20231206"
+jammy_version="~jammy+20231206"
+noble_version="~noble+20231206"
