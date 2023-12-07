801a21a49b2739fa9e743cf812748dfb7f5aac33
Robert Nelson <robertcnelson@gmail.com>
2023-12-07 17:41:35 -0600

    iwd (2.10-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-iwd/version.sh
+++ debian-12-iwd/version.sh
@@ -7,7 +7,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="iwd"
 debian_pkg_name="${package_name}"
-package_version="2.8"
+package_version="2.10"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -19,6 +19,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio1"
+local_patch="bbbio0"
 
-bookworm_version="~bookworm+20230926"
+bookworm_version="~bookworm+20231207"
