adc2e50d638a0e492816b398eaabf8b54344611a
Robert Nelson <robertcnelson@gmail.com>
2024-01-17 16:08:22 -0600

    ti-pru-software-v6.2 (6.2.0-0bbbio1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-pru-software-v6.2/version.sh
+++ ti-pru-software-v6.2/version.sh
@@ -17,7 +17,7 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
-local_patch="bbbio0"
+local_patch="bbbio1"
 
 clear_changelog="enable"
 
