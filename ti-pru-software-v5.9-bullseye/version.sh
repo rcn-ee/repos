897b79e2232bde1c4d5e3c83b00f6dd0dfeff8a8
Robert Nelson <robertcnelson@gmail.com>
2024-01-18 11:42:22 -0600

    add old bullseye debian package
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-pru-software-v5.9-bullseye/version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+#https://git.ti.com/gitweb?p=pru-software-support-package/pru-software-support-package.git;a=tags
+#https://git.gfnd.rcn-ee.org/TexasInstruments/pru-software-support-package/tags
+
+package_name="ti-pru-software-v5.9"
+debian_pkg_name="${package_name}"
+package_version="5.9.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+local_patch="bbbio1"
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20240117"
+bookworm_version="~bookworm+20240117"
+trixie_version="~trixie+20240117"
