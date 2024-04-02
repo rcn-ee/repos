7940a9a6603263d796f8be27d7b239b37bf74575
Robert Nelson <robertcnelson@gmail.com>
2024-04-02 10:00:14 -0500

    ell (0.64-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-libell/version.sh
+++ debian-12-libell/version.sh
@@ -7,7 +7,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="ell"
 debian_pkg_name="${package_name}"
-package_version="0.63"
+package_version="0.64"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -21,4 +21,4 @@ debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="bbbio0"
 
-bookworm_version="~bookworm+20240314"
+bookworm_version="~bookworm+20240402"
