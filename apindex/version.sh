33b6c9d3d633787d7cd08f78e14d90ef7d528ba3
Robert Nelson <robertcnelson@gmail.com>
2024-06-04 09:27:03 -0500

    apindex (2.2-1.20231128.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- apindex/version.sh
+++ apindex/version.sh
@@ -2,7 +2,7 @@
 
 package_name="apindex"
 debian_pkg_name="${package_name}"
-package_version="2.2-1.20231128.0"
+package_version="2.2-1.20231128.1"
 package_source=""
 src_dir=""
 
@@ -18,8 +18,9 @@ debian_patch=""
 clear_changelog="enable"
 
 buster_version="~buster+20231128"
-bullseye_version="~bullseye+20231128"
-bookworm_version="~bookworm+20231128"
-trixie_version="~trixie+20231128"
-focal_version="~focal+20231128"
-jammy_version="~jammy+20231128"
+bullseye_version="~bullseye+20240604"
+bookworm_version="~bookworm+20240604"
+trixie_version="~trixie+20240604"
+focal_version="~focal+20240604"
+jammy_version="~jammy+20240604"
+noble_version="~noble+20240604"
