05c17a7342234524f566d68e214802dbc55bb8e1
Robert Nelson <robertcnelson@gmail.com>
2024-06-25 13:54:51 -0500

    bb-wlan0-defaults (1.20240625.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wlan0-defaults/version.sh
+++ bb-wlan0-defaults/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wlan0-defaults"
 debian_pkg_name="${package_name}"
-package_version="1.20230929.1"
+package_version="1.20240625.0"
 package_source=""
 src_dir=""
 
@@ -17,7 +17,9 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230929"
-bookworm_version="~bookworm+20230929"
-focal_version="~focal+20230929"
-jammy_version="~jammy+20230929"
+bullseye_version="~bullseye+20240625"
+bookworm_version="~bookworm+20240625"
+trixie_version="~trixie+20240625"
+focal_version="~focal+20240625"
+jammy_version="~jammy+20240625"
+noble_version="~noble+20240625"
