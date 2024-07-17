65c67cf6a6a88a1e10814f2802fabacb4e6bb0b1
Robert Nelson <robertcnelson@gmail.com>
2024-07-17 13:27:36 -0500

    bb-usb-gadgets (1.20240717.0)
    fixes: https://github.com/RobertCNelson/omap-image-builder/issues/216
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-usb-gadgets/version.sh
+++ bb-usb-gadgets/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-usb-gadgets"
 debian_pkg_name="${package_name}"
-package_version="1.20240704.0"
+package_version="1.20240717.0"
 package_source=""
 src_dir=""
 
@@ -17,9 +17,9 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240704"
-bookworm_version="~bookworm+20240704"
-trixie_version="~trixie+20240704"
-focal_version="~focal+20240704"
-jammy_version="~jammy+20240704"
-noble_version="~noble+20240704"
+bullseye_version="~bullseye+20240717"
+bookworm_version="~bookworm+20240717"
+trixie_version="~trixie+20240717"
+focal_version="~focal+20240717"
+jammy_version="~jammy+20240717"
+noble_version="~noble+20240717"
