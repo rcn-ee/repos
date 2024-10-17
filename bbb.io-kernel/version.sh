41c87311b3bc5b3cbf198d7ba1f65eb384c6d568
Robert Nelson <robertcnelson@gmail.com>
2024-10-17 10:56:58 -0500

    bbb.io-kernel (1.20241017.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-kernel/version.sh
+++ bbb.io-kernel/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bbb.io-kernel"
 debian_pkg_name="${package_name}"
-package_version="1.20241004.0"
+package_version="1.20241017.0"
 package_source=""
 src_dir=""
 
@@ -17,9 +17,9 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20241004"
-bookworm_version="~bookworm+20241004"
-trixie_version="~trixie+20241004"
-focal_version="~focal+20241004"
-jammy_version="~jammy+20241004"
-noble_version="~noble+20241004"
+bullseye_version="~bullseye+20241017"
+bookworm_version="~bookworm+20241017"
+trixie_version="~trixie+20241017"
+focal_version="~focal+20241017"
+jammy_version="~jammy+20241017"
+noble_version="~noble+20241017"
