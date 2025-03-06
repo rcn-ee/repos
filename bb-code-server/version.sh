9527255e75293f81694345caf557ec4da165204e
Robert Nelson <robertcnelson@gmail.com>
2025-03-06 11:04:02 -0600

    bb-code-server (4.97.2-0bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -3,7 +3,7 @@
 #https://github.com/coder/code-server/releases
 
 arch="armv7l"
-ver="4.93.1"
+ver="4.97.2"
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
@@ -24,8 +24,8 @@ debian_dl_1="https://github.com/coder/code-server/releases/download/v${ver}/code
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240926"
-bookworm_version="~bookworm+20240926"
-trixie_version="~trixie+20240926"
-jammy_version="~jammy+20240926"
-noble_version="~noble+20240926"
+bullseye_version="~bullseye+20250306"
+bookworm_version="~bookworm+20250306"
+trixie_version="~trixie+20250306"
+jammy_version="~jammy+20250306"
+noble_version="~noble+20250306"
